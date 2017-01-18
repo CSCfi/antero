8ae4969197cbe8c0cace796d19d105fec60584e7
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-01-18 15:19:28 +0200

    Add loaders for CIMO related tables.

---
+++ db/sql/246__dw_create_procedure_p_lataa_d_liikkuvuudentyyppi.sql
@@ -0,0 +1,4 @@
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dw.p_lataa_d_liikkuvuudentyyppi') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE dw.p_lataa_d_liikkuvuudentyyppi AS'
+END
