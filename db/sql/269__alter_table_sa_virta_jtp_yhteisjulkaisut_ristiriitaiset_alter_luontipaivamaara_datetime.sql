a2d6b87ef8cc7828305798e4f43ed5dbbdef0f16
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-01-24 13:12:06 +0200

    Fix script due to problem in changing bigint to datetime2. Make drop-add instead.

--- db/sql/269__alter_table_sa_virta_jtp_yhteisjulkaisut_ristiriitaiset_alter_luontipaivamaara_datetime.sql
+++ db/sql/269__alter_table_sa_virta_jtp_yhteisjulkaisut_ristiriitaiset_alter_luontipaivamaara_datetime.sql
@@ -1,7 +1,10 @@
+-- bigint will not convert to datetime?
 IF EXISTS (
   select * from INFORMATION_SCHEMA.COLUMNS
   where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_yhteisjulkaisut_ristiriitaiset'
   and COLUMN_NAME='luontipaivamaara' and DATA_TYPE='bigint'
 ) BEGIN
-ALTER TABLE sa.sa_virta_jtp_yhteisjulkaisut_ristiriitaiset ALTER COLUMN luontipaivamaara datetime2(0)
+ALTER TABLE sa.sa_virta_jtp_yhteisjulkaisut_ristiriitaiset DROP COLUMN luontipaivamaara
 END
+
+ALTER TABLE sa.sa_virta_jtp_yhteisjulkaisut_ristiriitaiset ADD luontipaivamaara datetime2(0) null
