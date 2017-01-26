b759229715c2880fae1bc787a198b2add1b99134
Joonas Pesonen <joonas.pesonen@csc.fi>
2017-01-26 15:57:12 +0200

    update aliases

--- db/sql/278__dw_alter_view_v_virta_otp_kvliikkuvuus.sql
+++ db/sql/278__dw_alter_view_v_virta_otp_kvliikkuvuus.sql
@@ -1,32 +1,32 @@
 ALTER VIEW [dw].[v_virta_otp_kvliikkuvuus] AS 
 SELECT d1.organisaatio_koodi
-	  ,d1.organisaatio_fi
+	  ,d1.organisaatio_fi AS 'Oppilaitos'
 	  ,d1.organisaatio_sv
 	  ,d1.organisaatio_en
 
       ,d2.ohjauksenala_koodi
-	  ,d2.ohjauksenala_nimi_fi
+	  ,d2.ohjauksenala_nimi_fi AS 'OKM Ohjauksen ala'
 	  ,d2.ohjauksenala_nimi_sv
 	  ,d2.ohjauksenala_nimi_en
 
-	  ,d3.koulutusastetaso2_koodi
-	  ,d3.koulutusastetaso2_fi
+	  ,d3.koulutusastetaso2_koodi AS 'Koulutusaste (taso 2) Koodi'
+	  ,d3.koulutusastetaso2_fi AS 'Koulutusaste (taso 2)'
 	  ,d3.koulutusalataso2_sv
 	  ,d3.koulutusastetaso2_en
-	  ,d3.koulutusalataso1_koodi
-	  ,d3.koulutusalataso1_fi
+	  ,d3.koulutusalataso1_koodi AS 'Koulutusala (taso 1) Koodi'
+	  ,d3.koulutusalataso1_fi AS 'Koulutusala (taso 1)'
 	  ,d3.koulutusalataso1_sv
 	  ,d3.koulutusalataso1_en
-	  ,d3.koulutusalataso2_koodi
-	  ,d3.koulutusalataso2_fi
+	  ,d3.koulutusalataso2_koodi AS 'Koulutusala (taso 2) Koodi'
+	  ,d3.koulutusalataso2_fi AS 'Koulutusala (taso 2)'
 	  ,d3.koulutusalataso2_sv
 	  ,d3.koulutusalataso2_en
-	  ,d3.koulutusalataso3_koodi
-	  ,d3.koulutusalataso3_fi
+	  ,d3.koulutusalataso3_koodi AS 'Koulutusala (taso 3) Koodi'
+	  ,d3.koulutusalataso3_fi AS 'Koulutusala (taso 3)'
 	  ,d3.koulutusalataso3_sv
 	  ,d3.koulutusalataso3_en
-	  ,d3.koulutusluokitus_koodi
-	  ,d3.koulutusluokitus_fi
+	  ,d3.koulutusluokitus_koodi AS 'Koulutus Koodi'
+	  ,d3.koulutusluokitus_fi AS 'Koulutusnimike'
 	  ,d3.koulutusluokitus_sv
 	  ,d3.koulutusluokitus_en
 
@@ -42,18 +42,18 @@ SELECT d1.organisaatio_koodi
 
 	  ,d6.maatjavaltiot2_koodi AS 'Lähtömaa Koodi'
 	  ,d6.maatjavaltiot2_fi AS 'Lähtömaa'
-	  ,d6.maatjavaltiot2_sv
-	  ,d6.maatjavaltiot2_en
+	  ,d6.maatjavaltiot2_sv AS 'Lähtömaa SV'
+	  ,d6.maatjavaltiot2_en AS 'Lähtömaa EN'
 
 	  ,d7.maatjavaltiot2_koodi AS 'Kohdemaa Koodi'
 	  ,d7.maatjavaltiot2_fi AS 'Kohdemaa'
-	  ,d7.maatjavaltiot2_sv
-	  ,d7.maatjavaltiot2_en
+	  ,d7.maatjavaltiot2_sv AS 'Kohdemaa SV'
+	  ,d7.maatjavaltiot2_en AS 'Kohdemaa EN'
 
 	  ,d8.maatjavaltiot2_koodi AS 'Kansalaisuus Koodi'
 	  ,d8.maatjavaltiot2_fi AS 'Kansalaisuus'
-	  ,d8.maatjavaltiot2_sv
-	  ,d8.maatjavaltiot2_en
+	  ,d8.maatjavaltiot2_sv AS 'Kansalaisuus SV'
+	  ,d8.maatjavaltiot2_en AS 'Kansalaisuus EN'
 
 	  ,d9.liikkuvuusohjelma_koodi AS 'Liikkuvuusohjelma Koodi'
 	  ,d9.liikkuvuusohjelma_fi AS 'Liikkuvuusohjelma'
