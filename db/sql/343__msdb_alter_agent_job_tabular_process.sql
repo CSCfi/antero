b5e6ff90f4fa7f195c399c0b00e7b15abc27fb2f
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-02-24 17:55:50 +0200

    Modify agent job by adding another step in it which gives a hint for others to find out job run is done.

---
+++ db/sql/343__msdb_alter_agent_job_tabular_process.sql
@@ -0,0 +1,38 @@
+-- NOTICE THE USE OF msdb DATABASE
+use msdb
+
+DECLARE @p_server nvarchar(128) = N'§sed-me-tabserver§'
+DECLARE @p_jobName nvarchar(128) = N'Tabular process'
+
+DECLARE @jobId BINARY(16)
+select @jobId=job_id from msdb.dbo.sysjobs_view where name=@p_jobName
+
+IF EXISTS (select * from msdb.dbo.sysjobs_view where name=@p_jobName) BEGIN
+EXEC msdb.dbo.sp_delete_jobstep @job_id=@jobId, @step_id=1
+
+EXEC msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'§to-be-set-by-procedure§', 
+  @step_id=1, 
+  @cmdexec_success_code=0, 
+  @on_success_action=3, 
+  @on_fail_action=2, 
+  @retry_attempts=0, 
+  @retry_interval=0, 
+  @os_run_priority=0, @subsystem=N'ANALYSISCOMMAND', 
+  @command=N'{"refresh": {"type": "full", "objects": [{"database": "§to-be-set-by-procedure§"}]}}', 
+  @server=@p_server, 
+  @flags=0, 
+  @proxy_name=N'ProcessTabularProxy'
+
+EXEC msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Mark ready', 
+  @step_id=2, 
+  @cmdexec_success_code=0, 
+  @on_success_action=1, 
+  @on_fail_action=2, 
+  @retry_attempts=0, 
+  @retry_interval=0, 
+  @os_run_priority=0, @subsystem=N'TSQL', 
+  @command=N'update tabularprocess set ready=1 where tabular=''§to-be-set-by-procedure§''', 
+  @database_name=N'ANTERO', 
+  @flags=0
+
+END
