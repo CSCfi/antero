5f89626be17aecc21f0338b77fc59531f9b09775
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-03-10 17:00:29 +0200

    Typo fix.

--- db/sql/429__alter_procedure_p_lataa_f_amk_opintopisteet.sql
+++ db/sql/429__alter_procedure_p_lataa_f_amk_opintopisteet.sql
@@ -1,3 +1,4 @@
+/*
 ALTER PROCEDURE dw.p_lataa_f_amk_opintopisteet
 AS
 TRUNCATE TABLE dw.f_amk_opintopisteet;
@@ -25,7 +26,6 @@ INSERT INTO dw.f_amk_opintopisteet (
   avoinamk_op_kansan_lkm,
   avoinamk_op_kansal_lkm,
   avoinamk_op_muut_lkm,
-  
   kv_vaihtoopiskelijat_op_lkm,
   kk_yhteistyo_op_lkm,
   erilopintooik_op_lkm,
@@ -34,6 +34,52 @@ INSERT INTO dw.f_amk_opintopisteet (
 
   source
 )
+--*/
+SELECT
+ coalesce(d1.id,-1) as d_amk_id
+,coalesce(d2.id,-1) as d_koulutusala_2002_id
+,-1 as d_ohjauksenala_id
+,coalesce(d3.id,-1) as d_amk_koulutustyyppi_id
+,coalesce(d4.id,-1) as d_tilannepvm_id
+,s.vuosi
+
+,kaikki_tutk_lkm
+
+,tki_pisteet_lkm
+,tki_harj_pisteet_lkm
+,tki_muu_pisteet_lkm
+
+,vierkiel_pisteet_lkm
+,ulkom_hyv_op_lkm
+
+,0 as ulkom_harj_op_lkm
+
+--amk2b
+,sb.avoinamk_op_lkm
+,sb.avoinamk_op_amk_lkm
+,sb.avoinamk_op_yliop_lkm
+,sb.avoinamk_op_kesa_lkm
+,sb.avoinamk_op_kansan_lkm
+,sb.avoinamk_op_kansal_lkm
+,sb.avoinamk_op_muut_lkm
+
+,sb.kv_vaihtoopiskelijat_op_lkm
+,sb.kk_yhteistyo_op_lkm
+,sb.erilopintooik_op_lkm
+,sb.mamu_op_lkm
+,sb.erikoul_suor_op_lkm
+
+,'etl: sa_suorat_hist_amk2a_opintopisteet' as source
+FROM sa.sa_suorat_hist_amk2a_opintopisteet s
+LEFT JOIN dw.d_amk d1 ON d1.amk_tunnus=s.amk_tunnus
+LEFT JOIN dw.d_koulutusala_2002 d2 ON d2.koodi=s.koulutusala_koodi
+LEFT JOIN dw.d_amk_koulutustyyppi d3 ON d3.koodi=s.koulutustyyppi_koodi
+LEFT JOIN dw.d_kalenteri as d4 ON d4.kalenteri_avain=cast(s.LATAUSAIKA as date)
+
+LEFT JOIN sa.sa_suorat_hist_amk2b_opintopisteet sb ON sb.korkeakoulu_tunnus=s.amk_tunnus and sb.vuosi=s.vuosi and sb.koulutusala_koodi=s.koulutusala_koodi
+WHERE s.koulutustyyppi_koodi<>'-1'
+
+UNION
 
 SELECT
  coalesce(d1.id,-1) as d_amk_id
@@ -69,17 +115,19 @@ SELECT
 ,sb.mamu_op_lkm
 ,sb.erikoul_suor_op_lkm
 
-,'etl: sa_suorat_hist_amk2*_opintopisteet' as source
-FROM sa.sa_suorat_hist_amk2a_opintopisteet s
-LEFT JOIN dw.d_amk d1 ON d1.amk_tunnus=s.amk_tunnus
-LEFT JOIN dw.d_koulutusala_2002 d2 ON d2.koodi=s.koulutusala_koodi
-LEFT JOIN dw.d_amk_koulutustyyppi d3 ON d3.koodi=s.koulutustyyppi_koodi
-LEFT JOIN dw.d_kalenteri as d4 ON d4.kalenteri_avain=cast(s.LATAUSAIKA as date)
+,'etl: sa_suorat_hist_amk2b_opintopisteet' as source
+FROM sa.sa_suorat_hist_amk2b_opintopisteet sb
+LEFT JOIN dw.d_amk d1 ON d1.amk_tunnus=sb.korkeakoulu_tunnus
+LEFT JOIN dw.d_koulutusala_2002 d2 ON d2.koodi=sb.koulutusala_koodi
+LEFT JOIN dw.d_amk_koulutustyyppi d3 ON d3.koodi='-1' --fixed!
+LEFT JOIN dw.d_kalenteri as d4 ON d4.kalenteri_avain=cast(sb.LATAUSAIKA as date)
 
--- TODO: should this be full outer join?
-LEFT JOIN sa.sa_suorat_hist_amk2b_opintopisteet sb ON sb.korkeakoulu_tunnus=s.amk_tunnus and sb.vuosi=s.vuosi and sb.koulutusala_koodi=s.koulutusala_koodi
+LEFT JOIN sa.sa_suorat_hist_amk2a_opintopisteet s ON sb.korkeakoulu_tunnus=s.amk_tunnus and sb.vuosi=s.vuosi and sb.koulutusala_koodi=s.koulutusala_koodi
+WHERE s.koulutustyyppi_koodi='-1'
 
-UNION
+
+
+UNION -- switch to VIRTA-OTP
 
 SELECT
  coalesce(d1.id,-1) as d_amk_id
@@ -130,8 +178,9 @@ WHERE s.koulutustyyppi<>'-1' --just so we don't get duplicates. -1's are picked 
 
 UNION
 
--- NB! get data from two tables (full outer join so either table with a row is sufficient)
---     this has some impact in getting dimension data, however. see below or-conditions
+-- NB! last get data from two tables so we rely on UNION to drop duplicates!
+-- first from sa_virta_otp_amkavoimenopintopisteetjarj
+--  then from sa_virta_otp_amkopintopiste (with koulutustyyppi='-1')
 SELECT
  coalesce((
   select d.id
@@ -139,13 +188,13 @@ SELECT
   from sa.sa_koodistot sk
   LEFT JOIN dw.d_amk d ON d.amk_tunnus=sk.nimi -- hack! koodisto as a map (using nimi for mapping!)
   where sk.koodisto='virtakorkeakoulutunnus'
-  and(sk.koodi=s.db or sk.koodi=a.db)
+  and(sk.koodi=s.db)
 ),-1) as d_amk_id
 ,-1 as d_koulutusala_2002_id
 ,coalesce((
   select d.id
   from dw.d_ohjauksenala d
-  where(d.ohjauksenala_koodi=s.koodi or d.ohjauksenala_koodi=a.koodi)
+  where(d.ohjauksenala_koodi=s.koodi)
 ),-1) as d_ohjauksenala_id
 ,coalesce((
   select d.id
@@ -154,9 +203,9 @@ SELECT
 ),-1) as d_amk_koulutustyyppi_id
 ,coalesce((select d.id
   from dw.d_kalenteri as d
-  where(d.kalenteri_avain=cast(s.loadtime as date) or d.kalenteri_avain=cast(a.loadtime as date))
+  where(d.kalenteri_avain=cast(s.loadtime as date))
 ),-1) as d_tilannepvm_id
-,coalesce(s.vuosi,a.vuosi) as vuosi
+,s.vuosi
 
 ,coalesce(a.perustutkinto,0) as kaikki_tutk_lkm
 ,coalesce(a.tkiToiminnanLaajuus,0) as tki_pisteet_lkm
@@ -217,16 +266,116 @@ SELECT
 
 ,s.source
 
--- NB! full outer join so either table is sufficient
 FROM
  sa.sa_virta_otp_amkavoimenopintopisteetjarj s
-FULL OUTER JOIN
+LEFT JOIN
  sa.sa_virta_otp_amkopintopiste a
   ON a.db=s.db and a.koodi=s.koodi and a.vuosi=s.vuosi
   and cast(a.loadtime as date)=cast(s.loadtime as date) -- as we're getting and collecting data every day make sure the data is from same original set
   and a.koulutustyyppi='-1' --just so we don't get too many duplicates. this should be ok but not verified from anywhere!
   and a.avoinKK=s.opintopisteet --just another sanity check...
-  -- nb! is this limit risky?
+
+WHERE s.oppilaitostyyppi='Ammattikorkeakoulut'
+--debug: where cast(s.loadtime as date)>=(select max(cast(loadtime as date)) from sa.sa_virta_otp_amkavoimenopintopisteetjarj)
+
+
+UNION --this will and should! drop duplicates
+
+SELECT
+ coalesce((
+  select d.id
+  -- hack! koodisto as a map (from "db" (kk-tunnus) to oppilaitosnumero)
+  from sa.sa_koodistot sk
+  LEFT JOIN dw.d_amk d ON d.amk_tunnus=sk.nimi -- hack! koodisto as a map (using nimi for mapping!)
+  where sk.koodisto='virtakorkeakoulutunnus'
+  and(sk.koodi=a.db)
+),-1) as d_amk_id
+,-1 as d_koulutusala_2002_id
+,coalesce((
+  select d.id
+  from dw.d_ohjauksenala d
+  where(d.ohjauksenala_koodi=a.koodi)
+),-1) as d_ohjauksenala_id
+,coalesce((
+  select d.id
+  from dw.d_amk_koulutustyyppi d
+  where d.koodi='-1' -- fixed!
+),-1) as d_amk_koulutustyyppi_id
+,coalesce((select d.id
+  from dw.d_kalenteri as d
+  where(d.kalenteri_avain=cast(a.loadtime as date))
+),-1) as d_tilannepvm_id
+,a.vuosi
+
+,coalesce(a.perustutkinto,0) as kaikki_tutk_lkm
+,coalesce(a.tkiToiminnanLaajuus,0) as tki_pisteet_lkm
+,coalesce(a.tkiHarjoittelunLaajuus,0) as tki_harj_pisteet_lkm
+,coalesce(a.tkiMuutLaajuus,0) as tki_muu_pisteet_lkm
+,coalesce(a.vieraskielinen,0) as vierkiel_pisteet_lkm
+,coalesce(a.hyvaksiluetut,0) as ulkom_hyv_op_lkm
+,coalesce(a.ulkomaaHarjoittelu,0) as ulkom_harj_op_lkm
+
+,coalesce(a.avoinKK,0) as avoinamk_op_lkm
+
+,coalesce(s.opintopisteet,0) as avoinamk_op_amk_lkm
+-- transponate measures (map values from multiple rows to one row)
+,coalesce((
+  select sum(sb.opintopisteet) from sa.sa_virta_otp_amkavoimenopintopisteetjarj sb
+  where sb.db=s.db and sb.koodi=s.koodi and sb.vuosi=s.vuosi
+  -- as we're getting and collecting data every day make sure the data is from same original set
+  and cast(sb.loadtime as date)=cast(s.loadtime as date)
+  and sb.oppilaitostyyppi='Yliopistot'
+),0) as avoinamk_op_yliop_lkm
+,coalesce((
+  select sum(sb.opintopisteet) from sa.sa_virta_otp_amkavoimenopintopisteetjarj sb
+  where sb.db=s.db and sb.koodi=s.koodi and sb.vuosi=s.vuosi
+  -- as we're getting and collecting data every day make sure the data is from same original set
+  and cast(sb.loadtime as date)=cast(s.loadtime as date)
+  and sb.oppilaitostyyppi='Kesäyliopistot'
+),0) as avoinamk_op_kesa_lkm
+,coalesce((
+  select sum(sb.opintopisteet) from sa.sa_virta_otp_amkavoimenopintopisteetjarj sb
+  where sb.db=s.db and sb.koodi=s.koodi and sb.vuosi=s.vuosi
+  -- as we're getting and collecting data every day make sure the data is from same original set
+  and cast(sb.loadtime as date)=cast(s.loadtime as date)
+  and sb.oppilaitostyyppi='Kansanopistot'
+),0) as avoinamk_op_kansan_lkm
+,coalesce((
+  select sum(sb.opintopisteet) from sa.sa_virta_otp_amkavoimenopintopisteetjarj sb
+  where sb.db=s.db and sb.koodi=s.koodi and sb.vuosi=s.vuosi
+  -- as we're getting and collecting data every day make sure the data is from same original set
+  and cast(sb.loadtime as date)=cast(s.loadtime as date)
+  and sb.oppilaitostyyppi='Kansalaisopistot'
+),0) as avoinamk_op_kansal_lkm
+,coalesce((
+  select sum(sb.opintopisteet) from sa.sa_virta_otp_amkavoimenopintopisteetjarj sb
+  where sb.db=s.db and sb.koodi=s.koodi and sb.vuosi=s.vuosi
+  -- as we're getting and collecting data every day make sure the data is from same original set
+  and cast(sb.loadtime as date)=cast(s.loadtime as date)
+  and sb.oppilaitostyyppi not in (
+   'Ammattikorkeakoulut','Yliopistot','Kesäyliopistot','Kansaopistot','Kansalaisopistot'
+  )
+),0) as avoinamk_op_muut_lkm
+
+-- NB! add from another source! (sa_virta_otp_amkopintopiste):
+,coalesce(a.kvVaihto,0) as kv_vaihtoopiskelijat_op_lkm
+,coalesce(a.joo,0) as kk_yhteistyo_op_lkm
+,coalesce(a.erillinenOO,0) as erilopintooik_op_lkm
+,coalesce(a.mValKo,0) as mamu_op_lkm
+,coalesce(a.erikoistumisopinnot,0) as erikoul_suor_op_lkm
+
+,a.source
+
+FROM
+ sa.sa_virta_otp_amkopintopiste a
+LEFT JOIN
+ sa.sa_virta_otp_amkavoimenopintopisteetjarj s
+  ON a.db=s.db and a.koodi=s.koodi and a.vuosi=s.vuosi
+  and cast(a.loadtime as date)=cast(s.loadtime as date) -- as we're getting and collecting data every day make sure the data is from same original set
+  and a.avoinKK=s.opintopisteet --just another sanity check...
+  -- nb! is this limit risky? not too bad, i think
   and s.oppilaitostyyppi='Ammattikorkeakoulut'
+
+WHERE a.koulutustyyppi='-1'
 
 --debug: where cast(s.loadtime as date)>=(select max(cast(loadtime as date)) from sa.sa_virta_otp_amkavoimenopintopisteetjarj)
