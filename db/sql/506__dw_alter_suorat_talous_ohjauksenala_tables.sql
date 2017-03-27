2926ee4921e3a47e263e89bd707012bc9ed51c82
hjaner <henrik.janer@csc.fi>
2017-03-27 10:10:55 +0300

    fix suorat talous ohjala script

--- db/sql/506__dw_alter_suorat_talous_ohjauksenala_tables.sql
+++ db/sql/506__dw_alter_suorat_talous_ohjauksenala_tables.sql
@@ -1,2 +1,10 @@
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_yo_talous_4_tutkimusmenot'
+  and COLUMN_NAME='ohjauksenala_koodi'
+) BEGIN
+
 ALTER TABLE sa.sa_suorat_yo_talous_4_tutkimusmenot ADD ohjauksenala_koodi int NULL;
 ALTER TABLE sa.sa_suorat_yo_talous_5_kustannustiedot ADD ohjauksenala varchar(255) NULL, ohjauksenala_koodi int NULL;
+
+END
