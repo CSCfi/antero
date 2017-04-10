eab5c76dd917f1080e89da8f22ed26674884b58c
hjaner <henrik.janer@csc.fi>
2017-04-10 09:01:15 +0300

    fix julkaisutyyppi_id

--- db/sql/535__dw_alter_table_f_yo_keksintoilmoitukset.sql
+++ db/sql/535__dw_alter_table_f_yo_keksintoilmoitukset.sql
@@ -1,8 +1,8 @@
 IF EXISTS (
   select * from INFORMATION_SCHEMA.COLUMNS
   where TABLE_SCHEMA='dw' and TABLE_NAME='f_yo_keksintoilmoitukset'
-  and COLUMN_NAME='julkaisutyyppi'
+  and COLUMN_NAME='julkaisutyyppi_id'
 )
 BEGIN
-ALTER TABLE dw.f_yo_keksintoilmoitukset drop column julkaisutyyppi;
+ALTER TABLE dw.f_yo_keksintoilmoitukset drop column julkaisutyyppi_id;
 END
