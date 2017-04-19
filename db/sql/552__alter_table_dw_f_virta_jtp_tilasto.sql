7d28056bbed62bfee7e2b592ec3d1f0b9566d8b2
pankalai <anssi.pankalainen@csc.fi>
2017-04-19 16:23:27 +0300

    edit julkaisutietopalvelu

---
+++ db/sql/552__alter_table_dw_f_virta_jtp_tilasto.sql
@@ -0,0 +1,48 @@
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_ohjauksenala_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_ohjauksenala_id int NOT NULL
+END
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_koulutusala95_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_koulutusala95_id int NOT NULL
+END
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_koulutusala02_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_koulutusala02_id int NOT NULL
+END
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_yhteisjulkaisuSHPKytkin_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_yhteisjulkaisuSHPKytkin_id int NOT NULL
+END
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_yhteisjulkaisuTutkimuslaitosKytkin_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_yhteisjulkaisuTutkimuslaitosKytkin_id int NOT NULL
+END
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_yhteisjulkaisuMuuKytkin_id'
+) BEGIN
+  ALTER TABLE dw.f_virta_jtp_tilasto ADD d_yhteisjulkaisuMuuKytkin_id int NOT NULL
+END
