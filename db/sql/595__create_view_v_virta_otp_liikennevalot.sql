971f629a550c10a77fcdaa7bad887b3e3d6a9389
Fredrik Finnberg <fredrik.finnberg@csc.fi>
2017-06-08 12:06:27 +0300

    Liikennevalo

--- db/sql/595__create_view_v_virta_otp_liikennevalot.sql
+++ db/sql/595__create_view_v_virta_otp_liikennevalot.sql
@@ -1,3 +1,35 @@
 IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'dw.v_virta_otp_liikennevalot'))
 EXEC dbo.sp_executesql @statement = N'create view dw.v_virta_otp_liikennevalot as select 1 as a'
 GO
+
+ALTER VIEW dw.[v_virta_otp_liikennevalot] AS
+
+SELECT [id]
+      ,[ensisijaisuusPuuttuu]
+      ,[kk]
+      ,[lasnaolevienTutkintoopiskelijoidenLkm]
+      ,[liikkuvuusjaksojenLkm]
+      ,[lukukausiilmoittautumisiaVuosilleLkm]
+      ,[luoja]
+      ,[luontipaivamaara]
+      ,[opintosuoritukseltaPuuttuuNimi]
+      ,[opintosuoritukseltaPuuttuuArvosana]
+      ,[opintosuoritukseltaPuuttuuKoulutusala]
+      ,[opintosuorituksenSisaltyvyys]
+      ,[opintosuoritusIlmanOpintooikeuttaAmk]
+      ,[opiskelijallaEiOleHetua]
+      ,[opiskelijallaEiOleOpiskeluoikeuttaLukukausiilmoittautumistaTaiOpintosuoritusta]
+      ,[opiskelijoidenLkm]
+      ,[opiskeluoikeudenOpiskeluoikeusjaksoEiMuodostaEheaaAikajaksoa]
+      ,[opiskeluoikeudenTilaEiMuodostaEheaaAikajaksoa]
+      ,[opiskeluoikeuksienEnsisijaisuuksissaPaallekkaisyyksia]
+      ,[opiskeluoikeusjaksoltaPuuttuuKoulutuskoodi]
+      ,[oppilaitostunnus]
+      ,[tietovarannonTietojenPaivitysLainMukaisellaAikataululla]
+      ,[tutkintoIlmanKoulutuskoodia]
+      ,[tutkintoopiskelijoidenLkm]
+      ,[loadtime]
+      ,[source]
+      ,[username]
+  FROM ANTERO.[sa].[sa_virta_otp_liikennevalot]
+GO
