7d1df4221cfbf1a486df5f7aa4518fdebda053d4
Kati Lehtosaari <kati.lehtosaari@csc.fi>
2017-07-06 10:35:03 +0300

    proseduurin luontilause vain f_virta_otp_opintopiste_pvm_yo

--- db/sql/658__create_procedure_f_virta_otp_opintopiste_pvm_yo.sql
+++ db/sql/658__create_procedure_f_virta_otp_opintopiste_pvm_yo.sql
@@ -1,51 +1,7 @@
 ﻿if not exists (select * from sys.objects where object_id = object_id('dw.p_lataa_f_virta_otp_opintopiste_pvm_yo') and type in ('p', 'pc'))
 begin
-exec dbo.sp_executesql @statement = N'create procedure dw.p_lataa_f_virta_otp_opintopiste_pvm_yo as' 
-end
+exec dbo.sp_executesql @statement = N'create procedure dw.p_lataa_f_virta_otp_opintopiste_pvm_yo as'end
 go
 
-ALTER PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_yo AS
-
-TRUNCATE TABLE dw.f_virta_otp_opintopiste_pvm_yo
-
-INSERT INTO dw.f_virta_otp_opintopiste_pvm_yo (
-  tilastovuosi,
-  suorituspaiva,
-  d_ohjauksenala_id,
-  d_yo_id,
-  lukumaara_int,
-  perustutk_opiskelijat_lkm,
-  vierask_suoritukset_lkm,
-  kansainv_vaihto_lkm,
-  kk_yhteistyo_lkm,
-  avoin_kk_lkm,
-  erillinen_opinto_oikeus_lkm,
-  ulkomailta_hyv_luet_lkm,
-  erillinen_opinto_opettaja_lkm,
-  ulkomaaharjoittelu_lkm,
-  erikoistumiskoulutus_lkm,
-
-  source
-  
-)
-SELECT
-  sa.vuosi,
-  convert(date,dateadd(s, convert(bigint, sa.suorituspaiva) / 1000, convert(datetime, '1-1-1970'))),
-  coalesce(d1.id,-1) AS d_ohjauksenala_id,
-  coalesce(d2.id,-1) AS d_yo_id,
-  sa.lukumaara,
-  sa.perustutkOpiskelijat,
-  sa.vieraskSuoritukset,
-  sa.kansainvVaihto,
-  sa.kkYhteistyo,
-  sa.avoinKK,
-  sa.erillinenOpintoOikeus,
-  sa.ulkomailtaHyvLuet,
-  sa.erillinenOpintoOpettaja,
-  sa.ulkomaaharjoittelu,
-  sa.erikoistumiskoulutus, 
-  sa.source
-
-FROM sa.sa_virta_otp_opintopistepvmyo as sa
-LEFT JOIN dw.d_ohjauksenala d1 ON d1.ohjauksenala_koodi=sa.koodi
-LEFT JOIN dw.d_yo d2 ON d2.yo_tunnus = sa.oppilaitostunnus
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dw.p_lataa_f_virta_otp_opintopiste_pvm_yo') AND type in (N'P', N'PC'))
+BEGIN EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_yo AS' END
