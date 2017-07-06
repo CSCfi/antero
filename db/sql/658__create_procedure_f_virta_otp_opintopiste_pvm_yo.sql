19f54005ed9660b92560a2084c9c1b4f495ed7b8
Kati Lehtosaari <kati.lehtosaari@csc.fi>
2017-07-06 10:39:53 +0300

    poistettu tuplakoodi

--- db/sql/658__create_procedure_f_virta_otp_opintopiste_pvm_yo.sql
+++ db/sql/658__create_procedure_f_virta_otp_opintopiste_pvm_yo.sql
@@ -1,7 +1,2 @@
-﻿if not exists (select * from sys.objects where object_id = object_id('dw.p_lataa_f_virta_otp_opintopiste_pvm_yo') and type in ('p', 'pc'))
-begin
-exec dbo.sp_executesql @statement = N'create procedure dw.p_lataa_f_virta_otp_opintopiste_pvm_yo as'end
-go
-
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dw.p_lataa_f_virta_otp_opintopiste_pvm_yo') AND type in (N'P', N'PC'))
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dw.p_lataa_f_virta_otp_opintopiste_pvm_yo') AND type in (N'P', N'PC'))
 BEGIN EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_yo AS' END
