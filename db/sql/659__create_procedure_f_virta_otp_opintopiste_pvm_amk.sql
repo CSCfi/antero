116dcd6ff83dc7fe14adfc4247ce3480665072d8
Kati Lehtosaari <kati.lehtosaari@csc.fi>
2017-07-06 12:55:48 +0300

    proseduuri f_virta_otp_opintopiste_pvm_amk

--- db/sql/659__create_procedure_f_virta_otp_opintopiste_pvm_amk.sql
+++ db/sql/659__create_procedure_f_virta_otp_opintopiste_pvm_amk.sql
@@ -3,21 +3,23 @@ IF NOT EXISTS (
   from INFORMATION_SCHEMA.ROUTINES
   where ROUTINE_TYPE='PROCEDURE'
   and ROUTINE_SCHEMA='dw'
-  and ROUTINE_NAME='p_lataa_f_virta_otp_opintopiste_pvm_yo'
+  and ROUTINE_NAME='p_lataa_f_virta_otp_opintopiste_pvm_amk'
 ) BEGIN
  
-exec('CREATE PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_yo AS') END
+exec('CREATE PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_amk AS') END
 GO
 
-ALTER PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_yo AS 
 
-TRUNCATE TABLE dw.f_virta_otp_opintopiste_pvm_yo;
+ALTER PROCEDURE dw.p_lataa_f_virta_otp_opintopiste_pvm_amk AS 
 
-INSERT INTO dw.f_virta_otp_opintopiste_pvm_yo (
+TRUNCATE TABLE dw.f_virta_otp_opintopiste_pvm_amk;
+
+INSERT INTO dw.f_virta_otp_opintopiste_pvm_amk (
   tilastovuosi,
   suorituspaiva,
   d_ohjauksenala_id,
-  d_yo_id,
+  d_amk_koulutustyyppi_id,
+  d_amk_id,
   lukumaara_int,
   perustutk_opiskelijat_lkm,
   vierask_suoritukset_lkm,
@@ -26,9 +28,13 @@ INSERT INTO dw.f_virta_otp_opintopiste_pvm_yo (
   avoin_kk_lkm,
   erillinen_opinto_oikeus_lkm,
   ulkomailta_hyv_luet_lkm,
-  erillinen_opinto_opettaja_lkm,
+  maahanm_val_koulutus_lkm,
+  TKI_toiminnan_laajuus_lkm,
+  TKI_muut_laajuus_lkm,
+  TKI_harjoittelun_laajuus_lkm,
   ulkomaaharjoittelu_lkm,
   erikoistumiskoulutus_lkm,
+
   source
   
 )
@@ -36,7 +42,8 @@ SELECT
   sa.vuosi,
   convert(date,dateadd(s, convert(bigint, sa.suorituspaiva) / 1000, convert(datetime, '1-1-1970'))),
   coalesce(d1.id,-1) AS d_ohjauksenala_id,
-  coalesce(d2.id,-1) AS d_yo_id,
+  coalesce(d2.id,-1) AS d_amk_koulutustyyppi_id,
+  coalesce(d3.id,-1) AS d_amk_id,
   sa.lukumaara,
   sa.perustutkOpiskelijat,
   sa.vieraskSuoritukset,
@@ -45,11 +52,15 @@ SELECT
   sa.avoinKK,
   sa.erillinenOpintoOikeus,
   sa.ulkomailtaHyvLuet,
-  sa.erillinenOpintoOpettaja,
+  sa.maahanmValKoulutus,
+  sa.tkiToiminnanLaajuus,
+  sa.tkiMuutLaajuus,
+  sa.tkiHarjoittelunLaajuus,
   sa.ulkomaaharjoittelu,
   sa.erikoistumiskoulutus, 
   sa.source
 
-FROM sa.sa_virta_otp_opintopistepvmyo as sa
+FROM sa.sa_virta_otp_opintopistepvmamk as sa
 LEFT JOIN dw.d_ohjauksenala d1 ON d1.ohjauksenala_koodi=sa.koodi
-LEFT JOIN dw.d_yo d2 ON d2.yo_tunnus = sa.oppilaitostunnus
+LEFT JOIN dw.d_amk_koulutustyyppi d2 ON d2.selite_fi = sa.koulutustyyppi
+LEFT JOIN dw.d_amk d3 ON d3.amk_tunnus = sa.oppilaitostunnus
