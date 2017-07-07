41ed26b68d2d6cc0f479b6ee6ff6b538d363b16a
pankalai <anssi.pankalainen@csc.fi>
2017-07-07 13:57:59 +0300

    check existence

--- db/sql/664__create_procedure_vipunentk_dw_f_elakkeelle_siirtyneet_ja_kuolleet.sql
+++ db/sql/664__create_procedure_vipunentk_dw_f_elakkeelle_siirtyneet_ja_kuolleet.sql
@@ -1,11 +1,19 @@
 USE [VipunenTK_DW]
 GO
-/****** Object:  StoredProcedure [dbo].[p_lataa_MKH_f_K3_16]    Script Date: 7.7.2017 13:44:41 ******/
+/****** Object:  StoredProcedure [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18]    Script Date: 7.7.2017 13:56:49 ******/
+DROP PROCEDURE IF EXISTS [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18]
+GO
+/****** Object:  StoredProcedure [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18]    Script Date: 7.7.2017 13:56:49 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18] AS
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18] AS' 
+END
+GO
+ALTER PROCEDURE [dbo].[p_lataa_f_elakkeelle_siirtyneet_ja_kuolleet_R_4_17_4_18] AS
 
 
 
@@ -13,3 +21,5 @@ Delete from VipunenTK.dbo.f_elakkeelle_siirtyneet_ja_kuolleet
 Insert into VipunenTK.dbo.f_elakkeelle_siirtyneet_ja_kuolleet
 	Select * from [VipunenTK_DW].[dbo].v_f_elakkeelle_siirtyneet_ja_kuolleet
 
+
+GO
