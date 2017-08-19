bed8865fd389caef6fca9fcc0cc47f89dc7b4620
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-08-19 14:35:47 +0300

    Fixes related to a problem caused by a very different problem.

--- db/sql/715__alter_table_dw_f_virta_jtp_tilasto.sql
+++ db/sql/715__alter_table_dw_f_virta_jtp_tilasto.sql
@@ -1,7 +1,15 @@
-USE [ANTERO]
-GO
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_julkaisufoorumitaso_vanha_id'
+) BEGIN
+ALTER TABLE dw.f_virta_jtp_tilasto ADD d_julkaisufoorumitaso_vanha_id int NULL
+END
 
-ALTER TABLE [dw].[f_virta_jtp_tilasto] ADD [d_julkaisufoorumitaso_vanha_id] [int] NULL,
-[d_julkaisufoorumitaso_uusi_id] [int] NULL
-
-GO
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='dw' and TABLE_NAME='f_virta_jtp_tilasto'
+  and COLUMN_NAME='d_julkaisufoorumitaso_uusi_id'
+) BEGIN
+ALTER TABLE dw.f_virta_jtp_tilasto ADD d_julkaisufoorumitaso_uusi_id int NULL
+END
