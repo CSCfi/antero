7d6b9112d8f3a11077376918ad41407ca58e259d
hjaner <henrik.janer@csc.fi>
2017-09-06 08:37:14 +0300

    edit db\sql\732__alter_table_f_plus_sa_yo_opettaja_ja_tutkijavierailut.sql

--- db/sql/732__alter_table_f_plus_sa_yo_opettaja_ja_tutkijavierailut.sql
+++ db/sql/732__alter_table_f_plus_sa_yo_opettaja_ja_tutkijavierailut.sql
@@ -7,5 +7,5 @@ and COLUMN_NAME='vierailut_ulkomaille_lkm'
 ALTER TABLE dw.f_yo_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomaille_lkm decimal(10,4);
 ALTER TABLE dw.f_yo_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);
 ALTER TABLE sa.sa_suorat_yo7_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);
-ALTER TABLE sa.sa_suorat_yo7_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);  
+ALTER TABLE sa.sa_suorat_yo7_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomaille_lkm decimal(10,4);  
 END
