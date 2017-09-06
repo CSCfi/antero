b3788247802d4a06c22b3d2a72146d054d8687c8
hjaner <henrik.janer@csc.fi>
2017-09-06 08:15:32 +0300

    add db\sql\732__alter_table_f_plus_sa_yo_opettaja_ja_tutkijavierailut.sql

---
+++ db/sql/732__alter_table_f_plus_sa_yo_opettaja_ja_tutkijavierailut.sql
@@ -0,0 +1,11 @@
+IF EXISTS (
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_SCHEMA='dw' and TABLE_NAME='f_yo_opettaja_ja_tutkijavierailut'
+and COLUMN_NAME='vierailut_ulkomaille_lkm'
+) BEGIN
+
+ALTER TABLE dw.f_yo_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomaille_lkm decimal(10,4);
+ALTER TABLE dw.f_yo_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);
+ALTER TABLE sa.sa_suorat_yo7_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);
+ALTER TABLE sa.sa_suorat_yo7_opettaja_ja_tutkijavierailut ALTER COLUMN vierailut_ulkomailta_lkm decimal(10,4);  
+END
