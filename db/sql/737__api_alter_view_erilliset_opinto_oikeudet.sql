547c63c8aa10452100ff8a6c6089d481735928aa
villehoo <ville.hamalainen@csc.fi>
2017-09-11 09:11:37 +0300

    add defaultorder column

---
+++ db/sql/737__api_alter_view_erilliset_opinto_oikeudet.sql
@@ -0,0 +1,81 @@
+IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'api.erilliset_opinto_oikeudet'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE VIEW api.erilliset_opinto_oikeudet AS
+SELECT 1 AS a
+'
+GO
+
+ALTER view api.erilliset_opinto_oikeudet as
+
+
+select
+
+[Tilastovuosi] = vuosi
+
+,Koulutusala = 'Tuntematon'
+,[OKM ohjauksen ala] = 'Tuntematon'
+
+,[Yliopisto] = d6.yo_nimi_fi
+
+--mittarit
+,f.suorittaneiden_lkm
+,f.opettajakoulutus_suorittaneiden_lkm
+,erillisilla_oik_op_lkm = null
+,erillisilla_oik_opkoul_op_lkm = null
+
+--koodit
+,[Koodit Yliopisto] = d6.yo_tunnus
+,[Koodit Koulutusala] = 99
+,[Koodit OKM ohjauksen ala] = 99
+
+
+--oletusjärjestys sorttausta varten, 1000000000+ lajittelee alikyselyn tulokset
+
+,1000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d6.id ASC) as defaultorder
+
+
+from [dw].[f_yo_erillisella_opinto_oikeudella_opiskelevat] f
+join dw.d_yo d6 on d6.id=f.d_yliopisto_id
+
+union all
+
+select
+
+[Tilastovuosi] = f.vuosi
+
+,Koulutusala = d1.opintoala95_nimi_fi
+,[OKM ohjauksen ala] = d2.ohjauksenala_nimi_fi
+
+,[Yliopisto] = case d6.yo_nimi_fi when 'Turun yliopisto (-2009)' then 'Turun yliopisto' else d6.yo_nimi_fi end
+
+--mittarit
+,null
+,null
+,[erillisilla_oik_op_lkm]
+,[erillisilla_oik_opkoul_op_lkm]
+
+--koodit
+,[Koodit Yliopisto] = d6.yo_tunnus
+,[Koodit Koulutusala] = case d1.opintoala95_koodi when -1 then 99 else d1.opintoala95_koodi end
+,[Koodit OKM ohjauksen ala] = case d2.ohjauksenala_koodi when -1 then 99 else d2.ohjauksenala_koodi end
+
+--oletusjärjestys sorttausta varten, 2000000000+ lajittelee alikyselyn tulokset
+
+,2000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC, d2.id ASC, d6.id ASC, d7.id ASC) as defaultorder
+
+
+from [dw].[f_yo_opintopisteet] f
+join dw.d_opintoala95 d1 on d1.id=f.d_opintoala95_id
+join dw.d_ohjauksenala d2 on d2.id=f.d_ohjauksenala_id
+join dw.d_yo d6 on d6.id=f.d_yo_id
+join dw.d_kalenteri d7 on d7.id=f.d_tilannepvm_id
+
+where d7.paivays like '%-03-02'
+
+
+GO
+
+
+/* revert
+drop view api.erilliset_opinto_oikeudet
+--*/
---
+++ db/sql/738__api_alter_view_harjoittelukoulut.sql
@@ -0,0 +1,150 @@
+IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'api.harjoittelukoulut'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE VIEW api.harjoittelukoulut AS
+SELECT 1 AS a
+'
+GO
+
+ALTER view api.harjoittelukoulut as
+
+
+select
+[Tilastovuosi] = vuosi
+,[Yliopisto] = d1.yo_nimi_fi
+
+,[Henkilöstöryhmä] = null
+
+,[Koulutusala] = 'Tieto puuttuu'
+,[Ohjauksen ala] = 'Tieto puuttuu'
+
+--mittarit
+,f.harjoittelukoulun_oppilaat_esiopetus
+,f.harjoittelukoulun_oppilaat_perusopetuksen_vuodet_1_6
+,f.harjoittelukoulun_oppilaat_perusopetuksen_vuodet_7_9
+,f.harjoittelukoulun_oppilaat_lukio
+,f.harjoittelukoulun_oppilaat_yhteensa
+,f.harjoittelukoulun_opettajankoulutustehtavien_kokonaisviikkotunnit
+,f.harjoittelukoulun_kokonaistuntimaara
+
+,[opintopisteet_lastentarha] = null
+,[opintopisteet_luokanopettaja] = null
+,[opintopisteet_aineenopettaja] = null
+,[opintopisteet_erityisopettaja] = null
+,[opintopisteet_opinto_ohjaaja] = null
+,[opintopisteet_aikuiskoulutus] = null
+,[opintopisteet_muut] = null
+
+,henkilotyovuosi = null
+
+--koodit
+,[Koodit Yliopisto] = d1.yo_tunnus
+,[Koodit Koulutusala] = 99
+,[Koodit Ohjauksen ala] = 99
+,[Koodit Henkilöstöryhmä] = 99
+
+--oletusjärjestys sorttausta varten, 1000000000+ lajittelee alikyselyn tulokset
+
+,1000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC) as defaultorder
+
+
+from [dw].[f_yo_harjoittelukoulut] f
+join dw.d_yo d1 on d1.id=f.d_yliopisto_id
+
+UNION ALL
+
+select
+[Tilastovuosi] = vuosi
+,[Yliopisto] = d1.yo_nimi_fi
+
+,null
+
+,[Koulutusala] = 'Tieto puuttuu'
+,[Ohjauksen ala] = 'Tieto puuttuu'
+
+--mittarit
+,null
+,null
+,null
+,null
+,null
+,null
+,null
+
+,f.suoritetut_opintopisteet_lastentarhanopettajat
+,f.suoritetut_opintopisteet_luokanopettajaharjoittelu
+,f.suoritetut_opintopisteet_aineenopettajaharjoittelu
+,f.suoritetut_opintopisteet_erityisopettajat
+,f.suoritetut_opintopisteet_opinto_ohjaajat
+,f.suoritetut_opintopisteet_aikuiskoulutus_harjoittelu
+,f.suoritetut_opintopisteet_muut_opettajankoulutusryhmat
+
+,null
+
+--koodit
+,[Koodit Yliopisto] = d1.yo_tunnus
+,[Koodit Koulutusala] = 99
+,[Koodit Ohjauksen ala] = 99
+,[Koodit Henkilöstöryhmä] = 99
+
+--oletusjärjestys sorttausta varten, 2000000000+ lajittelee alikyselyn tulokset
+
+,2000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC) as defaultorder
+
+
+from [dw].[f_yo_harjoittelukoulujen_opintopisteet] f
+join dw.d_yo d1 on d1.id=f.d_yliopisto_id
+
+UNION ALL
+
+select
+[Tilastovuosi] = vuosi
+,[Yliopisto] = d1.yo_nimi_fi
+
+,[Henkilöstöryhmä] = d3.selite_fi
+
+,[Koulutusala] = d4.selite_fi
+,[Ohjauksen ala] = d5.ohjauksenala_nimi_fi
+--mittarit
+,null
+,null
+,null
+,null
+,null
+,null
+,null
+
+,null
+,null
+,null
+,null
+,null
+,null
+,null
+
+,henkilotyovuosi
+
+--koodit
+,[Koodit Yliopisto] = d1.yo_tunnus
+,[Koodit Koulutusala] = case d4.koodi when -1 then 99 else d4.koodi end
+,[Koodit Ohjauksen ala] = case d5.ohjauksenala_koodi when -1 then 99 else d5.ohjauksenala_koodi end
+,[Koodit Henkilöstöryhmä] = d3.koodi
+
+--oletusjärjestys sorttausta varten, 3000000000+ lajittelee alikyselyn tulokset
+
+,3000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC, d2.id ASC, d3.id ASC, d4.id  ASC, d5.id ASC) as defaultorder
+
+
+from [dw].[f_yo_henkilon_tyo] f
+join dw.d_yo d1 on d1.id=f.d_yliopisto_id
+join dw.d_yo_tehtavanjaottelu d2 on d2.id=f.d_tehtavanjaottelu_id
+join dw.d_yo_harjoittelukoulujen_henkilostoryhma d3 on d3.id=f.d_harjoittelukoulujen_henkilostoryhma_id
+join dw.d_koulutusala_1995 d4 on d4.id=f.d_koulutusala_id
+join dw.d_ohjauksenala d5 on d5.id=f.d_ohjauksenala_id
+where d2.selite_fi='Harjoittelukoulujen henkilöstö'
+
+
+GO
+
+/* revert
+drop view api.harjoittelukoulut
+--*/
