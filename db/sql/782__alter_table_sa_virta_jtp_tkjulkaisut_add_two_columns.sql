824e66654cf8e408b160683af4dd69afdea53110
oeskola <olli.eskola@csc.fi>
2017-09-20 11:52:46 +0300

    added two columns

--- db/sql/782__alter_table_sa_virta_jtp_tkjulkaisut_add_two_columns.sql
+++ db/sql/782__alter_table_sa_virta_jtp_tkjulkaisut_add_two_columns.sql
@@ -11,5 +11,5 @@ IF EXISTS (
   where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
   and COLUMN_NAME='jufoLuokkaUusi' and DATA_TYPE='nvarchar'
 )  BEGIN
-  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN jufoLuokkaUusi NVARCHAR(5)) NULL
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN jufoLuokkaUusi NVARCHAR(5) NULL
 END
