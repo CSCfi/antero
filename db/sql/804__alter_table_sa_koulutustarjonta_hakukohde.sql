25fc6ab9fa001d324c02dac9b7ce4224deabb1ee
villehoo <ville.hamalainen@csc.fi>
2017-10-03 11:13:54 +0300

    add hakukohdeKoulutusOid

--- db/sql/804__alter_table_sa_koulutustarjonta_hakukohde.sql
+++ db/sql/804__alter_table_sa_koulutustarjonta_hakukohde.sql
@@ -1,5 +1,5 @@
 IF NOT EXISTS (
-  select * from INFORMATION_SCHEMA.TABLES
+select * from INFORMATION_SCHEMA.COLUMNS
   where TABLE_SCHEMA='sa' and TABLE_NAME='koulutustarjonta_hakukohde'
   and COLUMN_NAME='hakukohdeKoulutusOids'
 ) BEGIN
