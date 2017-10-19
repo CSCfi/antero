ac9cccffdce0777fccf241715d5fa5446e38f318
villehoo <ville.hamalainen@csc.fi>
2017-10-19 09:38:15 +0300

    2nd try jarjestyksia d_amk_%-tauluihin

--- db/sql/858__alter_tables_dw_d_amk_toimipisteen_toimipaikka.sql
+++ db/sql/858__alter_tables_dw_d_amk_toimipisteen_toimipaikka.sql
@@ -6,6 +6,7 @@ IF NOT EXISTS (
 
 BEGIN
 	ALTER TABLE dw.d_amk_toimipisteen_toimipaikka ADD jarjestys_koodi_amk AS case when koodi_amk = -1 then '99999' else cast(koodi_amk as varchar(10))
+	END
 END
 
 
@@ -15,6 +16,7 @@ IF NOT EXISTS (
 
 BEGIN
 	ALTER TABLE dw.d_amk_toimipisteen_toimipaikka ADD jarjestys_koodi_toimipaikka AS case when koodi_toimipaikka = -1 then '99999' else cast(koodi_toimipaikka as varchar(10))
+	END
 END
 
 
@@ -24,4 +26,5 @@ IF NOT EXISTS (
 
 BEGIN
 	ALTER TABLE dw.d_amk_toimipisteen_toimipaikka ADD jarjestys_koodi_toimipiste AS case when koodi_toimipiste = -1 then '99999' else cast(koodi_toimipiste as varchar(10))
+	END
 END
