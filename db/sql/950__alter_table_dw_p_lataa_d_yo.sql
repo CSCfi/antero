475ac15174433e77f8906f241464a6362fbfddd3
villehoo <ville.hamalainen@csc.fi>
2017-10-23 11:36:04 +0300

    add jarjestys to dimensons and procedures

--- db/sql/950__alter_table_dw_p_lataa_d_yo.sql
+++ db/sql/950__alter_table_dw_p_lataa_d_yo.sql
@@ -28,11 +28,11 @@ when not matched then
            ,source)
   values (src.koodi, src.nimi, src.nimi_sv, src.nimi_en, src.source);
 
-IF NOT EXISTS (
-	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_toiminto' AND COLUMN_NAME='jarjestys_toiminto_koodi')
+	IF NOT EXISTS (
+		SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+		WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_yo' AND COLUMN_NAME='jarjestys_yo_koodi')
 
-BEGIN
-	ALTER TABLE dw.d_toiminto ADD jarjestys_toiminto_koodi AS case when toiminto_koodi = '-1' then 'ZZZZZ' else cast(toiminto_koodi as varchar(10))
+	BEGIN
+		ALTER TABLE dw.d_yo ADD jarjestys_yo_koodi AS case when yo_tunnus = '-1' then 'ZZZZZ' else cast(yo_tunnus as varchar(10))
+		END
 	END
-END
