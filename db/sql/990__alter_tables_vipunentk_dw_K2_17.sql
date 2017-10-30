434bfc0ebd2944ac98f790087d2d075a8c90104e
pankalai <anssi.pankalainen@csc.fi>
2017-10-30 16:55:07 +0200

    fixed alter table

--- db/sql/990__alter_tables_vipunentk_dw_K2_17.sql
+++ db/sql/990__alter_tables_vipunentk_dw_K2_17.sql
@@ -2,7 +2,7 @@ USE [VipunenTK]
 GO
 IF NOT EXISTS (
 	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='[f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk]' AND COLUMN_NAME='hallinnonala2_id')
+	WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk' AND COLUMN_NAME='hallinnonala2_id')
 
 BEGIN
 ALTER TABLE [dbo].[f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk]
@@ -12,7 +12,7 @@ GO
 USE [VipunenTK_DW]
 IF NOT EXISTS (
 	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='[f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk]' AND COLUMN_NAME='hallinnonala2')
+	WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk' AND COLUMN_NAME='hallinnonala2')
 
 BEGIN
 ALTER TABLE [dbo].[f_K2_17_ulkomaalaistaustaiset_opisk_ja_tutk]
