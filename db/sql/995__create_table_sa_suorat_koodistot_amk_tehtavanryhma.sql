e3788e482cb9ecd1fca85f56e54fb09d910abe68
villehoo <ville.hamalainen@csc.fi>
2017-10-31 10:10:44 +0200

    uusi suorien koodistotaulu

--- db/sql/995__create_table_sa_suorat_koodistot_amk_tehtavanryhma.sql
+++ db/sql/995__create_table_sa_suorat_koodistot_amk_tehtavanryhma.sql
@@ -2,7 +2,7 @@
 --  create table sa_suorat_koodistot_amk_tehtavanryhma
 --
 
-
+USE ANTERO
 IF NOT EXISTS
 	(SELECT * FROM INFORMATION_SCHEMA.TABLES
 	WHERE TABLE_SCHEMA='dw'
@@ -10,11 +10,12 @@ IF NOT EXISTS
 
 BEGIN
 CREATE TABLE [sa].[sa_suorat_koodistot_amk_tehtavanryhma](
-      [koodi]
-      ,[selite_FI]
-      ,[selite_SV]
-      ,[selite_EN]
-      ,[lataaja]
-      ,[latausaika])
-
+      [koodi] varchar(20)
+      ,[selite_FI] varchar(100)
+      ,[selite_SV] varchar(100)
+      ,[selite_EN] varchar(100)
+      ,[lataaja] varchar(100)
+      ,[latausaika] varchar(100)
+			,[source] varchar(100)
+		)
 END
