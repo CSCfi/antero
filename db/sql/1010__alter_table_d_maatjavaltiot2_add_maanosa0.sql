f56ed10a490cb3b9029c7d60bc9ab7b659ae33b1
villehoo <ville.hamalainen@csc.fi>
2017-11-17 13:45:56 +0200

    add maanosa0 (prod) column to d_maatjavaltiot2

--- db/sql/1010__alter_table_d_maatjavaltiot2_add_maanosa0.sql
+++ db/sql/1010__alter_table_d_maatjavaltiot2_add_maanosa0.sql
@@ -11,12 +11,3 @@ ALTER TABLE dw.d_maatjavaltiot2 ADD
   maanosa0_sv nvarchar(200) NULL,
   maanosa0_en nvarchar(200) NULL
 END
-
-IF NOT EXISTS (
-  	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-  	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_maatjavaltiot2' AND COLUMN_NAME='jarjestys_maanosa0_koodi')
-
-  BEGIN
-  	ALTER TABLE dw.d_maatjavaltiot2 ADD jarjestys_maanosa0_koodi AS case when maanosa0_koodi = '-1' then 'ZZZZZ' else cast(maanosa0_koodi as varchar(10))
-    END
-  END
