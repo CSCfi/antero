b270b61156716895a5248a2403a35e2ef69a112e
pankalai <anssi.pankalainen@csc.fi>
2017-12-14 14:26:28 +0200

    fix

--- db/sql/1071__alter_column_haku_ja_valinta_korkea_aste.sql
+++ db/sql/1071__alter_column_haku_ja_valinta_korkea_aste.sql
@@ -3,13 +3,17 @@ USE [ANTERO]
 GO
 
 
-IF EXISTS(SELECT * FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '[dw].[f_haku_ja_valinta_hakeneet_korkea_aste]' AND COLUMN_NAME = 'd_maatjavaltiot_pohjakoulutuksen_maa_kk') 
+IF EXISTS (
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='f_haku_ja_valinta_hakeneet_korkea_aste' AND COLUMN_NAME='d_maatjavaltiot_pohjakoulutuksen_maa_kk')
 BEGIN
 ALTER TABLE [dw].[f_haku_ja_valinta_hakeneet_korkea_aste]
 DROP COLUMN d_maatjavaltiot_pohjakoulutuksen_maa_kk;
 END
 GO
-IF NOT EXISTS(SELECT * FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '[dw].[f_haku_ja_valinta_hakeneet_korkea_aste]' AND COLUMN_NAME = 'd_kytkin_pohjakoulutuskk_ulkomaat_id') 
+IF NOT EXISTS (
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='f_haku_ja_valinta_hakeneet_korkea_aste' AND COLUMN_NAME='d_kytkin_pohjakoulutuskk_ulkomaat_id')
 BEGIN
 ALTER TABLE [dw].[f_haku_ja_valinta_hakeneet_korkea_aste]
 ADD d_kytkin_pohjakoulutuskk_ulkomaat_id INT NULL;
