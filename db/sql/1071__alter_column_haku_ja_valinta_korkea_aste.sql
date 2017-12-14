4807fe06a0be5a3eeae34b88eeb07849b9010d83
pankalai <anssi.pankalainen@csc.fi>
2017-12-14 14:19:30 +0200

    fix alter table

--- db/sql/1071__alter_column_haku_ja_valinta_korkea_aste.sql
+++ db/sql/1071__alter_column_haku_ja_valinta_korkea_aste.sql
@@ -2,4 +2,15 @@ USE [ANTERO]
 
 GO
 
-sp_rename '[dw].[f_haku_ja_valinta_hakeneet_korkea_aste].d_maatjavaltiot_pohjakoulutuksen_maa_kk', 'd_kytkin_pohjakoulutuskk_ulkomaat_id', 'COLUMN';
+
+IF EXISTS(SELECT * FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '[dw].[f_haku_ja_valinta_hakeneet_korkea_aste]' AND COLUMN_NAME = 'd_maatjavaltiot_pohjakoulutuksen_maa_kk') 
+BEGIN
+ALTER TABLE [dw].[f_haku_ja_valinta_hakeneet_korkea_aste]
+DROP COLUMN d_maatjavaltiot_pohjakoulutuksen_maa_kk;
+END
+GO
+IF NOT EXISTS(SELECT * FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '[dw].[f_haku_ja_valinta_hakeneet_korkea_aste]' AND COLUMN_NAME = 'd_kytkin_pohjakoulutuskk_ulkomaat_id') 
+BEGIN
+ALTER TABLE [dw].[f_haku_ja_valinta_hakeneet_korkea_aste]
+ADD d_kytkin_pohjakoulutuskk_ulkomaat_id INT NULL;
+END
