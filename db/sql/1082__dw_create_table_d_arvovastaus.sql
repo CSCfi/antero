83b2b76559127d63a9f612adc79aa331f31be1a0
villehoo <ville.hamalainen@csc.fi>
2017-12-19 10:50:50 +0200

    add table d_arvovastaus

---
+++ db/sql/1082__dw_create_table_d_arvovastaus.sql
@@ -0,0 +1,25 @@
+USE [ANTERO]
+GO
+IF NOT EXISTS (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='dw' and TABLE_NAME='d_arvovastaus') BEGIN
+
+CREATE TABLE dw.d_arvovastaus (
+  id int IDENTITY(1,1) NOT NULL,
+  loadtime datetime NOT NULL,
+  source nvarchar(100) NOT NULL,
+  username nvarchar(30) NOT NULL,
+  vuosi   nvarchar(5) NOT NULL,
+  vastaus_koodi nvarchar(5) NOT NULL,
+  vastaus_fi nvarchar(100) NOT NULL,
+  vastaus_sv nvarchar(100) NOT NULL,
+  vastaus_en nvarchar(100) NOT NULL,
+  kysymysryhmaid nvarchar(100) NOT NULL,
+  vastaustyyppi nvarchar(100) NOT NULL,
+
+ CONSTRAINT PK__d_arvovastaus PRIMARY KEY CLUSTERED (id ASC)
+)
+;
+ALTER TABLE dw.d_arvovastaus ADD  CONSTRAINT DF__d_nimitystapa__loadtime  DEFAULT (getdate()) FOR loadtime
+;
+ALTER TABLE dw.d_arvovastaus ADD  CONSTRAINT DF__d_nimitystapa__username  DEFAULT (suser_sname()) FOR username
+;
+END
