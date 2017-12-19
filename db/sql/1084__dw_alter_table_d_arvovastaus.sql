97ac552db23d31e6c09b69d68ffac10a2c36f971
villehoo <ville.hamalainen@csc.fi>
2017-12-19 12:25:19 +0200

    alter table d_arvovastaus & create-alter sp lataa_d_arvovastaus

---
+++ db/sql/1084__dw_alter_table_d_arvovastaus.sql
@@ -0,0 +1,8 @@
+USE [ANTERO]
+GO
+IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='dw' and TABLE_NAME='d_arvovastaus')
+BEGIN
+  ALTER TABLE dw.d_arvovastaus ALTER COLUMN vastaus_fi nvarchar(500);
+  ALTER TABLE dw.d_arvovastaus ALTER COLUMN vastaus_fi nvarchar(500);
+  ALTER TABLE dw.d_arvovastaus ALTER COLUMN vastaus_fi nvarchar(500);
+END
