c9048e1bd78bac531a0f890e6305470fe8953626
villehoo <ville.hamalainen@csc.fi>
2017-12-21 11:14:29 +0200

    create table & sp for amk_rekrytointihaut

---
+++ db/sql/1104__dw_create_table_f_amk_rekrytointihaut.sql
@@ -0,0 +1,36 @@
+--
+--  create table f_amk_rekrytointihaut
+--
+
+IF NOT EXISTS
+	(SELECT * FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_SCHEMA='dw'
+	AND TABLE_NAME='f_amk_rekrytointihaut')
+
+BEGIN
+
+CREATE TABLE [dw].[f_amk_rekrytointihaut]
+ (
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[d_korkeakoulu_id] [int] NOT NULL,
+	[vuosi] [int] NOT NULL,
+	[hakunumero] [int] NOT NULL,
+	[d_kytkin_haun_kansainvalisyys_id] [int] NOT NULL,
+	[d_sukupuoli_id] [int] NOT NULL,
+	[d_kytkin_kansalaisuus_suomi_id] [int] NOT NULL,
+	[d_rekrytointi_maanosa_id] [int] NOT NULL,
+	[hakijat_lkm] [int] NULL,
+	[loadtime] [datetime] NOT NULL,
+	[source] [varchar](100) NOT NULL,
+	[username] [varchar](30) NOT NULL,
+	CONSTRAINT [PK__f_amk_rekrytointihaut] PRIMARY KEY CLUSTERED
+	( [id] ASC ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+END
+GO
+
+ALTER TABLE [dw].[f_amk_rekrytointihaut] ADD  CONSTRAINT [DF_f_amk_rekrytointihaut_loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [dw].[f_amk_rekrytointihaut] ADD  CONSTRAINT [DF_f_amk_rekrytointihaut_username]  DEFAULT (suser_sname()) FOR [username]
+GO
---
+++ db/sql/1106__create_procedure_p_lataa_f_amk_rekrytointi_valitut.sql
@@ -0,0 +1,9 @@
+IF NOT EXISTS (
+		SELECT *
+		FROM sys.objects
+		WHERE object_id = OBJECT_ID(N'dw.p_lataa_f_yo_rekrytointi_valitut')
+			AND type IN (N'P',N'PC')
+		)
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE dw.p_lataa_f_yo_rekrytointi_valitut AS'
+END
