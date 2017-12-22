7e7ddb49cad56014b7b4e7fbeec13446a6c387b8
pankalai <anssi.pankalainen@csc.fi>
2017-12-22 10:25:08 +0200

    added check existence

--- db/sql/1118__alter_table_vipunentk_f_opettajat_perusopetus.sql
+++ db/sql/1118__alter_table_vipunentk_f_opettajat_perusopetus.sql
@@ -1,29 +1,39 @@
 USE [VipunenTK]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa]
 GO
-ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] DROP CONSTRAINT IF EXISTS [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika]
 GO
-/****** Object:  Table [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]    Script Date: 22.12.2017 9:38:22 ******/
-DROP TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]
+/****** Object:  Table [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]    Script Date: 22.12.2017 10:23:35 ******/
+DROP TABLE IF EXISTS [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]
 GO
-/****** Object:  Table [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]    Script Date: 22.12.2017 9:38:22 ******/
+/****** Object:  Table [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]    Script Date: 22.12.2017 10:23:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot](
 	[tilastointivuosi] [nvarchar](4) NULL,
 	[tilv_date] [date] NULL,
@@ -63,46 +73,62 @@ CREATE TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot](
 	[resurssiopettajan_osallistuminen_id] [int] NULL,
 	[resurssiopettajan_tunnit] [int] NULL
 ) ON [PRIMARY]
-
+END
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika] FOREIGN KEY([ika_id])
 REFERENCES [dbo].[d_ika] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_ika]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa] FOREIGN KEY([tehtavatyyppi_perusopetuksessa_id])
 REFERENCES [dbo].[d_opettajan_tehtavatyyppi_perusopetuksessa] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajan_tehtavatyyppi_perusopetuksessa]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine] FOREIGN KEY([opettajan_eniten_opettama_aine_id])
 REFERENCES [dbo].[d_opettajat_aine] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_aine]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste] FOREIGN KEY([paaasiallinen_koulutusmuoto_id])
 REFERENCES [dbo].[d_opettajat_koulutusaste] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_koulutusaste]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste] FOREIGN KEY([oman_luokan_aste_id])
 REFERENCES [dbo].[d_opettajat_luokka_aste] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opettajat_luokka_aste]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli] FOREIGN KEY([oppilaitoksen_kieli_id])
 REFERENCES [dbo].[d_opetuskieli2] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_opetuskieli]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi] FOREIGN KEY([oppilaitostyyppi_id])
 REFERENCES [dbo].[d_oppilaitostyyppi] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_oppilaitostyyppi]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]  WITH CHECK ADD  CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli] FOREIGN KEY([sukupuoli_id])
 REFERENCES [dbo].[d_sukupuoli] ([id])
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli]') AND parent_object_id = OBJECT_ID(N'[dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot]'))
 ALTER TABLE [dbo].[f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot] CHECK CONSTRAINT [FK_f_Opettajat_perusopetus_kelpoisuus_opetusryhmakoot_d_sukupuoli]
 GO
 USE [ANTERO]
