c67aeeab4c23a856258f3fdfdc6b02d2317c782c
villehoo <ville.hamalainen@csc.fi>
2018-01-22 09:43:47 +0200

    fix sa_suorat_amk7c_rekrytointi_valitun_taustatiedot

---
+++ db/sql/1175__drop_create_table_sa_suorat_amk7c_rekrytointihaut_valitun_taustatiedot.sql
@@ -0,0 +1,27 @@
+USE [ANTERO]
+
+IF  EXISTS
+		(select * from INFORMATION_SCHEMA.TABLES
+			where TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_amk7c_rekrytointi_valitun_taustatiedot')
+BEGIN
+drop table [sa].[sa_suorat_amk7c_rekrytointi_valitun_taustatiedot]
+END
+IF NOT EXISTS
+		(select * from INFORMATION_SCHEMA.TABLES
+			where TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_amk7c_rekrytointi_valitun_taustatiedot')
+
+BEGIN
+CREATE TABLE [sa].[sa_suorat_amk7c_rekrytointi_valitun_taustatiedot](
+	[korkeakoulutunnus] [varchar](50) NULL,
+	[Vastaajatunnus] [nvarchar](max) NULL,
+	[Henkilönumero] [nvarchar](max) NULL,
+	[Haun numero] [nvarchar](max) NULL,
+	[kysymysryhmä] [varchar](100) NULL,
+	[Kysymys] [nvarchar](400) NULL,
+	[Vastaus] [nvarchar](400) NULL,
+	[IMP_CREATED_DATE] [datetime] NULL,
+	[IMP_UPDATED_DATE] [datetime] NULL,
+	[IMP_CREATED_BY] [varchar](100) NULL
+
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
