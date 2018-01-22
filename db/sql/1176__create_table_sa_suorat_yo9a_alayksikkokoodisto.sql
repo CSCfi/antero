71dcdf9185b4f121edb0bf388fa61a57829bdc85
villehoo <ville.hamalainen@csc.fi>
2018-01-22 09:54:15 +0200

    fix sa_suorat_amk7c_rekrytointi_valitun_taustatiedot

---
+++ db/sql/1176__create_table_sa_suorat_yo9a_alayksikkokoodisto.sql
@@ -0,0 +1,17 @@
+USE [ANTERO]
+
+IF NOT EXISTS
+    (select * from INFORMATION_SCHEMA.TABLES
+     where TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_yo9a_alayksikkokoodisto')
+BEGIN
+CREATE TABLE [sa].[sa_suorat_yo9a_alayksikkokoodisto](
+	[Vuosi] [varchar](5) NULL,
+	[Korkeakoulun_koodi] [varchar](10) NULL,
+	[Paayksikon_koodi] [varchar](10) NULL,
+	[Alayksikon_koodi] [varchar](10) NULL,
+	[Alayksikon_nimi] [varchar](100) NULL,
+	[IMP_CREATED_DATE] [datetime] NULL,
+	[IMP_UPDATED_DATE] [datetime] NULL,
+	[IMP_CREATED_BY] [varchar](100) NULL
+) ON [PRIMARY]
+END
