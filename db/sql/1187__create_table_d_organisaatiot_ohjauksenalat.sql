b6ca751bf52cac45a959a2af528a1ce61be21531
villehoo <ville.hamalainen@csc.fi>
2018-01-29 13:40:46 +0200

    create table and procedure for d_organisaatiot_ohjauksen_alat

---
+++ db/sql/1187__create_table_d_organisaatiot_ohjauksenalat.sql
@@ -0,0 +1,46 @@
+--
+--  create table d_organisaatiot_ohjauksen_alat
+--
+
+IF NOT EXISTS
+	(SELECT * FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_SCHEMA='dw'
+	AND TABLE_NAME='d_organisaatiot_ohjauksen_alat')
+
+BEGIN
+
+CREATE TABLE [dw].[d_organisaatiot_ohjauksen_alat]
+ (
+	[id] [int] IDENTITY(0,1) NOT NULL,
+	[Vuosi] [int] NOT NULL,
+	[korkeakoulu_koodi]	[varchar](10) NOT NULL,
+	[alayksikkö_koodi]	[varchar](100) NOT NULL,
+	[ohjauksen_ala_koodi]	[varchar](3) NOT NULL,
+	[jyvitys]	[varchar](10) NOT NULL,
+	[max 100 %]	[varchar](5) NOT NULL,
+	[loadtime] [datetime] NOT NULL,
+	[source] [varchar](100) NOT NULL,
+	[username] [varchar](30) NOT NULL,
+	--[jarjestys_korkeakoulu_koodi] [varchar](10)  NOT NULL,
+	--[jarjestys_alayksikkö_koodi]  [varchar](100) NOT NULL,
+	--[jarjestys_ohjauksen_ala_koodi]	[varchar](3) NOT NULL,
+	CONSTRAINT [PK__d_organisaatiot_ohjauksenalat] PRIMARY KEY CLUSTERED
+	(
+	[id] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]<
+END
+GO
+
+ALTER TABLE [dw].[d_organisaatiot_ohjauksen_alat] ADD  CONSTRAINT [DF_d_organisaatiot_ohjauksen_alat_loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [dw].[d_organisaatiot_ohjauksen_alat] ADD  CONSTRAINT [DF_d_organisaatiot_ohjauksen_alat_username]  DEFAULT (suser_sname()) FOR [username]
+GO
+
+ALTER TABLE [dw].[d_organisaatiot_ohjauksen_alat] ADD [jarjestys_korkeakoulu_koodi] AS (case when [korkeakoulu_koodi]=(-1) then '99999' else CONVERT([varchar](10),[korkeakoulu_koodi]) end)
+GO
+ALTER TABLE [dw].[d_organisaatiot_ohjauksen_alat] ADD [jarjestys_alayksikkö_koodi]  AS (case when [alayksikkö_koodi]=(-1) then 'ZzZzZ' else CONVERT([varchar](100),[alayksikkö_koodi]) end)
+GO
+ALTER TABLE [dw].[d_organisaatiot_ohjauksen_alat] ADD [jarjestys_ohjauksen_ala_koodi] AS (case when [ohjauksen_ala_koodi]=(-1) then '999' else CONVERT([varchar](3),[ohjauksen_ala_koodi]) end)
+GO
