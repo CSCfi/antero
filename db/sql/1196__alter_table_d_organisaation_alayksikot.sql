468779f323515ec862b42921c2f546762dbae4cd
villehoo <ville.hamalainen@csc.fi>
2018-01-30 12:51:33 +0200

    try to fix prod paayksikko_koodi add

--- db/sql/1196__alter_table_d_organisaation_alayksikot.sql
+++ db/sql/1196__alter_table_d_organisaation_alayksikot.sql
@@ -1,12 +1,28 @@
-USE [ANTERO]
 
-IF NOT EXISTS (SELECT 1 FROM sys.columns
-          WHERE Name = N'paayksikko_koodi'
-          AND Object_ID = Object_ID(N'dw.d_organisaation_alayksikot'))
+USE [ANTERO]
+IF EXISTS (
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_organisaation_alayksikot' AND COLUMN_NAME='jarjestys_paayksikko_koodi')
+
+BEGIN
+	ALTER TABLE dw.d_organisaation_alayksikot drop column jarjestys_paayksikko_koodi
+END
+
+IF  EXISTS (
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_organisaation_alayksikot' AND COLUMN_NAME='paayksikko_koodi')
+
+BEGIN
+	ALTER TABLE dw.d_organisaation_alayksikot drop column paayksikko_koodi
+END
+
+IF NOT EXISTS (
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_organisaation_alayksikot' AND COLUMN_NAME='paayksikko_koodi')
 
 BEGIN
 ALTER TABLE [dw].[d_organisaation_alayksikot]
-ADD paayksikko_koodi varchar(100)
+ADD paayksikko_koodi varchar(100) null
 END
 
 
@@ -28,13 +44,7 @@ BEGIN
   END
 END
 
-IF EXISTS (
-	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_organisaation_alayksikot' AND COLUMN_NAME='jarjestys_paayksikko_koodi')
 
-BEGIN
-	ALTER TABLE dw.d_organisaation_alayksikot drop column jarjestys_paayksikko_koodi
-END
 IF NOT EXISTS (
 	SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 	WHERE TABLE_SCHEMA='dw' AND TABLE_NAME='d_organisaation_alayksikot' AND COLUMN_NAME='jarjestys_paayksikko_koodi')
