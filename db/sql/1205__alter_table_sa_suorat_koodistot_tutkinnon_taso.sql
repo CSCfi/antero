533d8a93adcf68cdc95530141e07b415a9f82bfb
villehoo <ville.hamalainen@csc.fi>
2018-01-31 13:58:00 +0200

    alter table sa.sa_suorat_koodistot_tutkinnon_taso

---
+++ db/sql/1205__alter_table_sa_suorat_koodistot_tutkinnon_taso.sql
@@ -0,0 +1,9 @@
+
+USE [ANTERO]
+
+ALTER TABLE sa.sa_suorat_koodistot_tutkinnon_taso ALTER COLUMN selite_FI VARCHAR(50)
+;
+ALTER TABLE sa.sa_suorat_koodistot_tutkinnon_taso ALTER COLUMN selite_SV VARCHAR(50)
+;
+ALTER TABLE sa.sa_suorat_koodistot_tutkinnon_taso ALTER COLUMN selite_EN VARCHAR(50)
+;
