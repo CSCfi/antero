36763ba5214d830ddbc37a36d33bd97dc07aff5f
villehoo <ville.hamalainen@csc.fi>
2018-02-05 10:42:56 +0200

    alter table sa.sa_suorat_amk8a_alayksikkokoodisto

---
+++ db/sql/1216__alter_table_sa_suorat_amk8a_alayksikkokoodisto.sql
@@ -0,0 +1,6 @@
+USE [ANTERO]
+ALTER TABLE sa.sa_suorat_amk8a_alayksikkokoodisto ALTER COLUMN [Vuosi] [nvarchar](5) NULL
+ALTER TABLE sa.sa_suorat_amk8a_alayksikkokoodisto ALTER COLUMN   	[Korkeakoulun_koodi] [nvarchar](10) NULL
+ALTER TABLE sa.sa_suorat_amk8a_alayksikkokoodisto ALTER COLUMN   	[Paayksikon_koodi] [nvarchar](30) NULL
+ALTER TABLE sa.sa_suorat_amk8a_alayksikkokoodisto ALTER COLUMN   	[Alayksikon_koodi] [nvarchar](30) NULL
+ALTER TABLE sa.sa_suorat_amk8a_alayksikkokoodisto ALTER COLUMN   	[Alayksikon_nimi] [nvarchar](100) NULL
