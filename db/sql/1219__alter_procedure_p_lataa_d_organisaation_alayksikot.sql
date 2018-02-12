61a06979cdd5acb813a4f02679baab9ea11733ba
villehoo <ville.hamalainen@csc.fi>
2018-02-12 16:11:25 +0200

    YTL views -  add s2017

--- db/sql/1219__alter_procedure_p_lataa_d_organisaation_alayksikot.sql
+++ db/sql/1219__alter_procedure_p_lataa_d_organisaation_alayksikot.sql
@@ -35,7 +35,7 @@ USING (
       ,[Alayksikon_koodi]
       ,[Alayksikon_nimi]
 		,'etl: p_lataa_d_organisaation_alayksikot (' + [IMP_CREATED_BY] +')'AS source
-	FROM sa.sa_suorat_amk8a_alayksikkokoodisto
+	FROM sa.sa_suorat_amk8a_alayksikkokoodisto where vuosi < '2017'
 	) AS src
 	ON target.vuosi = src.vuosi
 		AND target.korkeakoulu_koodi = src.korkeakoulun_koodi
@@ -65,6 +65,44 @@ WHEN NOT MATCHED
 			,src.[alayksikon_nimi]
 			,src.source
 			);
+			MERGE dw.d_organisaation_alayksikot AS target
+			USING (
+				SELECT [vuosi]
+				  ,[Korkeakoulun_koodi]
+			      ,COALESCE([Paayksikon_koodi], '-1') as Paayksikon_koodi
+			      ,[Alayksikon_koodi]
+			      ,[Alayksikon_nimi]
+					,'etl: p_lataa_d_organisaation_alayksikot (' + [IMP_CREATED_BY] +')'AS source
+				FROM sa.sa_suorat_amk8a_alayksikkokoodisto where vuosi > '2016'
+				) AS src
+				ON target.vuosi = src.vuosi
+					AND target.korkeakoulu_koodi = src.korkeakoulun_koodi
+					AND target.alayksikko_koodi = src.alayksikon_koodi
+
+			WHEN MATCHED
+				THEN
+					UPDATE
+					SET target.alayksikko_koodi = src.[alayksikon_koodi]
+							,target.paayksikko_koodi = src.[paayksikon_koodi]
+							,target.source = src.source
+			WHEN NOT MATCHED
+				THEN
+					INSERT (
+						[vuosi]
+						,[korkeakoulu_koodi]
+						,[paayksikko_koodi]
+						,[alayksikko_koodi]
+						,[alayksikko_nimi]
+						,source
+						)
+					VALUES (
+						src.[vuosi]
+						,src.[korkeakoulun_koodi]
+						,src.[Paayksikon_koodi]
+						,src.[alayksikon_koodi]
+						,src.[alayksikon_nimi]
+						,src.source
+						);
 MERGE dw.d_organisaation_alayksikot AS target
 USING (
 	SELECT [vuosi]
