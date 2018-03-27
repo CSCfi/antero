590b58e95c59cd2acc2c82a9f446beb9389e6442
oheino <oheino@csc.fi>
2018-03-27 16:13:14 +0300

    create fact and procedure 55op summa amk

---
+++ db/sql/1489__create_procedure_lataa_f_virta_otp_55_summa_amk.sql
@@ -0,0 +1,50 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_virta_otp_55_summa_amk]    Script Date: 27.3.2018 16:10:23 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dw].[p_lataa_f_virta_otp_55_summa_amk] AS
+
+--TRUNCATE TABLE dw.f_virta_otp_55_summa
+
+INSERT INTO dw.f_virta_otp_55_summa_amk (
+  vuosi,
+  kuukausi,
+  d_oppilaitos_id,
+  d_koulutusluokitus_id,
+  suorittanut55,
+  suorittanut55ilmanPankkia,
+  suorittanut55pankinAvulla,
+  suorittanut27,
+  source
+)
+SELECT
+  f.vuosi,
+  month(f.loadtime),
+  coalesce(d1.id,-1) AS d_oppilaitos_id,
+  coalesce(d2.id,-1) AS d_koulutusluokitus_id,
+ 
+  sum(case when (f.suorittanut55ilmanPankkia = 1 or f.suorittanut55pankinAvulla = 1 or f.suorittanut27 = 1) Then 1 Else 0 end) as suorittanut55,
+  sum(case when (f.suorittanut55ilmanPankkia = 1) Then 1 Else 0 end) as suorittanut55ilmanPankkia,
+  sum(case when (f.suorittanut55pankinAvulla = 1) Then 1 Else 0 end) as suorittanut55pankinAvulla,
+  sum(case when (f.suorittanut27 = 1) Then 1 Else 0 end) as suorittanut27,
+  f.source
+
+FROM sa.sa_virta_otp_viisviis as f
+LEFT JOIN dw.d_amk d1 ON d1.amk_tunnus = f.oppilaitostunnus
+LEFT JOIN dw.d_koulutusluokitus d2 ON d2.koulutusluokitus_koodi = f.tkoodi
+
+WHERE (f.suorittanut55ilmanPankkia = 1 or f.suorittanut55pankinAvulla = 1 or f.suorittanut27 = 1)
+GROUP BY f.vuosi, month(f.loadtime), d1.id, d2.id, f.source
+
+
+GO
+
+
