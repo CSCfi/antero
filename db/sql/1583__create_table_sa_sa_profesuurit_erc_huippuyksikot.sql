eb883856eefdab3ed2641916f3155d73da911885
Karin-Oka <janne.karin-oka@csc.fi>
2018-04-27 17:05:53 +0300

    naming naming

---
+++ db/sql/1583__create_table_sa_sa_profesuurit_erc_huippuyksikot.sql
@@ -0,0 +1,18 @@
+GO USE [ANTERO]
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[SA_profesuurit_erc_huippuyksikot]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [sa].[SA_profesuurit_erc_huippuyksikot](
+	[Tieteenala] [nvarchar](max) NOT NULL,
+	[TA10] [nvarchar](50) NOT NULL,
+	[TTHV] [nvarchar](50) NOT NULL,
+	[Yliopisto] [nvarchar](50) NOT NULL,
+	[Organisaatio_koodi] [nvarchar](50) NOT NULL,
+	[Osal_hui_yks_14-19_18-25] [nvarchar](50) NULL,
+	[SA_prof_13-16] [nvarchar](50) NULL,
+	[ERC_StG_CoG_AdG_15-17] [nvarchar](50) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+USE [ANTERO]
