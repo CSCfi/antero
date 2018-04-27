723b79e7812c3ede8b513212499bc210b424c2c6
Karin-Oka <janne.karin-oka@csc.fi>
2018-04-27 16:42:12 +0300

    Tiputetaan ja luodaan uusiks

---
+++ db/sql/1584__drop_table_vipunentk_lisatiedot_Haku_valinta_kandi_maisteri.sql
@@ -0,0 +1,9 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  Table [dbo].[Haku_valinta_kandi_maisteri]    Script Date: 27.4.2018 16:39:10 ******/
+DROP TABLE [dbo].[Haku_valinta_kandi_maisteri]
+GO
+
+
+USE [ANTERO]
