0c53f3b6b37e498a99475e38cc80a94704908787
Karin-Oka <janne.karin-oka@csc.fi>
2018-04-27 16:28:05 +0300

    Few fixes to naming

--- db/sql/1583__create_table_vipunentk_lisatiedot_Haku_valinta_kandi_maisteri.sql
+++ db/sql/1583__create_table_vipunentk_lisatiedot_Haku_valinta_kandi_maisteri.sql
@@ -5,7 +5,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Haku_valinta_kandi-maisteri]') AND type in (N'U'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Haku_valinta_kandi_maisteri]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [dbo].[Haku_valinta_kandi_maisteri](
 	[I_sykli_kandi] [nchar](6) NOT NULL,
