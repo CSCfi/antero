91b5fb957916ae995b96f8604d68d3d00fccd8d6
Karin-Oka <janne.karin-oka@csc.fi>
2018-04-27 15:23:22 +0300

    Renamed table

--- db/sql/1578__create_table_vipunentk_lisatiedot_Haku_valinta_kandi_maisteri.sql
+++ db/sql/1578__create_table_vipunentk_lisatiedot_Haku_valinta_kandi_maisteri.sql
@@ -7,7 +7,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Haku_valinta_kandi-maisteri]') AND type in (N'U'))
 BEGIN
-CREATE TABLE [dbo].[Haku_valinta_kandi-maisteri](
+CREATE TABLE [dbo].[Haku_valinta_kandi_maisteri](
 	[I_sykli_kandi] [nchar](6) NOT NULL,
 	[I_sykli_maisteri] [nchar](6) NOT NULL
 ) ON [PRIMARY]
