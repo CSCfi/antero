2dd300a5bb250cbd792dfbb3a1afa5ed51d82bd8
pankalai <anssi.pankalainen@csc.fi>
2018-04-27 12:06:20 +0300

    added existence check

--- db/sql/1578__create_table_vipunentk_lisatiedot_Haku_valinta_kandi-maisteri.sql
+++ db/sql/1578__create_table_vipunentk_lisatiedot_Haku_valinta_kandi-maisteri.sql
@@ -1,10 +1,16 @@
 USE [VipunenTK_lisatiedot]
 GO
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Haku_valinta_kandi-maisteri]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Haku_valinta_kandi-maisteri](
 	[I_sykli_kandi] [nchar](6) NOT NULL,
 	[I_sykli_maisteri] [nchar](6) NOT NULL
 ) ON [PRIMARY]
-
+END
 GO
 USE [ANTERO]
