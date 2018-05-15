b7fa1d9575c7a54589baad11dea74654c8846645
fredrikfinnberg <fredrik.finnberg@gmail.com>
2018-05-15 17:27:17 +0300

    Korjaus pvm ei date bigint

---
+++ db/sql/1636__recreate_table_sa_virta_otp_korkeakouluhaut_hakijat_tutkinnot.sql
@@ -0,0 +1,34 @@
+-- Korkeakouluhakijoiden OID Tutkinnot Virrasta
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.TABLES
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_otp_korkeakouluhaut_hakijat_tutkinnot'
+) BEGIN
+DROP TABLE sa.sa_virta_otp_korkeakouluhaut_hakijat_tutkinnot
+END
+
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.TABLES
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_otp_korkeakouluhaut_hakijat_tutkinnot'
+) BEGIN
+
+
+CREATE TABLE sa.sa_virta_otp_korkeakouluhaut_hakijat_tutkinnot(
+
+		id bigint IDENTITY(1,1) NOT NULL,
+		
+		korkeakoulu varchar(5) NULL,
+		oid varchar(50) NOT NULL,
+		suorituspaivamaara bigint NULL, --date
+		tutkinto varchar(6) NULL,
+
+		loadtime datetime2(4) NOT NULL,
+		source nvarchar(255) NULL,
+		username nvarchar(128) NOT NULL,
+
+	CONSTRAINT PK_korkeakouluhaut_hakijat_tutkinnot PRIMARY KEY CLUSTERED (id ASC)
+)
+;
+
+END
