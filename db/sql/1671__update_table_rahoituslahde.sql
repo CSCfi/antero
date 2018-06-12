5ecbe7f5f2d262869757150df68df4b9946bcf57
tvaisane <teemu.vaisanen@csc.fi>
2018-06-12 11:09:23 +0300

    Removed GO from end

--- db/sql/1671__update_table_rahoituslahde.sql
+++ db/sql/1671__update_table_rahoituslahde.sql
@@ -4,14 +4,12 @@ GO
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' and TABLE_NAME='rahoituslahde') BEGIN
 
 update [dbo].[rahoituslahde]
-set     [rahoituslahde] = 'Lukukausimaksu' --old Lukukausimaksukokeilu
+set    [rahoituslahde] = 'Lukukausimaksu' --old Lukukausimaksukokeilu
       ,[rahoituslahde_SV] = 'Terminsavgift' --old Experiment med terminsavgift
       ,[rahoituslahde_EN] = 'Tuition fee'  --old Tuition fee trial period
 	  ,kommentti = 'CSC Teemu' --old CSC Jarmo
   where rahoituslahde_koodi = '5';
  
- END;
-
-  GO 
+ END; 
 
 USE [ANTERO]
