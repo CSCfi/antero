24cb811bf0deaf45a255362f08690ef8a7ec617c
tvaisane <teemu.vaisanen@csc.fi>
2018-06-12 09:44:34 +0300

    Update lukukausimaksukokeilu to lukukausimaksu (and translations)

---
+++ db/sql/1671__update_table_d_rahoituslahde.sql
@@ -0,0 +1,17 @@
+﻿USE [VipunenTK_lisatiedot]
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[rahoituslahde]') AND type in (N'U'))
+
+update [dbo].[rahoituslahde]
+set     [rahoituslahde] = 'Lukukausimaksu' --old Lukukausimaksukokeilu
+      ,[rahoituslahde_SV] = 'Terminsavgift' --old Experiment med terminsavgift
+      ,[rahoituslahde_EN] = 'Tuition fee'  --old Tuition fee trial period
+	  ,kommentti = 'CSC Teemu' --old CSC Jarmo
+  where rahoituslahde_koodi = '5'
+ 
+  GO 
+
+USE ANTERO
+
+GO
