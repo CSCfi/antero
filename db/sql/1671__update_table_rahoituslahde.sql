2a07c7c93e424042da30ff05bb6b7cd5a7821ecb
tvaisane <teemu.vaisanen@csc.fi>
2018-06-12 11:00:15 +0300

    Added semicolon at the end of where

--- db/sql/1671__update_table_rahoituslahde.sql
+++ db/sql/1671__update_table_rahoituslahde.sql
@@ -8,7 +8,7 @@ set     [rahoituslahde] = 'Lukukausimaksu' --old Lukukausimaksukokeilu
       ,[rahoituslahde_SV] = 'Terminsavgift' --old Experiment med terminsavgift
       ,[rahoituslahde_EN] = 'Tuition fee'  --old Tuition fee trial period
 	  ,kommentti = 'CSC Teemu' --old CSC Jarmo
-  where rahoituslahde_koodi = '5'
+  where rahoituslahde_koodi = '5';
  
  END;
 
