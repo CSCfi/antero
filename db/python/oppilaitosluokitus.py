c3189591af355f08530eef4488841c03e835d89e
villehoo <ville.hamalainen@csc.fi>
2018-06-12 11:26:32 +0300

    add key check for postinumeroUri (oppilaitosluokitus)

--- db/python/oppilaitosluokitus.py
+++ db/python/oppilaitosluokitus.py
@@ -83,13 +83,15 @@ def load(secure,hostname,url,schema,table,codeset,verbose=False,debug=False):
           if jj["kayntiosoite"]:
             row["osoite"] = jj["kayntiosoite"]["osoite"]
             if jj["kayntiosoite"]["postinumeroUri"]:
-              row["postinumero"] = jj["kayntiosoite"]["postinumeroUri"].replace("posti_","")
+            #fix KeyError
+              row["postinumero"] = None if ["postinumeroUri"] not in jj["kayntiosoite"]["postinumeroUri"] else jj["kayntiosoite"]["postinumeroUri"].replace("posti_","")
             row["postitoimipaikka"] = jj["kayntiosoite"]["postitoimipaikka"]
           elif jj["postiosoite"]:
             row["osoite"] = jj["postiosoite"]["osoite"]
             if jj["postiosoite"]["postinumeroUri"]:
               row["postinumero"] = jj["postiosoite"]["postinumeroUri"].replace("posti_","")
             row["postitoimipaikka"] = jj["postiosoite"]["postitoimipaikka"]
+
           # todo get coordinates (see geocoding.py)
 
 
