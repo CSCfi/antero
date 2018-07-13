28a875ad7893c74d827efb44316cd668de135f57
pankalai <anssi.pankalainen@csc.fi>
2018-07-13 11:29:14 +0300

    modified procedure of placement into further education and added new columns into age dimension

---
+++ db/sql/1727__alter_table_d_ika.sql
@@ -0,0 +1,11 @@
+alter table ANTERO.[dw].[d_ika]
+add ikaryhma3_fi varchar(100)
+
+alter table ANTERO.[dw].[d_ika]
+add ikaryhma3_sv varchar(100)
+
+alter table ANTERO.[dw].[d_ika]
+add ikaryhma3_en varchar(100)
+
+alter table ANTERO.[dw].[d_ika]
+add jarjestys_ikaryhma3 varchar(100)
