82b3d3141a8abdd4e012d7b52eb4f52cce219a74
tvaisane <teemu.vaisanen@csc.fi>
2018-08-30 10:22:11 +0300

    Added END

--- db/sql/1797__create_table_sa_varda_koodistot.sql
+++ db/sql/1797__create_table_sa_varda_koodistot.sql
@@ -8,9 +8,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[sa_varda_koodistot]') AND type in (N'U'))
-
-BEGIN
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='sa' and TABLE_NAME='sa_varda_koodistot') BEGIN
 
 CREATE TABLE [sa].[sa_varda_koodistot](
 	[id] [bigint] IDENTITY(1,1) NOT NULL,
@@ -32,6 +30,8 @@ CREATE TABLE [sa].[sa_varda_koodistot](
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
+END
 
 GO
 
