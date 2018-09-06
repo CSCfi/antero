9ed5dc68ef213cb7c1f81fe22b9a6438f2e828ef
Kati Lehtosaari <kati.lehtosaari@csc.fi>
2018-09-06 15:08:59 +0300

    taulu tutkinnonsuorittaneetvaihdossapervuosi Virrasta

---
+++ db/sql/1810__create_table_sa_virta_otp_tutkinnonsuorittaneetvaihdossapervuosi.sql
@@ -0,0 +1,39 @@
+-- Erikoistumiskoulutukset
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.TABLES
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_otp_erikoistumiskoulutukset'
+) BEGIN
+DROP TABLE sa.sa_virta_otp_erikoistumiskoulutukset
+END
+
+
+
+IF NOT EXISTS (
+  select * from INFORMATION_SCHEMA.TABLES
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_otp_erikoistumiskoulutukset'
+) BEGIN
+
+
+CREATE TABLE sa.sa_virta_otp_erikoistumiskoulutukset(
+
+		id bigint IDENTITY(1,1) NOT NULL,
+		
+		erikoistumiskoulutuskoodi nvarchar(20) NULL,
+		ika smallint  NULL,
+		ohjauksenalakoodi nvarchar(20) NULL,
+		opiskeluoikeudenalkamispaivamaara bigint NULL,
+		oppilaitoskoodi nvarchar(8) NULL,
+		sukupuolikoodi nvarchar (5) NOT NULL,
+
+		loadtime datetime2(4) NOT NULL,
+		source nvarchar(255) NULL,
+		username nvarchar(128) NOT NULL,
+
+	CONSTRAINT PK_erikoistumiskoulutukset PRIMARY KEY CLUSTERED (id ASC)
+)
+;
+
+END
+
+
