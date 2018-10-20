fde0e131bb2d51ff3b9c16b211e48fa1d0e8c8c1
fredrikfinnberg <fredrik.finnberg@gmail.com>
2018-10-20 15:38:42 +0300

    TK opiskelija OKM Ennakko

---
+++ db/sql/1898__create_table_sa_virta_otp_tk_opisk_ennakko.sql
@@ -0,0 +1,89 @@
+--  Tilastokeskus Opiskelijatiedonkeruu OKM Ennakko sa taulu
+USE ANTERO
+GO
+
+IF NOT EXISTS (
+  SELECT * FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_SCHEMA='sa' AND TABLE_NAME='sa_virta_otp_tk_opisk_ennakko'
+) BEGIN
+
+CREATE TABLE sa.sa_virta_otp_tk_opisk_ennakko  (
+
+	id bigint IDENTITY(1,1) NOT NULL,
+  
+	aikielir1x varchar(2) NULL,
+	aineisto varchar(1) NULL,
+	allk varchar(1) NULL,
+	alvv varchar(4) NULL,
+	alvvkk varchar(4) NULL,
+	alvvkklasna varchar(4) NULL,
+	alvvkksek varchar(4) NULL,
+	alvvkkseklasna varchar(4) NULL,
+	alvvopa varchar(4) NULL,
+	alvvopalasna varchar(4) NULL,
+	alvvsek varchar(4) NULL,
+	alvvseklasna varchar(4) NULL,
+	avo varchar(1) NULL,
+	ensisop varchar(1) NULL,
+	fte varchar(1) NULL,
+	fuksi varchar(1) NULL,
+	jarj varchar(6) NULL,
+	kansalr2 varchar(1) NULL,
+	kirtu1k varchar(1) NULL,
+	kirtu1v varchar(4) NULL,
+	kk varchar(2) NULL,
+	kkieli varchar(2) NULL,
+	kkun varchar(3) NULL,
+	koulk varchar(6) NULL,
+	koultyp varchar(1) NULL,
+	lahde varchar(2) NULL,
+	lasnalk int NULL,
+	lkm int NULL,
+	ltop varchar(1) NULL,
+	luontipvm varchar(10) NULL,
+	olosyys varchar(1) NULL,
+	olotamm varchar(1) NULL,
+	om varchar(1) NULL,
+	op55 varchar(1) NULL,
+	opek int NULL,
+	opes int NULL,
+	opkelp varchar(1) NULL,
+	opker int NULL,
+	oplaaj int NULL,
+	opmopa varchar(3) NULL,
+	opoikv varchar(4) NULL,
+	opyht0 int NULL,
+	opyht104 int NULL,
+	opyht119 int NULL,
+	opyht120 int NULL,
+	opyht14 int NULL,
+	opyht29 int NULL,
+	opyht44 int NULL,
+	opyht59 int NULL,
+	opyht74 int NULL,
+	opyht89 int NULL,
+	poissalk int NULL,
+	rahlahde varchar(1) NULL,
+	sp varchar(1) NULL,
+	syntv varchar(4) NULL,
+	tilmaa varchar(1) NULL,
+	tilv varchar(4) NULL,
+	tilvaskun varchar(3) NULL,
+	tilvaskun2x varchar(3) NULL,
+	tilvaskunx varchar(3) NULL,
+	tunn varchar(5) NULL,	
+
+	loadtime datetime2(4) NULL,
+	source nvarchar(255) NULL,
+	username nvarchar(128) NOT NULL,
+	
+  CONSTRAINT PK__sa_virta_otp_tk_opisk_ennakko PRIMARY KEY CLUSTERED (id ASC)
+)
+;
+
+ALTER TABLE sa.sa_virta_otp_tk_opisk_ennakko ADD  CONSTRAINT DF__sa_virta_otp_tk_opisk_ennakko__loadtime  DEFAULT (getdate()) FOR [loadtime]
+;
+ALTER TABLE sa.sa_virta_otp_tk_opisk_ennakko ADD  CONSTRAINT DF__sa_virta_otp_tk_opisk_ennakko__username  DEFAULT (suser_name()) FOR username
+;
+
+END
