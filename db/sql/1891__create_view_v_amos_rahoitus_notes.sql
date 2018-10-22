709b300a55c32f5ab58654bbe4ef29b77c0fcf9b
juhahelminen <juha.helminen@csc.fi>
2018-10-22 13:12:56 +0300

    next

--- db/sql/1891__create_view_v_amos_rahoitus_notes.sql
+++ db/sql/1891__create_view_v_amos_rahoitus_notes.sql
@@ -1,24 +1,22 @@
-USE Antero
+USE [Antero]
 GO
-
-IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'dw.v_amos_rahoitus_notes'))
-EXEC dbo.sp_executesql @statement = N'CREATE VIEW dw.v_amos_rahoitus_notes AS SELECT 1 AS a'
-
+IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dw].[v_amos_rahoitus_notes]'))
+EXEC dbo.sp_executesql @statement = N'CREATE VIEW [dw].v_amos_rahoitus_notes AS SELECT 1 AS a'
 GO
-
-ALTER VIEW dw.v_amos_rahoitus_notes
+ALTER VIEW [dw].[v_amos_rahoitus_notes]
 AS
 SELECT        noteid, pvm, tekija, kommentti
 FROM            dw.d_amos_rahoitus_notes
 union
 select top 20 * from (
 SELECT distinct top 2 
-round(rand(1000)*100,0) as noteid
-,      imp_date as pvm
+[id] as noteid
+,      [latauspvm] as pvm
    , 'db-admin' as tekija
-      ,filename as kommentti
-
-  FROM ANTERO.sa.valos_siirtotiedosto_1
+      ,[tiedostonimi] as kommentti
+  FROM [ANTERO].[sa].[sa_amos_siirtotiedosto_okm2]
   order by imp_date desc) as v
 order by pvm desc
 
+
+GO
---
+++ db/sql/1899__create_table_sa_amos_siirtotiedosto_okm2.sql
@@ -0,0 +1,24 @@
+USE [Antero]
+GO 
+IF NOT EXISTS (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='dw' and TABLE_NAME='sa_amos_siirtotiedosto_okm2') 
+BEGIN
+CREATE TABLE [sa].[sa_amos_siirtotiedosto_okm2](
+ [id] [bigint] IDENTITY(1,1) NOT NULL,
+ [versio] [varchar](50) NOT NULL,
+ [kommentti] [varchar](255) NULL,
+ [rahoitus_mukaanlukien_alv] [float] NOT NULL,
+ [alv] [float] NOT NULL,
+ [strategiarahoitus] [float] NOT NULL,
+ [harkinnanvarainen_perusrahoitus] [float] NOT NULL,
+ [kiky_kohdennus] [float] NOT NULL,
+ [jako_ja_oikaisuvara] [float] NOT NULL,
+ [karin_oma_sarake1] [varchar](max) NULL,
+ [karin_oma_sarake2] [varchar](max) NULL,
+ [karin_oma_sarake3] [varchar](max) NULL,
+ [tiedostonimi] [varchar](255) NULL,
+ [latauspvm] [date] NULL,
+ [lastmodified] [datetime] NULL,
+ [username] [varchar](100) NULL,
+ [loadtime] [datetime] NULL)
+END
+GO
