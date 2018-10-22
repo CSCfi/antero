ff415540ff8ff78ba70c8b133b248ea627cd94e9
juhahelminen <juha.helminen@csc.fi>
2018-10-22 14:17:23 +0300

    notes

--- db/sql/1891__create_view_v_amos_rahoitus_notes.sql
+++ db/sql/1891__create_view_v_amos_rahoitus_notes.sql
@@ -1,9 +1,8 @@
 USE [Antero]
 GO
 IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dw].[v_amos_rahoitus_notes]'))
-EXEC dbo.sp_executesql @statement = N'CREATE VIEW [dw].v_amos_rahoitus_notes AS SELECT 1 AS a'
-GO
-ALTER VIEW [dw].[v_amos_rahoitus_notes]
+EXEC dbo.sp_executesql @statement = N'
+CREATE VIEW [dw].[v_amos_rahoitus_notes]
 AS
 SELECT        noteid, pvm, tekija, kommentti
 FROM            dw.d_amos_rahoitus_notes
@@ -18,5 +17,6 @@ SELECT distinct top 2
   order by imp_date desc) as v
 order by pvm desc
 
-
+'
+END
 GO
