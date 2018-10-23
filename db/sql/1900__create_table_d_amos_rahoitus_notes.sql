ec85937130f4d2add725b0ea01a2fc2d5248d9ea
fredrikfinnberg <fredrik.finnberg@gmail.com>
2018-10-23 07:53:29 +0300

    korjaus toimenpide 4

--- db/sql/1900__create_table_d_amos_rahoitus_notes.sql
+++ db/sql/1900__create_table_d_amos_rahoitus_notes.sql
@@ -1,6 +1,6 @@
 USE [Antero]
 GO 
-IF NOT EXISTS (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='dw' and TABLE_NAME='sa_amos_siirtotiedosto_okm6') 
+IF NOT EXISTS (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='dw' and TABLE_NAME='d_amos_rahoitus_notes') 
 BEGIN
 CREATE TABLE [dw].[d_amos_rahoitus_notes](
  [noteid] [int] IDENTITY(1,1) NOT NULL,
