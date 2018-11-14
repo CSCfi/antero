f3b90e2f9a9fe19eab3ded47adaeabce4da7b836
pankalai <anssi.pankalainen@csc.fi>
2018-11-14 12:36:25 +0200

    edit koski opiskelijavuodet

---
+++ db/sql/1946__create_table_koski_amm_opiskelijavuodet.sql
@@ -0,0 +1,32 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[f_koski_opiskelijavuodet]    Script Date: 14.11.2018 12:25:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[f_koski_amm_opiskelijavuodet](
+	[vuosi] [int] NULL,
+	[d_kalenteri_id] [int] NULL,
+	[d_sukupuoli_id] [int] NULL,
+	[d_kieli_aidinkieli_id] [int] NULL,
+	[d_ika_id] [int] NULL,
+	[d_osa_aikaisuus_id] [int] NULL,
+	[d_erityisopetus_id] [int] NULL,
+	[d_majoitus_id] [int] NULL,
+	[d_kytkin_oppisopimuskoulutus_id] [int] NULL,
+	[d_kytkin_vankilaopetus_id] [int] NULL,
+	[d_kytkin_henkilostokoulutus_id] [int] NULL,
+	[d_kytkin_tyovoimakoulutus_id] [int] NULL,
+	[d_koulutusluokitus_id] [int] NULL,
+	[d_organisaatioluokitus_oppilaitos_id] [int] NULL,
+	[d_organisaatioluokitus_jarj_id] [int] NULL,
+	[d_kustannusryhma_id] [int] NULL,
+	[d_suorituksen_tyyppi_id] [int] NULL,
+	[d_opintojenrahoitus_id] [int] NULL,
+	[opiskelijavuodet] [decimal](20, 10) NULL
+) ON [PRIMARY]
+
---
+++ db/sql/1951__drop_table_koski_amm_opiskelijavuodet.sql
@@ -0,0 +1,5 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[f_koski_opiskelijavuodet]    Script Date: 14.11.2018 12:26:06 ******/
+DROP TABLE [dw].[f_koski_opiskelijavuodet]
