bc9b428c7015e18c192ce5928140a4ac63b7578c
villehoo <ville.hamalainen@csc.fi>
2018-12-10 17:19:49 +0200

    add missing columns by recreating table sa_arvo_vastaukset fixed

--- db/sql/2011__drop_create_table_sa_arvo_vastaukset.sql
+++ db/sql/2011__drop_create_table_sa_arvo_vastaukset.sql
@@ -32,7 +32,7 @@ CREATE TABLE [sa].[sa_arvo_vastaukset](
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
+END
 GO
 
 ALTER TABLE [sa].[sa_arvo_vastaukset] ADD  CONSTRAINT [DF__sa_arvo_vastaukset__loadtime]  DEFAULT (getdate()) FOR [loadtime]
