169c5ea33d645819b3e3b5fde15614723ebbac2a
oeskola <olli.eskola@csc.fi>
2019-05-02 17:27:40 +0300

    e

--- db/sql/2211__create_table_d_taidealantyyppikategoria.sql
+++ db/sql/2211__create_table_d_taidealantyyppikategoria.sql
@@ -16,9 +16,7 @@ CREATE TABLE [dw].[d_taidealantyyppikategoria](
 	[taidealantyyppikategoria_nimi_fi] [varchar](100) NOT NULL,
 	[taidealantyyppikategoria_nimi_sv] [varchar](100) NOT NULL,
 	[taidealantyyppikategoria_nimi_en] [varchar](100) NOT NULL,
-	[loadtime] [datetime] NOT NULL,
 	[source] [varchar](100) NOT NULL,
-	[username] [varchar](30) NOT NULL,
 	[jarjestys_taidealantyyppikategoria_koodi]  AS (case when [taidealantyyppikategoria_koodi]=(-1) then '99999' else CONVERT([varchar](10),[taidealantyyppikategoria_koodi]) end),
  CONSTRAINT [PK_D_TAIDEALANTYYPPIKATEGORIA] PRIMARY KEY CLUSTERED 
 (
@@ -26,12 +24,6 @@ CREATE TABLE [dw].[d_taidealantyyppikategoria](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
-GO
-
-ALTER TABLE [dw].[d_taidealantyyppikategoria] ADD  CONSTRAINT [DF__d_taidealantyyppikategoria__loadtime__5224328E]  DEFAULT (getdate()) FOR [loadtime]
-GO
-
-ALTER TABLE [dw].[d_taidealantyyppikategoria] ADD  CONSTRAINT [DF__d_taidealantyyppikategoria__username__531856C7]  DEFAULT (suser_sname()) FOR [username]
 
 
 
