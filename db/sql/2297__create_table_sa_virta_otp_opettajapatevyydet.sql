359be1b447d6651297c1df9e88b775a406a41b63
fredrikfinnberg <fredrik.finnberg@gmail.com>
2019-06-14 14:38:22 +0300

    OKM ope korjaus

--- db/sql/2297__create_table_sa_virta_otp_opettajapatevyydet.sql
+++ db/sql/2297__create_table_sa_virta_otp_opettajapatevyydet.sql
@@ -1,6 +1,15 @@
 -- OKM opettajatiedonkeruu tulostaulu
 USE ANTERO
 GO
+IF EXISTS (
+  SELECT * FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_SCHEMA='sa' AND TABLE_NAME='sa_virta_otp_opettajapatevyydet'
+) 
+BEGIN
+DROP TABLE sa.sa_virta_otp_opettajapatevyydet
+END
+
+
 IF NOT EXISTS (
   SELECT * FROM INFORMATION_SCHEMA.TABLES
   WHERE TABLE_SCHEMA='sa' AND TABLE_NAME='sa_virta_otp_opettajapatevyydet'
