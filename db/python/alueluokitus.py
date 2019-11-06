df7bd81f226a0755e6be93632dac4e4dd0f7b773
villehoo <ville.hamalainen@csc.fi>
2019-11-06 14:29:19 +0200

    CSCVIPUNEN-1644 adding caller-id header alueluokitus debug

--- db/python/alueluokitus.py
+++ db/python/alueluokitus.py
@@ -53,9 +53,9 @@ def load(secure,hostname,url,schema,table,codeset,verbose=False,debug=False):
     httpconn = httplib.HTTPConnection(hostname)
     print strftime("%Y-%m-%d %H:%M:%S", localtime())+" load from "+hostname+url
 
-  reqheaders = {'Content-Type': 'application/json', 'Caller-Id': '1.2.246.562.10.2013112012294919827487.vipunen'}
-  #httpconn.request('GET', url, headers=reqheaders)
-  httpconn.request('GET', url)
+  reqheaders = {'Caller-id': '1.2.246.562.10.2013112012294919827487.vipunen'}
+  httpconn.request('GET', url, headers=reqheaders)
+  #httpconn.request('GET', url)
 
   r = httpconn.getresponse()
   j = json.loads(r.read())
