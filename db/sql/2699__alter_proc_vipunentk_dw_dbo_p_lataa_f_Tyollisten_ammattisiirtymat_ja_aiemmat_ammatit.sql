2417534918c81ec59e69fe73db4792a7a13134fe
jsyrjala <jsyrjala@DWITESTDESK3>
2019-12-09 21:06:20 +0200

    use

--- db/sql/2699__alter_proc_vipunentk_dw_dbo_p_lataa_f_Tyollisten_ammattisiirtymat_ja_aiemmat_ammatit.sql
+++ db/sql/2699__alter_proc_vipunentk_dw_dbo_p_lataa_f_Tyollisten_ammattisiirtymat_ja_aiemmat_ammatit.sql
@@ -192,4 +192,3 @@ SELECT f.[tilastovuosi]
   LEFT OUTER JOIN VipunenTK..d_koulutusluokitus d28 ON d28.koulutusluokitus_avain = f.koulutusala_taso1_erikoistumiskoulutus_amk_siirtyman_paassa
   LEFT OUTER JOIN VipunenTK..d_koulutusluokitus d29 ON d29.koulutusluokitus_avain = f.koulutusala_taso1_erikoistumiskoulutus_yo_siirtyman_paassa
   
-  USE [ANTERO]
