84ecf3b745569a4f07983750a4d08042408430c1
pankalai <anssi.pankalainen@csc.fi>
2019-12-31 14:55:25 +0200

    CSCVIPUNEN-1624

--- tabular/powershell/tabulardeploy.ps1
+++ tabular/powershell/tabulardeploy.ps1
@@ -98,10 +98,10 @@ try
                 }
                 if ($s[0] -eq 'Data Source') {
 					if ($s[1] -like '*arch') {
-						$connstr += $s[0]+"="+"dwitvipuarch" #$prodsqlserver.replace("sql16","arch")
+						$connstr += $s[0]+"="+$prodsqlserver.replace("sql16","arch")
 					}
 					else {
-						$connstr += $s[0]+"="+$prodsqlserver # replace
+						$connstr += $s[0]+"="+"dwipvipusql16"#$prodsqlserver # replace
 					}
                 } else {
                     $connstr += $_ # keep
