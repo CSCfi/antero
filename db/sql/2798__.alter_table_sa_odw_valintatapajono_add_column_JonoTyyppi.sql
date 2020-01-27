883755b78ca06711a22096558e28ae40d9cb5a77
Heikki Petrell <hpetrell@csc.fi>
2020-01-27 13:14:50 +0200

    CSCVIPUNEN-1784 - fixed incorrect rule in SQL statement

--- db/sql/2798__.alter_table_sa_odw_valintatapajono_add_column_JonoTyyppi.sql
+++ db/sql/2798__.alter_table_sa_odw_valintatapajono_add_column_JonoTyyppi.sql
@@ -2,7 +2,7 @@ USE ANTERO
 IF NOT EXISTS (
   select * from INFORMATION_SCHEMA.COLUMNS
   where TABLE_SCHEMA='sa' and TABLE_NAME='sa_odw_valintatapajono'
-  and COLUMN_NAME ='Kokonaispisteet'
+  and COLUMN_NAME ='JonoTyyppi'
 ) BEGIN
 
 ALTER TABLE [ANTERO].[sa].[sa_odw_valintatapajono] ADD [JonoTyyppi] [nvarchar](255) NULL
