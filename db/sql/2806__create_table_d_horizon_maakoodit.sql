d4d40c12004ae64ff9cf34bc290fea4f2ec3c598
vavainio <vavainio@DWITESTDESK3>
2020-02-03 15:38:54 +0200

    CSCVIPUNEN-1760 - create table d_horizon_maakoodit

--- db/sql/2806__create_table_d_horizon_maakoodit.sql
+++ db/sql/2806__create_table_d_horizon_maakoodit.sql
@@ -1,13 +1,19 @@
-USE ANTERO
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[d_horizon_maakoodit]    Script Date: 3.2.2020 15:34:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[d_horizon_maakoodit](
+	[eu_koodi] [varchar](2) NULL,
+	[iso_koodi] [varchar](4) NULL,
+	[Maa] [varchar](44) NULL
+) ON [PRIMARY]
 
 GO
 
-SELECT 
-	s.eu_koodi,
-	s.isoCode as iso_koodi,
-	s.name as Maa 
-	INTO
-	ANTERO.dw.d_horizon_maakoodit
-	FROM
-sa.sa_horizon_maakoodit s
 
