ee070cb92dc10028b2d211ac7a13e7f568317473
vavainio <vavainio@DWITESTDESK3>
2020-02-03 15:53:52 +0200

    CSCVIPUNEN-1760 - create table d_horizon_maakoodit

--- db/sql/2806__create_table_d_horizon_maakoodit.sql
+++ db/sql/2806__create_table_d_horizon_maakoodit.sql
@@ -1,15 +1,9 @@
 USE [ANTERO]
+
 GO
 
-DROP TABLE [dw].[d_horizon_maakoodit]
-GO
-
-/****** Object:  Table [dw].[d_horizon_maakoodit]    Script Date: 3.2.2020 15:34:08 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[d_horizon_maakoodit]') AND type in (N'U'))
+BEGIN
 
 CREATE TABLE [dw].[d_horizon_maakoodit](
 	[eu_koodi] [varchar](2) NULL,
@@ -17,6 +11,4 @@ CREATE TABLE [dw].[d_horizon_maakoodit](
 	[Maa] [varchar](44) NULL
 ) ON [PRIMARY]
 
-GO
-
-
+END
