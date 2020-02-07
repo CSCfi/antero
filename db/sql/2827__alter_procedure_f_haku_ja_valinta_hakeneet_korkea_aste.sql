7bded6fb5cc15d629d9acab07835bb7d9db82a8c
vavainio <vavainio@DWITESTDESK3>
2020-02-07 10:01:46 +0200

    CSCVIPUNEN-1548 - alter procedure f_haku_ja_valinta_hakeneet_korkea_aste

--- db/sql/2827__alter_procedure_f_haku_ja_valinta_hakeneet_korkea_aste.sql
+++ db/sql/2827__alter_procedure_f_haku_ja_valinta_hakeneet_korkea_aste.sql
@@ -1,13 +1,12 @@
 USE [ANTERO]
 GO
 
-/****** Object:  StoredProcedure [dw].[p_lataa_f_haku_ja_valinta_hakeneet_korkea_aste]    Script Date: 7.2.2020 9:44:27 ******/
+/****** Object:  StoredProcedure [dw].[p_lataa_f_haku_ja_valinta_hakeneet_korkea_aste]    Script Date: 7.2.2020 9:48:37 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 
 
@@ -80,9 +79,6 @@ d_hakukohde_kuudes_hakukohde_id,
 d_koulutus_kuudes_hakukohde_id,
 d_organisaatioluokitus_kuudes_hakukohde_id,
 d_tutkinnon_aloitussykli_kuudes_hakukohde_id,
-d_valintatapajono_id,
-valintatapajono_nimi,
-kokonaispisteet,
 loadtime,
 source,
 username
@@ -147,9 +143,6 @@ coalesce(d26b.id,'-1') as d_hakukohde_kuudes_hakukohde_id,
 coalesce(d26e.id,'-1') as d_koulutus_kuudes_hakukohde_id,
 coalesce(d26f.id,'-1') as d_organisaatioluokitus_kuudes_hakukohde_id,
 coalesce(d26g.id,'-1') as d_tutkinnon_aloitussykli_kuudes_hakukohde_id,
---coalesce(d34.id,'-1') as d_valintatapajono_id,
---d33.JonoNimi,
---d33.Kokonaispisteet as kokonaispisteet, 
 getdate(),
 'ETL: p_lataa_f_haku_ja_valinta_hakeneet_korkea_aste' AS source,
 suser_name()
@@ -241,8 +234,9 @@ left join (
 		WHEN MONTH(d8.loppuPvm) < 8 AND d16.koulutuksen_alkamiskausi = 'k' THEN d8.loppuPvm
 		ELSE cast((cast((YEAR(d8.loppuPvm) + 1) as varchar(4)) + '-01-01') as date) END
 
---left join sa.sa_odw_valintatapajono d33 on d33.HakuOID = f.HakuOid and d33.HakukohdeOID = f.HakukohdeOID and d33.HenkiloOID = f.HenkiloOID
---left join dw.d_valintatapajono d34 on d34.koodi = d33.JonoTyyppi
+
+
+
 
 
 
