b853fca448f123e0a866d4e77427789af08a4391
pankalai <anssi.pankalainen@csc.fi>
2020-03-22 18:38:59 +0200

    CSCVIPUNEN-1363: alter view and valintatapajono procedure

---
+++ db/sql/2906__alter_procedure_d_valintatapajono.sql
@@ -0,0 +1,84 @@
+USE [ANTERO]
+GO
+/****** Object:  StoredProcedure [dw].[p_lataa_d_valintatapajono]    Script Date: 22.3.2020 18:26:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dw].[p_lataa_d_valintatapajono] AS
+if not exists (select * from dw.d_valintatapajono where id=-1) begin
+  set identity_insert dw.d_valintatapajono on;
+  insert into dw.d_valintatapajono (
+    id,
+    koodi,
+    selite_fi,
+    selite_sv,
+    selite_en,
+	jarjestys,
+    source
+  )
+  select
+    -1,
+    koodi,nimi,nimi_sv,nimi_en,
+	'öö',
+    source
+  from sa.sa_koodistot
+  where koodisto='vipunenmeta'
+  and koodi='-1'
+  ;
+  set identity_insert dw.d_valintatapajono off;
+end else begin
+  update d
+  set
+    koodi=s.koodi,
+    selite_fi=s.nimi,
+    selite_sv=s.nimi_sv,
+    selite_en=s.nimi_en,
+	jarjestys='öö',
+    d.source=s.source
+  from dw.d_valintatapajono d
+  join sa.sa_koodistot s on s.koodi=d.koodi
+  where s.koodisto='vipunenmeta'
+  and s.koodi='-1'
+  ;
+end
+
+MERGE dw.d_valintatapajono AS target
+USING (
+  SELECT
+    koodi,
+    COALESCE(nimi, nimi_sv, nimi_en) AS nimi,
+    COALESCE(nimi_sv, nimi, nimi_en) AS nimi_sv,
+    COALESCE(nimi_en, nimi, nimi_sv) AS nimi_en,
+	jarjestys = koodi,
+    source
+  FROM sa.sa_koodistot
+  where koodisto='valintatapajono'
+) AS src
+ON target.koodi = src.koodi
+
+WHEN MATCHED THEN
+  UPDATE SET
+    selite_fi = src.nimi,
+    selite_sv = src.nimi_sv,
+    selite_en = src.nimi_en,
+	jarjestys = src.koodi,
+    target.source = src.source
+
+WHEN NOT MATCHED THEN
+  INSERT (
+    koodi,
+    selite_fi,
+    selite_sv,
+    selite_en,
+	jarjestys,
+    source
+  )
+  VALUES (
+    src.koodi, src.nimi, src.nimi_sv, src.nimi_en, src.jarjestys,
+    src.source
+  )
+  ;
+  
