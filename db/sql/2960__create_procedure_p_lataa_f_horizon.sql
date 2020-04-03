4ce7cce3949eee318111519035815dde64214c01
vavainio <vavainio@DWITESTDESK3>
2020-04-03 09:09:24 +0300

    CSCVIPUNEN-1760 - Create procedure p_lataa_f_horizon

---
+++ db/sql/2960__create_procedure_p_lataa_f_horizon.sql
@@ -0,0 +1,46 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_horizon]   ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[p_lataa_f_horizon]') AND type in (N'P', N'PC')) AND EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[sa_horizon_organisaatioiden_rahoitus]') AND type in (N'U'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dw].[p_lataa_f_horizon] AS' 
+END
+GO
+
+ALTER PROCEDURE [dw].[p_lataa_f_horizon] AS
+
+TRUNCATE TABLE dw.f_horizon
+INSERT INTO dw.f_horizon (	
+	projectRcn,
+	projectID,
+	id,
+	projectAcronym,
+	[name],
+	shortName,
+	[role],
+	endOfParticipation,
+	country,
+	ecContribution
+)
+Select	
+	projectRcn,
+	projectID,
+	id,
+	projectAcronym,
+	[name],
+	shortName,
+	[role],
+	endOfParticipation,
+	country,
+	ecContribution
+FROM [ANTERO].[sa].[sa_horizon_organisaatioiden_rahoitus]
+
+
