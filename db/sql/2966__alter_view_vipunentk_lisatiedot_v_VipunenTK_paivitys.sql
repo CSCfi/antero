5330bdcc43a4d72f7594f6d10156d911c76abacb
jsyrjala2 <jsyrjala2@DWITESTDESK3>
2020-04-03 11:16:25 +0300

    CSCVIPUNEN-1919: update v_sa_4_11, alter vipunentk lisatiedot view and procs

---
+++ db/sql/2966__alter_view_vipunentk_lisatiedot_v_VipunenTK_paivitys.sql
@@ -0,0 +1,60 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  View [dbo].[v_VipunenTK_paivitys]    Script Date: 3.4.2020 11:09:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+ALTER VIEW [dbo].[v_VipunenTK_paivitys]
+as
+
+SELECT distinct 
+tk.Taulu as aineisto
+,afp.aineisto_avain
+,proseduuri
+,k.kuutio_nimi
+
+FROM [TK_H9098_CSC].[dbo].[TKsiirto_muutokset] tk
+left join [VipunenTK_lisatiedot].[dbo].[Vipunen_aineisto_fakta_proseduuri] afp on	
+	REPLACE(REPLACE(afp.aineisto_avain, 'TK_K', 'TK_'), 'TK_R', 'TK_')
+	=
+	REPLACE(REPLACE(CASE 
+						WHEN LEN(LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 1)) > 9 
+						THEN LEFT(Taulu, LEN(LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 6))) 
+						ELSE LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 1) 
+					END
+					, 'TK_K'
+					, 'TK_'
+					)
+			, 'TK_R'
+			, 'TK_'
+			)
+left join VipunenTK_lisatiedot.dbo.Vipunen_aineisto_kuutio ak on ak.aineisto_avain=afp.aineisto_avain
+left join VipunenTK_lisatiedot.dbo.Vipunen_kuutio k on k.kuutio_avain=ak.kuutio_avain
+
+where /*siirtoaika=(select max(siirtoaika) from [TK_H9098_CSC].[dbo].[TKsiirto_muutokset]) and*/ 
+proseduuri is not null
+ 
+
+
+
+
+
+
+
+GO
+
+
+USE [ANTERO]
