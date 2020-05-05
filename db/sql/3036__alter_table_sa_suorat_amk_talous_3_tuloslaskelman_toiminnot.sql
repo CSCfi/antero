e0b6f22fbce8fbf77f6cc37789110bc0cd6a7ba2
villehoo <ville.hamalainen@csc.fi>
2020-05-05 10:08:07 +0300

    CSCVIPUNEN-1790 /1789  add new columns

--- db/sql/3036__alter_table_sa_suorat_amk_talous_3_tuloslaskelman_toiminnot.sql
+++ db/sql/3036__alter_table_sa_suorat_amk_talous_3_tuloslaskelman_toiminnot.sql
@@ -3,11 +3,11 @@ USE ANTERO
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_amk_talous_3_tuloslaskelman_toiminnot'
 AND COLUMN_NAME ='KULUTYHT')
 BEGIN
-	ALTER TABLE [sa].[sa_suorat_amk_talous_3_tuloslaskelman_toiminnot]  ADD COLUMN [KULUTYHT] int  NULL
+	ALTER TABLE [sa].[sa_suorat_amk_talous_3_tuloslaskelman_toiminnot]  ADD  [KULUTYHT] int  NULL
 END
 
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='sa' and TABLE_NAME='sa_suorat_amk_talous_3_tuloslaskelman_toiminnot'
 AND COLUMN_NAME ='KULKAT')
 BEGIN
-	ALTER TABLE [sa].[sa_suorat_amk_talous_3_tuloslaskelman_toiminnot]  ADD COLUMN [KULKAT] int  NULL
+	ALTER TABLE [sa].[sa_suorat_amk_talous_3_tuloslaskelman_toiminnot]  ADD [KULKAT] int  NULL
 END
