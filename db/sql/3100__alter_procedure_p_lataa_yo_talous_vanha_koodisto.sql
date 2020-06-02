8160217ff7cc57d76f8a525f9fd71d70e477e33b
villehoo <ville.hamalainen@csc.fi>
2020-06-02 17:05:04 +0300

    CSCVIPUNEN-1999 fix procedure

--- db/sql/3100__alter_procedure_p_lataa_yo_talous_vanha_koodisto.sql
+++ db/sql/3100__alter_procedure_p_lataa_yo_talous_vanha_koodisto.sql
@@ -283,7 +283,7 @@ select d1.id as d_yo_id
 			  ,hlokulutyht)) as unpvt
 			  --where aineistotyyppi_osa_1 = case when (select count(*) from sa.sa_suorat_yo_talous_1_tuloslaskelma where tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and yliop_koodi = unpvt.yliop_koodi) > 0 then 'L' else 'E' end
 			  --rajoittaa uusien talouskoodistojen luvut fakta-taulusta
-      	   WHERE aineistotyyppi = case when (select count(*) from sa.sa_suorat_yo_talous_1_tuloslaskelma  where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and amk_koodi = unpvt.amk_koodi) > 0 then 'L' else 'E' end
+      	   WHERE Aineistotyyppi_osa_1 = case when (select count(*) from sa.sa_suorat_yo_talous_1_tuloslaskelma  where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and YLIOP_koodi = unpvt.YLIOP_koodi) > 0 then 'L' else 'E' end
       	   AND TILIK < 2019
 
 	union all
@@ -505,8 +505,8 @@ select d1.id as d_yo_id
 			  ,yjrah
 			  ,veropvara)) as unpvt
         --rajoittaa vanhan talouskoodiston lukuihin
-        WHERE aineistotyyppi_osa_1 = case when (select count(*) frrom sa.sa_suorat_yo_talous_2_tase where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and amk_koodi = unpvt.amk_koodi) > 0 then 'L' else 'E' end
-        AND TILIK < 2019)
+        WHERE aineistotyyppi_osa_1 = case when (select count(*) from sa.sa_suorat_yo_talous_2_tase where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and YLIOP_koodi = unpvt.YLIOP_koodi) > 0 then 'L' else 'E' end
+        AND TILIK < 2019
 
 	union all
 
@@ -822,7 +822,7 @@ select d1.id as d_yo_id
 			,rahoitustuotot_ja_kulut)) as unpvt
       --where aineistotyyppi_osa_1 = case when (select count(*) from sa.sa_suorat_talous_6_liiketoiminnan_tuloslaskelma where tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and amk_koodi = unpvt.amk_koodi) > 0 then 'L' else 'E' end
       --rajoittaa uusien talouskoodistojen luvut fakta-taulusta
-      WHERE aineistotyyppi = case when (select count(*) from sa.sa_suorat_amk_talous_6_liiketoiminnan_tuloslaskelma where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi_osa_1 = 'L' and amk_koodi = unpvt.amk_koodi) > 0 then 'L' else 'E' end
+      WHERE aineistotyyppi = case when (select count(*) from sa.sa_suorat_amk_talous_6_liiketoiminnan_tuloslaskelma where tilik < 2018 and tilik = unpvt.tilik and aineistotyyppi = 'L' and yliop_koodi = unpvt.yliop_koodi) > 0 then 'L' else 'E' end
       AND TILIK < 2019
 ) f
   inner join dw.d_yo d1 on f.yliop_koodi = d1.yo_tunnus
