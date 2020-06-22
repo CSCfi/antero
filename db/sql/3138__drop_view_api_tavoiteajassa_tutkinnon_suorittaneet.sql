d5674afa80e7714006633db8545a867ee38fade7
jsyrjala2 <jsyrjala2@DWITESTDESK3>
2020-06-22 17:50:09 +0300

    remove table manually from test before drop view

--- db/sql/3138__drop_view_api_tavoiteajassa_tutkinnon_suorittaneet.sql
+++ db/sql/3138__drop_view_api_tavoiteajassa_tutkinnon_suorittaneet.sql
@@ -2,5 +2,5 @@ USE [ANTERO]
 GO
 
 /****** Object:  View [api].[tavoiteajassa_tutkinnon_suorittaneet]    Script Date: 22.6.2020 16:32:06 ******/
-DROP TABLE IF EXISTS [api].[tavoiteajassa_tutkinnon_suorittaneet]
+
 DROP VIEW IF EXISTS [api].[tavoiteajassa_tutkinnon_suorittaneet]
