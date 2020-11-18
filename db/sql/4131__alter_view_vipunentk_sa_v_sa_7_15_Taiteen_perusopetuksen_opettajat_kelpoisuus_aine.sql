97d18da59804effbc5a50ea2c19da07afafb4f15
jsyrjala2 <jsyrjala2@DWITESTDESK3>
2020-11-18 15:57:40 +0200

    CSCVIPUNEN-2175: alter tk sa views 2_23, 3_8, 7_15 and 7_17, update tk auto-update scripts for tpo _vos_ editions

---
+++ db/sql/4131__alter_view_vipunentk_sa_v_sa_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine.sql
@@ -0,0 +1,91 @@
+USE [VipunenTK_SA]
+GO
+
+/****** Object:  View [dbo].[v_sa_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]    Script Date: 17.11.2020 19:36:40 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER VIEW [dbo].[v_sa_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine] AS
+
+/*
+Drop table [VipunenTK_DW].[dbo].[f_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]
+Select * into [VipunenTK_DW].[dbo].[f_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine] FROM [VipunenTK_SA].[dbo].[v_sa_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]
+Truncate table [VipunenTK_DW].[dbo].[f_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]
+Insert into [VipunenTK_DW].[dbo].[f_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]
+Select *  FROM [VipunenTK_SA].[dbo].[v_sa_7_15_Taiteen_perusopetuksen_opettajat_kelpoisuus_aine]
+*/
+
+SELECT [tilv] = CAST(kaikkivuodet.tilv as nvarchar(4))
+      ,[tilv_date] = Cast(kaikkivuodet.tilv+'0101' as date)
+	  ,[t_okieli] = Cast(kaikkivuodet.t_okieli as nvarchar(4))
+	  ,[t_tyos] = Cast(kaikkivuodet.t_tyos as nvarchar(3))
+      ,[t_virka] = Cast(kaikkivuodet.t_virka as nvarchar(3))
+      ,[t_kelp] = Cast(kaikkivuodet.t_kelp as nvarchar(3))
+      ,[t_p1] = Cast(kaikkivuodet.t_p1 as nvarchar(3))
+      ,[t_p2] = Cast(kaikkivuodet.t_p2 as nvarchar(3))
+	  ,[t_otaso] = Cast(kaikkivuodet.t_otaso as nvarchar(3))
+      ,[t_ala1] = Cast(kaikkivuodet.t_ala1 as nvarchar(3))
+	  ,[t_aine1] = Cast(kaikkivuodet.t_aine1 as nvarchar(3))
+      ,[t_sp] = Cast(kaikkivuodet.t_sp as nvarchar(2))
+      ,[t_maakunta] = Cast(kaikkivuodet.t_maakunta as nvarchar(2))
+	  ,[t_okunryh] =  Cast(kaikkivuodet.t_okunryh as nvarchar(2))
+	  ,[t_ika] = Cast(kaikkivuodet.t_ika as nvarchar(10))
+	  ,[t_osa1] = Cast(kaikkivuodet.t_osa1 as nvarchar(10))
+	  ,[t_osa2] = Cast(kaikkivuodet.t_osa2 as nvarchar(10))
+      ,[tietolahde] = CAST(kaikkivuodet.tietolahde as nvarchar(50))
+      ,[rivinumero] = CAST(kaikkivuodet.rivinumero as int)
+  FROM (
+  
+  SELECT [tilv]
+      ,[t_okieli]
+	  ,[t_tyos]
+      ,[t_virka]
+      ,[t_kelp]
+      ,[t_p1] = coalesce(t_p1,0)
+      ,[t_p2] = coalesce(t_p2,0)
+	  ,[t_otaso]
+      ,[t_ala1]
+      ,[t_aine1]
+      ,[t_sp]
+      ,[t_maakunta]
+      ,[t_okunryh]
+      ,[t_ika] = case [t_ika]
+	   when '01' then '30655v30'
+	   when '02' then '19655v30'
+	   when '03' then '19655v35'
+	   when '04' then '19655v40'
+	   when '05' then '19655v45'
+	   when '06' then '19655v50'
+	   when '07' then '55'
+	   when '08' then '56'
+	   when '09' then '57'
+	   when '10' then '58'
+	   when '11' then '59'
+	   when '12' then '60'
+	   when '13' then '61'
+	   when '14' then '62'
+	   when '15' then '63'
+	   when '16' then '64'
+	   when '17' then '19655v65'
+	   else '-1'
+	   end
+	   ,[t_osa1]
+	   ,[t_osa2]
+      ,[tietolahde]='7_15_sopv_19'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+  FROM [TK_H9098_CSC].[dbo].[TK_K7_15_vos_sopv_19]
+
+  --UNION ALL
+
+) kaikkivuodet
+
+
+
+GO
+
+
+USE [ANTERO]
---
+++ db/sql/4134__alter_proc_vipunentk_lisatiedot_lataa_muuttuneet_tiedot_Vipunen_automaattipaivitys_fakta.sql
@@ -0,0 +1,61 @@
+USE [VipunenTK_lisatiedot]
+GO
+/****** Object:  StoredProcedure [dbo].[lataa_muuttuneet_tiedot_Vipunen_automaattipaivitys_fakta]    Script Date: 17.11.2020 19:51:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [dbo].[lataa_muuttuneet_tiedot_Vipunen_automaattipaivitys_fakta] AS
+
+--Vertaa [TK_H9098_CSC].[dbo].[TKsiirto_muutokset] ja [VipunenTK_lisatiedot].[dbo].[Vipunen_automaattipaivitys_fakta] tietoja, ja lisää jälkimmäiseen puuttuvat rivit siten että faktalataus_suoritettu saa arvon nolla.
+
+MERGE dbo.Vipunen_automaattipaivitys_fakta AS target
+USING 
+(
+  SELECT distinct
+	Ajopvm
+	,Taulu
+  FROM TK_H9098_CSC.[dbo].[TKsiirto_muutokset]
+  WHERE muutos in ('B Uusi taulu jossa sisältöä','C Rivilkm muuttunut, ei nollaan','D Taulu tyhjentynyt','G Kentän summa muuttunut')
+) AS src
+ON target.Ajopvm = src.Ajopvm and target.Taulu = src.Taulu
+
+WHEN NOT MATCHED 
+AND /*varmistetaan että aineistoa käyttävän faktan proseduuri ei ole asetettu nulliksi*/ 
+src.Taulu IN
+(
+	SELECT distinct tk.Taulu
+	FROM [TK_H9098_CSC].[dbo].[TKsiirto_muutokset] tk
+	LEFT JOIN [VipunenTK_lisatiedot].[dbo].[Vipunen_aineisto_fakta_proseduuri] afp on	
+		REPLACE(REPLACE(afp.aineisto_avain, 'TK_K', 'TK_'), 'TK_R', 'TK_')
+		=
+		REPLACE(REPLACE(CASE 
+							WHEN Taulu LIKE '%_vos_%' THEN LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 5) --tpo 7.1X sopv19
+							WHEN LEN(LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 1)) > 9 THEN LEFT(Taulu, LEN(LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 6))) --sopv sis. monta tilv
+							ELSE LEFT(Taulu, CHARINDEX('_sopv', Taulu) - 1) 
+						END
+						, 'TK_K'
+						, 'TK_'
+						)
+				, 'TK_R'
+				, 'TK_'
+				)
+	WHERE proseduuri is not null
+)
+
+THEN
+  INSERT (
+    Ajopvm
+	,Taulu
+	,Faktalataus_suoritettu
+  )
+  VALUES (
+    Ajopvm
+	,Taulu
+	,0   
+  );
+
+USE [ANTERO]
