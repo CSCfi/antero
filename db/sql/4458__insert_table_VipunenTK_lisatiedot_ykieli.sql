f6f89622a0f0525dbb9ca390916187c7f879722c
jsyrjala2 <jsyrjala2@DWITESTDESK3>
2021-03-06 00:41:46 +0200

    CSCVIPUNEN-2397: necessary etl changes for TK ssis to run

---
+++ db/sql/4458__insert_table_VipunenTK_lisatiedot_ykieli.sql
@@ -0,0 +1,27 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+
+if not exists (select * from [VipunenTK_lisatiedot].[dbo].[ykieli] where ykieli_koodi = '8' or id = 5)
+
+begin
+
+  insert into [VipunenTK_lisatiedot].[dbo].[ykieli]
+
+  select [id] = 5
+      ,[luotu] = '2020-03-05'
+      ,[alkaa]
+      ,[paattyy]
+      ,[ykieli_koodi] = '8'
+      ,[koodinselite]
+      ,[koodinselite_SV]
+      ,[koodinselite_EN]
+      ,[jarjestys]
+      ,[virhetilanne]
+      ,[poistettu]
+      ,[tietolahde]
+      ,[kommentti]
+  from [VipunenTK_lisatiedot].[dbo].[ykieli]
+  where id = -1
+
+end
+
+go use antero
