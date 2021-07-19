c0fb39abcecd38fb5394ae723e333e9ed2509c3e
vavainio <vavainio@DWITESTDESK3>
2021-07-19 15:43:01 +0300

    CSCVIPUNEN-2088 - Altered PowerShell script

--- PBI/PowerShell/PBI_Dataset_Refresh.ps1
+++ PBI/PowerShell/PBI_Dataset_Refresh.ps1
@@ -12,6 +12,25 @@ Where {$_.Name -eq $env:PBIDS} | ForEach {$_.Id}
 
 $RefreshDS = 'groups/' + $WSID + '/datasets/' + $DSID + '/refreshes'
 
+$RefreshDSCheck = 'groups/' + $WSID + '/datasets/' + $DSID + '/refreshes?$top=1'
+
 $MailFailureNotify = @{"notifyOption"="MailOnFailure"}
 
 Invoke-PowerBIRestMethod -Url $RefreshDS -Method Post -Body $MailFailureNotify
+
+$RefreshStatus = "Unknown"
+
+while ($RefreshStatus -eq "Unknown")
+{	
+	$RefreshStatus = (ConvertFrom-Json (Invoke-PowerBIRestMethod -Url $RefreshDSCheck -Method Get)).value.status
+	ECHO "Inspecting refresh status..."
+	Start-Sleep -s 30
+}
+
+if ($RefreshStatus -eq "Completed") {
+    ECHO "Refresh was completed"
+} elseif ($RefreshStatus -eq "Failed") {
+    ECHO "Refresh failed"
+} else {
+    ECHO "Something unexpected happened"
+}
