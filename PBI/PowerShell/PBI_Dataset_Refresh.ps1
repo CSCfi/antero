9f9aac127869c67eef808919629bba0011cd0565
vavainio <vavainio@DWITESTDESK3>
2021-07-20 09:09:10 +0300

    CSCVIPUNEN-2088 - Added transformation

--- PBI/PowerShell/PBI_Dataset_Refresh.ps1
+++ PBI/PowerShell/PBI_Dataset_Refresh.ps1
@@ -3,10 +3,13 @@ $SecPasswd = ConvertTo-SecureString $env:PBIPW -AsPlainText -Force
 
 $Cred = New-Object System.Management.Automation.PSCredential($env:PBIUN,$SecPasswd)
 
+# Login to Power BI
 Connect-PowerBIServiceAccount -Credential $Cred
 
+# Get workspaceid
 $WSID = (Get-PowerBIWorkspace -Name $env:PBIWS -Scope Individual).Id
 
+# Get datasetid
 $DSID = Get-PowerBIDataset -Scope Individual -WorkspaceId $WSID | `
 Where {$_.Name -eq $env:PBIDS} | ForEach {$_.Id}
 
@@ -16,11 +19,16 @@ $RefreshDSCheck = 'groups/' + $WSID + '/datasets/' + $DSID + '/refreshes?$top=1'
 
 $MailFailureNotify = @{"notifyOption"="MailOnFailure"}
 
+# Refresh dataset
 Invoke-PowerBIRestMethod -Url $RefreshDS -Method Post -Body $MailFailureNotify
 
+# Refresh status of dataset
 $RefreshStatus = "Unknown"
 
-while ($RefreshStatus -eq "Unknown")
+$limit = (Get-Date).AddMinutes(90)
+
+# While loop runs until dataset refresh has been completed or failed. Time limit for the loop is 90 minutes. 
+while ($RefreshStatus -eq "Unknown" -and (Get-Date) -le $limit)
 {	
 	$RefreshStatus = (ConvertFrom-Json (Invoke-PowerBIRestMethod -Url $RefreshDSCheck -Method Get)).value.status
 	ECHO "Inspecting refresh status..."
@@ -34,3 +42,6 @@ if ($RefreshStatus -eq "Completed") {
 } else {
     ECHO "Something unexpected happened"
 }
+
+# Saves refresh status in a file
+Out-File -FilePath D:\PBI\RefreshStatus\Status.csv -InputObject $RefreshStatus -Encoding ASCII -Width 50
