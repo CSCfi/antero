6996fb85f000e87a9a6ed6c41e74dd8b0d708cd5
vavainio <vavainio@DWITESTDESK3>
2021-08-03 12:58:13 +0300

    CSCVIPUNEN-2088 - Added new transformation

--- PBI/PowerShell/PBI_Dataset_Refresh.ps1
+++ PBI/PowerShell/PBI_Dataset_Refresh.ps1
@@ -21,15 +21,17 @@ $MailFailureNotify = @{"notifyOption"="MailOnFailure"}
 
 $error.clear()
 
+$startTime = Get-Date
+
 # Refresh dataset and error handling
 Invoke-PowerBIRestMethod -Url $RefreshDS -Method Post -Body $MailFailureNotify
 if (!$error) {
 	# Refresh status of dataset
 	$RefreshStatus = "Unknown"
 
-	$limit = (Get-Date).AddMinutes(90)
+	$limit = (Get-Date).AddMinutes(60)
 
-	# While loop runs until dataset refresh has been completed or failed. Time limit for the loop is 90 minutes. 
+	# While loop runs until dataset refresh has been completed or failed. Time limit for the loop is 60 minutes. 
 
 	ECHO "################################################"
 
@@ -53,3 +55,13 @@ if (!$error) {
 } else {
 	ECHO "Refresh failed"
 }
+
+$endTime = Get-Date
+
+$duration = $endTime - $startTime
+
+$duration = $duration.ToString("hh\:mm\:ss")
+
+$output = "$env:PBIDS;$RefreshStatus;$startTime;$endTime;$duration"
+
+Out-File -FilePath D:\PBI\RefreshStatus\Refresh.csv -InputObject $output -Encoding ASCII -Width 50
