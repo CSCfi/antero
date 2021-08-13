a2b3bf14bccf62b0dacea42e7594d028e06f187a
villehoo <ville.hamalainen@csc.fi>
2021-08-13 11:25:12 +0300

    debugging load_p3_v2.py

--- db/python/load_v2_p3.py
+++ db/python/load_v2_p3.py
@@ -38,7 +38,7 @@ def load(secure,hostname,url,schema,table,postdata,condition,verbose,rowcount):
 
   # automatic POST with (post)data
   print("value used for , -r, --rowcount=", rowcount)
-  request = Request(address, data=postdata, headers=reqheaders)
+  request = Request(address, data=postdata, headers=reqheaders, timeout=300)
   try:
     response = urlopen(request)
   except http.client.IncompleteRead as e:
