947dcb00be1354e7a74be75514b2a96faf5596f1
vavainio <vavainio@DWITESTDESK3>
2021-09-01 08:09:30 +0300

    CSCVIPUNEN-2584 - Create table f_haku_ja_valinta_korkea_aste_valintatapajonot

---
+++ db/sql/4823__create_table_f_haku_ja_valinta_korkea_aste_valintatapajonot.sql
@@ -0,0 +1,30 @@
+USE [ANTERO]
+
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[f_haku_ja_valinta_korkea_aste_valintatapajonot]') AND type in (N'U'))
+BEGIN
+
+CREATE TABLE [dw].[f_haku_ja_valinta_korkea_aste_valintatapajonot](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[henkilo_oid] [varchar](255) NULL,
+	[haku_oid] [varchar](255) NULL,
+	[d_hakukohde_id] [varchar](255) NULL,
+	[d_valintatapajono_id] [int] NULL,
+	[valintatapajono_nimi] [varchar](250) NULL,
+	[kokonaispisteet] [decimal](12, 2) NULL,
+	[paatelty] [int] NULL,
+	[loadtime] [datetime] NULL,
+	[source] [varchar](250) NOT NULL,
+	[username] [varchar](250) NULL,
+ CONSTRAINT [PK__f_haku_ja_valinta_korkea_aste_valintatapajonot] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+END
+
+
