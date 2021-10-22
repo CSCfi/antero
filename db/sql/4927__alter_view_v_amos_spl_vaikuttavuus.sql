0f257ef1196a65b499a4c7fb6eaa39b176a8209f
eskarppi <eskarppi@DWITESTDESK3>
2021-10-22 13:14:45 +0300

    CSCVIPUNEN2666

--- db/sql/4927__alter_view_v_amos_spl_vaikuttavuus.sql
+++ db/sql/4927__alter_view_v_amos_spl_vaikuttavuus.sql
@@ -1,4 +1,4 @@
-USE [ANTERO]
+ï»¿USE [ANTERO]
 GO
 
 /****** Object:  View [dw].[v_amos_spl_vaikuttavuus]    Script Date: 22.10.2021 12:55:52 ******/
@@ -27,15 +27,15 @@ SELECT
       ,[Tilastovuosi]
 
 -- ORGANISAATIOMUUTTUJAT
-      ,[nimi_amos_spl] AS [Koulutuksen järjestäjä]
-	  ,Suoritepäätös
- --     ,[Koulutuksen järjestäjä (opiskelun aikainen)]
-      ,[Koulutuksen järjestäjä (tutkinnon)]
+      ,[nimi_amos_spl] AS [Koulutuksen jÃ¤rjestÃ¤jÃ¤]
+	  ,SuoritepÃ¤Ã¤tÃ¶s
+ --     ,[Koulutuksen jÃ¤rjestÃ¤jÃ¤ (opiskelun aikainen)]
+      ,[Koulutuksen jÃ¤rjestÃ¤jÃ¤ (tutkinnon)]
 
 -- KOULUTUSMUUTTUJAT
 
       ,[Koulutuksen tavoite]
-      ,[Työvoimapoliittinen koulutus] AS [Työvoimakoulutus]
+      ,[TyÃ¶voimapoliittinen koulutus] AS [TyÃ¶voimakoulutus]
       ,[Kuntakoodi_tutkinnon]
       ,[Koulutusaste, taso 2]
       ,[Koulutusala, taso 1]
@@ -45,60 +45,60 @@ SELECT
       ,[paa_asiallinen_toiminta_maakunta_koodi] AS [Koodi Maakunta]
       ,[Kuntakoodi_opiskelunaikainen]
 
--- LÄHTÖTILANNEMUUTTUJAT
+-- LÃ„HTÃ–TILANNEMUUTTUJAT
 
       ,[Koulutusaste, taso 2, pohjakoulutus]
-      ,COALESCE([Pääasiallinen toiminta, ennen koulutusta, vuoden lopussa], 'Muu tai tuntematon') 
-		AS [Pääasiallinen toiminta, ennen koulutusta, vuoden lopussa]
+      ,COALESCE([PÃ¤Ã¤asiallinen toiminta, ennen koulutusta, vuoden lopussa], 'Muu tai tuntematon') 
+		AS [PÃ¤Ã¤asiallinen toiminta, ennen koulutusta, vuoden lopussa]
       ,[Koulutusaste, taso 2, ennen koulutusta, vuoden lopussa]
       ,[Ammattiasema, ennen koulutusta, vuoden lopussa]
-      ,COALESCE([Pääasiallinen toiminta, ennen koulutusta, heinäkuun lopussa], 'Muu tai tuntematon')
-		AS [Pääasiallinen toiminta, ennen koulutusta, heinäkuun lopussa]
-      ,[Koulutusaste, taso 2, ennen koulutusta, heinäkuun lopussa]
-      ,[Ammattiasema, ennen koulutusta, heinäkuun lopussa]
+      ,COALESCE([PÃ¤Ã¤asiallinen toiminta, ennen koulutusta, heinÃ¤kuun lopussa], 'Muu tai tuntematon')
+		AS [PÃ¤Ã¤asiallinen toiminta, ennen koulutusta, heinÃ¤kuun lopussa]
+      ,[Koulutusaste, taso 2, ennen koulutusta, heinÃ¤kuun lopussa]
+      ,[Ammattiasema, ennen koulutusta, heinÃ¤kuun lopussa]
 
 -- LOPPUTILANNEMUUTTUJAT
 
       ,COALESCE([Sijoittuminen], 'Muu tai tuntematon')
 		AS Sijoittuminen
-      ,[Maassaolo, koulutuksen jälkeen]
-      ,[Ammattiasema, koulutuksen jälkeen]
-      ,[Koulutusaste, taso 2, koulutuksen jälkeen]
-      ,[Koulutusala, taso 1, koulutuksen jälkeen]
+      ,[Maassaolo, koulutuksen jÃ¤lkeen]
+      ,[Ammattiasema, koulutuksen jÃ¤lkeen]
+      ,[Koulutusaste, taso 2, koulutuksen jÃ¤lkeen]
+      ,[Koulutusala, taso 1, koulutuksen jÃ¤lkeen]
 
--- LUKUMÄÄRÄMUUTTUJAT
+-- LUKUMÃ„Ã„RÃ„MUUTTUJAT
       ,[v Suoritetut tutkinnot]
       ,[v Suoritetut tutkinnon osat]
-      ,[v Työllinen, koko tutkinnon suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Jatko-opiskelija, koko tutkinnon suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Työtön, koko tutkinnon suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Muu (kuin jatko-opiskelija, työllinen tai työtön), koko tutkinnon suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Työllinen, tutkinnon osia suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Jatko-opiskelija, tutkinnon osia suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Työtön, tutkinnon osia suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Muu (kuin jatko-opiskelija, työllinen tai työtön), tutkinnon osia suorittanut, ennen koulutusta ei-työllinen]
-      ,[v Työllinen, koko tutkinnon suorittanut, ennen koulutusta työllinen]
-      ,[v Jatko-opiskelija, koko tutkinnon suorittanut, ennen koulutusta työllinen]
-      ,[v Työtön, koko tutkinnon suorittanut, ennen koulutusta työllinen]
-      ,[v Muu (kuin jatko-opiskelija, työllinen tai työtön), koko tutkinnon suorittanut, ennen koulutusta työllinen]
-      ,[v Työllinen, tutkinnon osia suorittanut, ennen koulutusta työllinen]
-      ,[v Jatko-opiskelija, tutkinnon osia suorittanut, ennen koulutusta työllinen]
-      ,[v Työtön, tutkinnon osia suorittanut, ennen koulutusta työllinen]
-      ,[v Muu (kuin jatko-opiskelija, työllinen tai työtön), tutkinnon osia suorittanut, ennen koulutusta työllinen]
-      ,[v Työlliset ja opiskelijat]
-      ,[v Työlliset]
+      ,[v TyÃ¶llinen, koko tutkinnon suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v Jatko-opiskelija, koko tutkinnon suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v TyÃ¶tÃ¶n, koko tutkinnon suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v Muu (kuin jatko-opiskelija, tyÃ¶llinen tai tyÃ¶tÃ¶n), koko tutkinnon suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v TyÃ¶llinen, tutkinnon osia suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v Jatko-opiskelija, tutkinnon osia suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v TyÃ¶tÃ¶n, tutkinnon osia suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v Muu (kuin jatko-opiskelija, tyÃ¶llinen tai tyÃ¶tÃ¶n), tutkinnon osia suorittanut, ennen koulutusta ei-tyÃ¶llinen]
+      ,[v TyÃ¶llinen, koko tutkinnon suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v Jatko-opiskelija, koko tutkinnon suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v TyÃ¶tÃ¶n, koko tutkinnon suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v Muu (kuin jatko-opiskelija, tyÃ¶llinen tai tyÃ¶tÃ¶n), koko tutkinnon suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v TyÃ¶llinen, tutkinnon osia suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v Jatko-opiskelija, tutkinnon osia suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v TyÃ¶tÃ¶n, tutkinnon osia suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v Muu (kuin jatko-opiskelija, tyÃ¶llinen tai tyÃ¶tÃ¶n), tutkinnon osia suorittanut, ennen koulutusta tyÃ¶llinen]
+      ,[v TyÃ¶lliset ja opiskelijat]
+      ,[v TyÃ¶lliset]
       ,[v Opiskelijat]
-      ,[v Työlliset, opiskelijat ja työttömät]
+      ,[v TyÃ¶lliset, opiskelijat ja tyÃ¶ttÃ¶mÃ¤t]
       ,[v statuskerroin]
-      ,[v Työllistyneet, ei työllisenä ennen koul.]
-      ,[v Työllistyneet, työllisenä ennen koul.]
-      ,[v Jatko-opiskelijat, ei työllisenä ennen koul.]
-      ,[v Jatko-opiskelijat, työllisenä ennen koul.]
-      ,[v Työtön]
+      ,[v TyÃ¶llistyneet, ei tyÃ¶llisenÃ¤ ennen koul.]
+      ,[v TyÃ¶llistyneet, tyÃ¶llisenÃ¤ ennen koul.]
+      ,[v Jatko-opiskelijat, ei tyÃ¶llisenÃ¤ ennen koul.]
+      ,[v Jatko-opiskelijat, tyÃ¶llisenÃ¤ ennen koul.]
+      ,[v TyÃ¶tÃ¶n]
       ,[v Muu tai tuntematon]
       ,[v Aluekerroin]
-      ,[v Järjestäjäkerroin]
-      ,[v Työllisten painotetut pisteet]
+      ,[v JÃ¤rjestÃ¤jÃ¤kerroin]
+      ,[v TyÃ¶llisten painotetut pisteet]
       ,[v Opiskelijoiden painotetut pisteet]
       ,[v lkm]
   FROM [ANTERO].[dw].[f_amos_spl_vaikuttavuus]
