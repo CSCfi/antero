b43336bf25a2a865c9fd6e5ccd521d74409182f3
villehoo <ville.hamalainen@csc.fi>
2021-10-27 15:18:17 +0300

    try to fix sql 4950

--- db/sql/4950__create_table_d_lukukausi.sql
+++ db/sql/4950__create_table_d_lukukausi.sql
@@ -1,13 +1,16 @@
 USE [ANTERO]
 GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[d_lukukausi]') AND type in (N'U'))
+BEGIN
+	DROP TABLE [dw].[d_lukukausi]
+END
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[d_lukukausi]') AND type in (N'U'))
-begin
-/****** Object:  Table [dw].[d_lukukausi]    Script Date: 27.10.2021 12:40:39 ******/
+BEGIN
 SET ANSI_NULLS ON
-GO
+
 SET QUOTED_IDENTIFIER ON
-GO
+
 CREATE TABLE [dw].[d_lukukausi](
 	[id] [bigint] IDENTITY(1,1) NOT NULL,
 	[lukukausi_fi] [varchar](100) NOT NULL,
@@ -17,25 +20,24 @@ CREATE TABLE [dw].[d_lukukausi](
 	[loadtime] [datetime] NOT NULL,
 	[source] [varchar](100) NOT NULL,
 	[username] [varchar](30) NOT NULL,
- CONSTRAINT [PK_d_lukukausi] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_d_lukukausi] PRIMARY KEY CLUSTERED
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
-GO
+
+
 ALTER TABLE [dw].[d_lukukausi] ADD  CONSTRAINT [DF__d_lukukausi__loadtime]  DEFAULT (getdate()) FOR [loadtime]
-GO
+
 ALTER TABLE [dw].[d_lukukausi] ADD  CONSTRAINT [DF__d_lukukausi__username]  DEFAULT (suser_name()) FOR [username]
-GO
 
 
-SET IDENTITY_INSERT [dw].[d_lukukausi] ON 
+
+SET IDENTITY_INSERT [dw].[d_lukukausi] ON
 
 INSERT [dw].[d_lukukausi] ([id], [lukukausi_fi], [lukukausi_sv], [lukukausi_en], [jarjestys_lukukausi], [source]) VALUES (1, 'Syys', 'Höst', 'Autumn', 1, 'manuaalinen')
 INSERT [dw].[d_lukukausi] ([id], [lukukausi_fi], [lukukausi_sv], [lukukausi_en], [jarjestys_lukukausi], [source]) VALUES (2, 'Kevät', 'Vår', 'Spring', 2, 'manuaalinen')
+
 SET IDENTITY_INSERT [dw].[d_lukukausi] OFF
-
-use antero
-
-end
+END
