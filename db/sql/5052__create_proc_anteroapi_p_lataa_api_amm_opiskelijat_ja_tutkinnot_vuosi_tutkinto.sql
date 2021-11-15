7626178a01afda7294a14842ed35238d951ebdd8
vavainio <valtteri.vainio@csc.fi>
2021-11-15 15:07:38 +0200

    Create proc AnteroAPI p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto

---
+++ db/sql/5052__create_proc_anteroapi_p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto.sql
@@ -0,0 +1,58 @@
+USE [AnteroAPI]
+GO
+
+DROP PROCEDURE IF EXISTS [dw].[p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dw].[p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto] AS
+
+BEGIN
+EXEC [dw].[p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto_esilataus]
+END
+
+IF(SELECT COUNT(*) FROM dw.api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto) > 0 AND (SELECT COUNT(DISTINCT tilastovuosi) FROM dw.api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto) >= 4
+
+BEGIN
+
+TRUNCATE TABLE [api].[amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto];
+
+INSERT INTO [api].[amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto](
+	defaultorder,
+	tilastovuosi,
+	tutkinto,
+	koulutuksen_jarjestaja,
+	uudet_opiskelijat_lkm,
+	opiskelijat_lkm,
+	tutkinnon_suorittaneet_lkm,
+	koodi_koulutuksen_jarjestaja,
+	koodi_tutkinto
+)
+
+SELECT
+	f.defaultorder,
+	f.tilastovuosi,
+	f.tutkinto,
+	f.koulutuksenJarjestaja,
+	f.uudetOpiskelijatLkm,
+	f.opiskelijatLkm,
+	f.tutkinnonSuorittaneetLkm,
+	f.koodiKoulutuksenJarjestaja,
+	f.koodiTutkinto
+FROM dw.api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto f
+
+END
+
+ELSE RAISERROR('Input table has 0 rows',11,1)
+
+GO
+
+USE [ANTERO]
+
+
