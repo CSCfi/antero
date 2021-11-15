23c347388f2b9fa21e71f44b9b820812bcf4d804
vavainio <valtteri.vainio@csc.fi>
2021-11-15 14:58:11 +0200

    Create proc AnteroAPI p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto_esilataus

---
+++ db/sql/5048__create_proc_anteroapi_p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto_esilataus.sql
@@ -0,0 +1,68 @@
+USE [AnteroAPI]
+GO
+
+DROP PROCEDURE IF EXISTS [dw].[p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto_esilataus]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dw].[p_lataa_api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto_esilataus] AS
+
+TRUNCATE TABLE [dw].[api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto];
+
+INSERT INTO [dw].[api_amm_opiskelijat_ja_tutkinnot_vuosi_tutkinto]
+
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY f.tilastovuosi
+							  ,d1.koulutusluokitus_fi
+							  ,d2.organisaatio_fi) as defaultorder
+	,f.tilastovuosi
+	,d1.koulutusluokitus_fi as tutkinto
+	,d2.organisaatio_fi as koulutuksenJarjestaja
+	,count(distinct case when f.uudet_opiskelijat = 1 then f.oppija_oid end) as uudetOpiskelijatLkm
+	,count(distinct case when f.opiskelijat = 1 then f.oppija_oid end) as opiskelijatLkm
+	,count(distinct case when f.tutkinnot = 1 then f.oppija_oid end) as tutkinnonSuorittaneetLkm 
+	,d2.organisaatio_koodi as koodiKoulutuksenJarjestaja
+	,d1.koulutusluokitus_koodi as koodiTutkinto
+FROM (
+	SELECT distinct
+		tilastovuosi
+		,d_koulutusluokitus_id
+		,d_organisaatioluokitus_jarj_id
+		,oppija_oid
+		,opiskelijat = 1
+		,uudet_opiskelijat = uusi
+		,tutkinnot = 0
+	FROM ANTERO.dw.f_koski_amm_opiskelijat
+
+	UNION ALL
+
+	SELECT distinct
+		tilastovuosi
+		,d_koulutusluokitus_id
+		,d_organisaatioluokitus_jarj_id
+		,oppija_oid
+		,opiskelijat = 0
+		,uudet_opiskelijat = 0
+		,tutkinnot = 1
+	FROM ANTERO.dw.f_koski_amm_tutkinnot ) f
+
+LEFT JOIN ANTERO.dw.d_koulutusluokitus d1 ON d1.id = f.d_koulutusluokitus_id
+LEFT JOIN ANTERO.dw.d_organisaatioluokitus d2 ON d2.id = f.d_organisaatioluokitus_jarj_id
+GROUP BY 
+f.tilastovuosi, 
+d1.koulutusluokitus_fi, 
+d2.organisaatio_fi,
+d2.organisaatio_koodi,
+d1.koulutusluokitus_koodi
+
+GO
+
+USE [ANTERO]
+
+
