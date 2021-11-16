e478b09970e7779b1bbe3bfcd925e4d6690bc1cf
vavainio <valtteri.vainio@csc.fi>
2021-11-16 13:33:49 +0200

    Create table AnteroAPI dw.api_ytl_arvosanat

---
+++ db/sql/5058__create_table_anteroapi_dw_api_ytl_arvosanat.sql
@@ -0,0 +1,40 @@
+USE [AnteroAPI]
+GO
+
+DROP TABLE IF EXISTS [dw].[api_ytl_arvosanat]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[api_ytl_arvosanat](
+	[defaultorder] [bigint] NOT NULL,
+	[opiskelija_koodi] [int] NOT NULL,
+	[tutkintokerta_koodi] [varchar](5) NOT NULL,
+	[tutkintokerta] [varchar](10) NULL,
+	[lukio_koodi] [varchar](5) NULL,
+	[lukio] [varchar](100) NULL,
+	[oppilaitostyyppi_koodi] [varchar](1) NULL,
+	[oppilaitostyyppi] [varchar](15) NULL,
+	[opetuskieli_koodi] [varchar](2) NULL,
+	[opetuskieli] [varchar](6) NULL,
+	[sukupuoli_koodi] [varchar](1) NULL,
+	[sukupuoli] [varchar](6) NULL,
+	[koe_koodi] [varchar](2) NULL,
+	[koe] [varchar](50) NULL,
+	[arvosana_koodi] [varchar](1) NULL,
+	[arvosana] [varchar](30) NULL,
+	[arvosanapisteet] [varchar](1) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[defaultorder] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+USE [ANTERO]
+
