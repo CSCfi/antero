077f3079d370cfd3b344287e25693a349185b92e
karppiesa <esa.karppinen@csc.fi>
2021-11-17 09:46:28 +0200

    e

---
+++ db/sql/5079__insert_sa_virta_otp_viisviis_2021.sql
@@ -0,0 +1,70 @@
+
+use antero
+
+insert into antero.sa.[sa_virta_otp_viisviis_2021]
+(
+[edellinenSyysolo]
+      ,[hetu]
+      ,[ika]
+      ,[kevat]
+      ,[loAloituspvm]
+      ,[olok]
+      ,[olos]
+      ,[ooAloituspvm]
+      ,[opSummaKunOtePankista]
+      ,[opiskelijaavain]
+      ,[opiskeluoikeusavain]
+      ,[opiskeluoikeusid]
+      ,[oppilaitos]
+      ,[oppilaitostunnus]
+      ,[pankkiKumuEnnen55]
+      ,[pankkiSaldo55]
+      ,[regDatum]
+      ,[sukupuoli]
+      ,[summa]
+      ,[suorittanut27]
+      ,[suorittanut55ilmanPankkia]
+      ,[suorittanut55pankinAvulla]
+      ,[syys]
+      ,[tkoodi]
+      ,[uusiOpisk]
+      ,[uusiOpiskKevat]
+      ,[uuttaPankkiin]
+      ,[vuosi]
+)
+
+select 
+[edellinenSyysolo]
+      ,[hetu]
+      ,[ika]
+      ,[kevat]
+      ,[loAloituspvm]
+      ,[olok]
+      ,[olos]
+      ,[ooAloituspvm]
+      ,[opSummaKunOtePankista]
+      ,[opiskelijaavain]
+      ,[opiskeluoikeusavain]
+      ,[opiskeluoikeusid]
+      ,[oppilaitos]
+      ,[oppilaitostunnus]
+      ,[pankkiKumuEnnen55]
+      ,[pankkiSaldo55]
+      ,[regDatum]
+      ,[sukupuoli]
+      ,[summa]
+      ,[suorittanut27]
+      ,[suorittanut55ilmanPankkia]
+      ,[suorittanut55pankinAvulla]
+      ,[syys]
+      ,[tkoodi]
+      ,[uusiOpisk]
+      ,[uusiOpiskKevat]
+      ,[uuttaPankkiin]
+      ,[vuosi]
+	  from antero.sa.sa_virta_otp_viisviis
+
+	  
+	  go
+	  use antero
+	  
