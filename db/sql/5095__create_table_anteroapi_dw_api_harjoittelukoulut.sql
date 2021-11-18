3adf3d4e445b0457401d1bcbac0006e949fa6025
vavainio <valtteri.vainio@csc.fi>
2021-11-18 15:11:43 +0200

    AnteroAPI Create tables and procedures for harjoittelukoulut

---
+++ db/sql/5095__create_table_anteroapi_dw_api_harjoittelukoulut.sql
@@ -0,0 +1,48 @@
+USE [AnteroAPI]
+GO
+
+DROP TABLE IF EXISTS [dw].[api_harjoittelukoulut]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[api_harjoittelukoulut](
+	[defaultorder] [bigint] NOT NULL,
+	[tilastovuosi] [int] NULL,
+	[yliopisto] [varchar](100) NULL,
+	[henkilostoryhma] [varchar](55) NULL,
+	[koulutusala] [varchar](55) NULL,
+	[ohjauksen_ala] [varchar](100) NULL,
+	[harjoittelukoulun_oppilaat_esiopetus] [int] NULL,
+	[harjoittelukoulun_oppilaat_perusopetuksen_vuodet_1_6] [int] NULL,
+	[harjoittelukoulun_oppilaat_perusopetuksen_vuodet_7_9] [int] NULL,
+	[harjoittelukoulun_oppilaat_lukio] [int] NULL,
+	[harjoittelukoulun_oppilaat_yhteensa] [int] NULL,
+	[harjoittelukoulun_opettajankoulutustehtavien_kokonaisviikkotunnit] [int] NULL,
+	[harjoittelukoulun_kokonaistuntimaara] [int] NULL,
+	[opintopisteet_lastentarha] [int] NULL,
+	[opintopisteet_luokanopettaja] [int] NULL,
+	[opintopisteet_aineenopettaja] [int] NULL,
+	[opintopisteet_erityisopettaja] [int] NULL,
+	[opintopisteet_opinto_ohjaaja] [int] NULL,
+	[opintopisteet_aikuiskoulutus] [int] NULL,
+	[opintopisteet_muut] [int] NULL,
+	[henkilotyovuosi] [decimal](10, 4) NULL,
+	[koodit_yliopisto] [varchar](5) NULL,
+	[koodit_koulutusala] [int] NULL,
+	[koodit_ohjauksen_ala] [int] NULL,
+	[koodit_henkilostoryhma] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[defaultorder] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+USE [ANTERO]
+
