a80fcb8058280c3fb1b51a36c1bef394aee545e0
villehoo <ville.hamalainen@csc.fi>
2021-11-26 10:08:49 +0200

    fix anteroAPI toimipisteet tables and procedures

--- db/sql/5120__create_proc_anteroapi_p_lataa_api_toimipisteet_esilataus.sql
+++ db/sql/5120__create_proc_anteroapi_p_lataa_api_toimipisteet_esilataus.sql
@@ -18,100 +18,69 @@ TRUNCATE TABLE [dw].[api_toimipisteet]
 
 INSERT INTO [dw].[api_toimipisteet]
 
-
-
---amk
 SELECT
-1000000000+ ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id) AS defaultorder
-,Tilastovuosi = f.vuosi
+1000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC, d2.id ASC , d3.id ASC , d4.id ASC , d5.id ASC, d6.id ASC ) AS defaultorder
+,[Tilastovuosi] = f.vuosi
+,[Yliopisto] = d1.yo_nimi_fi
+,[Ammattikorkeakoulu] = NULL
+,[Toimipiste] = d2.selite_fi
+,[Toimipaikka] = d3.selite_fi
 
---,Ammattikorkeakoulu = d7.amk_nimi_fi
-,Yliopisto = d1.yo_nimi_fi
-,Ammattikorkeakoulu = NULL
-,Toimipiste = NULL
-,Toimipaikka = NULL
+,[Toiminta: tutkintoon johtava koulutus] = d4.kytkin_fi
+,[Toiminta: tutkimustoiminta] = d5.kytkin_fi
+,[Toiminta: muu] = d6.kytkin_fi
+,[Toiminta: TKI] = NULL
+,[Toiminta: palvelutoiminta] = NULL
+,[Toiminta: nuorten koulutus] = NULL
+,[Toiminta: aikuiskoulutus] = NULL
+,[Toiminta: ylempään amk-tutkintoon johtava koulutus] = NULL
 
---mittarit
-,harjoittelukoulujen_tilat
-,muut_vuokratilat
-,yliopistokiinteistoyhtion_toimitilat_aalto_yliopistokiinteistot_oy
-,yliopistokiinteistoyhtion_toimitilat_helsingin_yliopistokiinteistot_oy
-,yliopistokiinteistoyhtion_toimitilat_suomen_yliopistokiinteistot_oy
-,toimipaikan_henkilokunnan_henkilotyovuodet = NULL
-,tutkimusaseman_henkilokunnan_maksimimäärä = NULL
-,tutkimusaseman_henkilokunnan_minimäärä = NULL
-,tutkimusaseman_yopymisvuorokaudet = NULL
+,[toimipaikan_henkilokunnan_henkilotyovuodet]
+,[tutkimusaseman_henkilokunnan_maksimimäärä]
+,[tutkimusaseman_henkilokunnan_minimäärä]
+,[tutkimusaseman_yopymisvuorokaudet]
 
---koodit
 ,[Koodit Yliopisto] = d1.yo_tunnus
 ,[Koodit Ammattikorkeakoulu] = NULL
 
-from [antero].[dw].[f_yo_tilat] f
-JOIN antero.dw.d_yo d1 on d1.id=f.d_yliopisto_id
+FROM [ANTERO].[dw].[f_yo_toimipisteet] f
+LEFT JOIN antero.dw.d_yo d1 ON d1.id=f.d_yliopisto_id
+LEFT JOIN antero.dw.d_yo_toimipiste d2 ON d2.id=f.d_toimipiste_id and d2.vuosi=f.vuosi
+LEFT JOIN antero.dw.d_yo_toimipisteen_toimipaikka d3 ON d3.id=f.d_toimipaikka_id
+LEFT JOIN antero.dw.d_kytkin d4 ON d4.id=d2.d_toiminta_tutkintoon_johtava_koulutus_id
+LEFT JOIN antero.dw.d_kytkin d5 ON d5.id=d2.d_toiminta_tutkimustoiminta_id
+LEFT JOIN antero.dw.d_kytkin d6 ON d6.id=d2.d_toiminta_muu_id
 
 UNION ALL
 
 SELECT
-2000000000+ ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id, d3.id) AS defaultorder
-,Tilastovuosi = f.vuosi
---,Ammattikorkeakoulu = d7.amk_nimi_fi
-,Yliopisto = NULL
-,Ammattikorkeakoulu = d1.amk_nimi_fi
+ 2000000000+ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id ASC, d3.id ASC) AS defaultorder
+,[Tilastovuosi] = f.vuosi
+,[Yliopisto] = NULL
+,[Ammattikorkeakoulu] = d1.amk_nimi_fi
+,[Toimipiste] = d3.toimipisteen_nimi
+,[Toimipaikka] = d3.toimipaikan_nimi
 
-,Toimipiste = d3.toimipisteen_nimi
-,Toimipaikka = d3.toimipaikan_nimi
+,[Toiminta: tutkintoon johtava koulutus] = NULL
+,[Toiminta: tutkimustoiminta] = NULL
+,[Toiminta: muu] = NULL
+,[Toiminta: TKI] = d3.tki_toiminta
+,[Toiminta: palvelutoiminta] = d3.palvelutoiminta
+,[Toiminta: nuorten koulutus] = d3.nuorten_koulutus
+,[Toiminta: aikuiskoulutus] = d3.aikuiskoulutus
+,[Toiminta: ylempään amk-tutkintoon johtava koulutus] = d3.ylempaan_amk_tutkintoon_johtava_koulutus
 
---mittarit
-,harjoittelukoulujen_tilat = NULL
-,muut_vuokratilat = NULL
-,yliopistokiinteistoyhtion_toimitilat_aalto_yliopistokiinteistot_oy = NULL
-,yliopistokiinteistoyhtion_toimitilat_helsingin_yliopistokiinteistot_oy = NULL
-,yliopistokiinteistoyhtion_toimitilat_suomen_yliopistokiinteistot_oy = NULL
-,toimipaikan_henkilokunnan_henkilotyovuodet
-,tutkimusaseman_henkilokunnan_maksimimäärä = NULL
-,tutkimusaseman_henkilokunnan_minimäärä = NULL
-,tutkimusaseman_yopymisvuorokaudet = NULL
+,[toimipaikan_henkilokunnan_henkilotyovuodet]
+,NULL
+,NULL
+,NULL
 
-
---koodit
 ,[Koodit Yliopisto] = NULL
 ,[Koodit Ammattikorkeakoulu] = d1.amk_tunnus
 
-FROM [antero].[dw].[f_amk_toimipisteet] f
-JOIN antero.dw.d_amk d1 on d1.id=f.d_amk_id
---JOIN antero.dw.d_amk_toimipiste d2 on d2.id=f.d_toimipiste_id
-JOIN antero.dw.d_amk_toimipisteen_toimipaikka d3 on d3.id=f.d_toimipisteen_toimipaikka_id
---JOIN antero.dw.d_amk_t
-
-UNION ALL
-
-SELECT
-3000000000+ ROW_NUMBER() OVER(ORDER BY f.id ASC, d1.id, d3.id) AS defaultorder
-,Tilastovuosi = f.vuosi
-,Yliopisto = d1.yo_nimi_fi
-,Ammattikorkeakoulu = NULL
-
-,Toimipiste = d2.selite_fi
-,Toimipaikka = d3.selite_fi
-
---mittarit
-,harjoittelukoulujen_tilat = NULL
-,muut_vuokratilat = NULL
-,yliopistokiinteistoyhtion_toimitilat_aalto_yliopistokiinteistot_oy = NULL
-,yliopistokiinteistoyhtion_toimitilat_helsingin_yliopistokiinteistot_oy = NULL
-,yliopistokiinteistoyhtion_toimitilat_suomen_yliopistokiinteistot_oy = NULL
-,toimipaikan_henkilokunnan_henkilotyovuodet
-,tutkimusaseman_henkilokunnan_maksimimäärä
-,tutkimusaseman_henkilokunnan_minimäärä
-,tutkimusaseman_yopymisvuorokaudet
-
---koodit
-,[Koodit Yliopisto] = d1.yo_tunnus
-,[Koodit Ammattikorkeakoulu] = NULL
-
-FROM [antero].[dw].[f_yo_toimipisteet] f
-JOIN antero.dw.d_yo d1 on d1.id=f.d_yliopisto_id
-JOIN antero.dw.d_yo_toimipiste d2 on d2.id=f.d_toimipiste_id and d2.vuosi = f.vuosi
-JOIN antero.dw.d_yo_toimipisteen_toimipaikka d3 on d3.id=f.d_toimipaikka_id
+FROM [ANTERO].[dw].[f_amk_toimipisteet] f
+LEFT JOIN antero.dw.d_amk d1 ON d1.id=f.d_amk_id
+--LEFT JOIN antero.dw.d_amk_toimipisteen_toimipaikka d2 ON d2.id=f.d_toimipiste_id
+LEFT JOIN antero.dw.d_amk_toimipisteen_toimipaikka d3 ON d3.id=f.d_toimipisteen_toimipaikka_id
 
 GO
