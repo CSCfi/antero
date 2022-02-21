0b3137844500c3671018be713fd2037fa822217d
juhasyrj <juha.syrjala@csc.fi>
2022-02-21 15:52:10 +0200

    add alter drop createables before altering views

---
+++ db/sql/5904__alter_tables_koski_esi_ja_perus.sql
@@ -0,0 +1,8 @@
+USE [ANTERO]
+GO
+
+ALTER TABLE dw.f_koski_esi_ja_perus_oppilaat_ja_paattaneet_kuukausittain ADD kuutioon_1_0 int null
+ALTER TABLE dw.f_koski_esi_ja_perus_oppilaat_ja_paattaneet_lukuvuosittain ADD kuutioon_1_0 int null
+
+
+
