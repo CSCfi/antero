ef72f304e832fe0a5bddeed60df08e79dff0a33f
juhasyrj <juha.syrjala@csc.fi>
2022-02-22 19:24:54 +0200

    try splitting begin..end

--- db/sql/5921__alter_table_d_organisaatioluokitus.sql
+++ db/sql/5921__alter_table_d_organisaatioluokitus.sql
@@ -18,9 +18,25 @@ begin
 	alter table [dw].[d_organisaatioluokitus]
 	add [koulutuksen_jarjestajan_yritysmuoto_sv] nvarchar(100) null,
 		[koulutuksen_jarjestajan_yritysmuoto_en] nvarchar(100) null
+		
+end
+;
+
+
+if exists (
+
+	SELECT top 1 * 
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE table_schema = 'dw'
+	AND table_name = 'd_organisaatioluokitus'
+	AND column_name in ('koulutuksen_jarjestajan_yritysmuoto_sv', 'koulutuksen_jarjestajan_yritysmuoto_en')
+
+)
+
+begin
 
 	update [dw].[d_organisaatioluokitus]
 	set [koulutuksen_jarjestajan_yritysmuoto_sv] = 'Information saknas',
 		[koulutuksen_jarjestajan_yritysmuoto_en] = 'Missing data'
-
+		
 end
