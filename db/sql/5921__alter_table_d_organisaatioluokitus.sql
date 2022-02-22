cb164ad1ef1873378ac6a68aeccf68f3ce28983d
juhasyrj <juha.syrjala@csc.fi>
2022-02-22 19:19:11 +0200

    CSCVIPUNEN-2390: add yritysmuoto translation cols into dim & proc

---
+++ db/sql/5921__alter_table_d_organisaatioluokitus.sql
@@ -0,0 +1,26 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+use antero 
+go
+
+
+if not exists (
+
+	SELECT top 1 * 
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE table_schema = 'dw'
+	AND table_name = 'd_organisaatioluokitus'
+	AND column_name in ('koulutuksen_jarjestajan_yritysmuoto_sv', 'koulutuksen_jarjestajan_yritysmuoto_en')
+
+)
+
+begin
+
+	alter table [dw].[d_organisaatioluokitus]
+	add [koulutuksen_jarjestajan_yritysmuoto_sv] nvarchar(100) null,
+		[koulutuksen_jarjestajan_yritysmuoto_en] nvarchar(100) null
+
+	update [dw].[d_organisaatioluokitus]
+	set [koulutuksen_jarjestajan_yritysmuoto_sv] = 'Information saknas',
+		[koulutuksen_jarjestajan_yritysmuoto_en] = 'Missing data'
+
+end
