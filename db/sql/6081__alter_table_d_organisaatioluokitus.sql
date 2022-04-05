a58e2c91c1fffb328d108c807f237e01b11e9591
villehoo <ville.hamalainen@csc.fi>
2022-04-05 11:25:46 +0300

    fix organisaatioluokitus-etl

--- db/sql/6081__alter_table_d_organisaatioluokitus.sql
+++ db/sql/6081__alter_table_d_organisaatioluokitus.sql
@@ -8,6 +8,6 @@ SET QUOTED_IDENTIFIER ON
 GO
 USE [ANTERO]
 GO
-ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organidwatio] [nvarchar](500) NULL
-ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organidwatio_sv] [nvarchar](500) NULL
-ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organidwatio_en] [nvarchar](500) NULL
+ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organisaatio_fi] [nvarchar](500) NULL
+ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organisaatio_sv] [nvarchar](500) NULL
+ALTER TABLE [dw].[d_organisaatioluokitus] ALTER COLUMN [organisatio_en] [nvarchar](500) NULL
