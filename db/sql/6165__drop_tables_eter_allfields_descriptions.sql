f402740d543b46c1150d3bdb13a5bf7f2c7e35bc
vavainio <valtteri.vainio@csc.fi>
2022-04-27 11:49:44 +0300

    CSCVIPUNEN-2890

--- db/sql/6165__drop_tables_eter_allfields_descriptions.sql
+++ db/sql/6165__drop_tables_eter_allfields_descriptions.sql
@@ -1,6 +1,6 @@
 USE [ANTERO]
 GO
-DROP TABLE [ANTERO].[sa].[sa_eter_allfields_descriptions]
+DROP TABLE IF EXISTS [ANTERO].[sa].[sa_eter_allfields_descriptions]
 GO
-DROP TABLE [ANTERO].[dw].[f_eter_allfields_descriptions]
+DROP TABLE IF EXISTS [ANTERO].[dw].[f_eter_allfields_descriptions]
 USE [ANTERO]
