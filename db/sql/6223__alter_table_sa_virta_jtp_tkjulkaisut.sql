501a59cd3de7512be58c7a143b2986d408941d56
villehoo <ville.hamalainen@csc.fi>
2022-05-19 09:01:03 +0300

    Alter sa_virta_jtp_tkjulkaisut

---
+++ db/sql/6223__alter_table_sa_virta_jtp_tkjulkaisut.sql
@@ -0,0 +1,69 @@
+USE ANTERO
+GO
+
+
+
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='SivunumeroTeksti' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN SivunumeroTeksti NVARCHAR(MAX) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='JufoTunnus' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN JufoTunnus NVARCHAR(20) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='JulkaisumaaKoodi' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN JulkaisumaaKoodi NVARCHAR(20) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='VolyymiTeksti' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN VolyymiTeksti NVARCHAR(MAX) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='LehdenNumeroTeksti' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN LehdenNumeroTeksti NVARCHAR(MAX) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='JulkaisunKieliKoodi' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN JulkaisunKieliKoodi NVARCHAR(20) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='Lataus_ID' and DATA_TYPE='varchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN Lataus_ID NVARCHAR(255) NULL
+END
+
+IF EXISTS (
+  select * from INFORMATION_SCHEMA.COLUMNS
+  where TABLE_SCHEMA='sa' and TABLE_NAME='sa_virta_jtp_tkjulkaisut'
+  and COLUMN_NAME='YhteisjulkaisunTunnus' and DATA_TYPE='nvarchar'
+)  BEGIN
+  ALTER TABLE sa.sa_virta_jtp_tkjulkaisut ALTER COLUMN YhteisjulkaisunTunnus NVARCHAR(50) NULL
+END
