6c4ca9932d6dbe442bf365162c984aab93d37e3a
juhasyrj <juha.syrjala@csc.fi>
2022-06-24 12:59:45 +0300

    CSCVIPUNEN-3011: migrate 4.8 etl changes until & incl. dw targets

---
+++ db/sql/6270__drop_create_table_vipunentk_dw_f_4_8_Tutkinnon_suorittaneiden_myohempi_koulutus.sql
@@ -0,0 +1,9 @@
+use [VipunenTK_DW]
+go
+
+Drop table VipunenTK_DW.dbo.f_4_8_Tutkinnon_suorittaneiden_myohempi_koulutus
+go
+
+Select top 0 * into VipunenTK_DW.dbo.f_4_8_Tutkinnon_suorittaneiden_myohempi_koulutus 
+from VipunenTK_DW.dbo.v_f_4_8_Tutkinnon_suorittaneiden_myohempi_koulutus
+go
---
+++ db/sql/6272__drop_create_table_vipunentk_dw_f_tutkinnon_suorittaneiden_myohempi_koulutus_4_8.sql
@@ -0,0 +1,9 @@
+use [VipunenTK_DW]
+go
+
+Drop table VipunenTK_DW.dbo.f_tutkinnon_suorittaneiden_myohempi_koulutus_4_8
+go
+
+Select top 0 * into VipunenTK_DW.dbo.f_tutkinnon_suorittaneiden_myohempi_koulutus_4_8 
+from VipunenTK_DW.dbo.v_f_tutkinnon_suorittaneiden_myohempi_koulutus_4_8
+go
