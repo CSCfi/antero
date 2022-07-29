19534e24c7b4f407c3835881ffa43081809e9bbf
vavainio <valtteri.vainio@csc.fi>
2022-07-29 13:00:07 +0300

    CSCVIPUNEN-2926

---
+++ db/sql/6327__alter_proc_p_lataa_data_model.sql
@@ -0,0 +1,132 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_data_model]    Script Date: 29.7.2022 7:58:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE  [dw].[p_lataa_data_model] AS
+
+SELECT DISTINCT
+	   v.[database]
+      ,v.[schema]
+      ,v.[view]
+	  ,v.referenced_database_name as [view_table_database]
+	  ,v.referenced_schema_name as [view_table_schema]
+	  ,v.referenced_entity_name as [view_table]
+INTO #tempView
+FROM [ANTERO].[dbo].[views] v
+
+SELECT DISTINCT
+	   p.[database] as database_proc
+	  ,p.[schema] as schema_proc
+	  ,p.[procedure]
+	  ,p.[referenced_database_name]
+	  ,p.[referenced_schema_name]
+	  ,p.[referenced_entity_name]
+	  ,p.manual_load
+INTO #tempProc
+FROM [ANTERO].[dbo].[procedures] p
+
+TRUNCATE TABLE ANTERO.dbo.data_model
+
+INSERT INTO ANTERO.dbo.data_model
+
+SELECT DISTINCT
+	   v.[database]
+      ,v.[schema]
+      ,v.[view]
+	  ,v.[view_table_database]
+	  ,v.[view_table_schema]
+	  ,v.[view_table]
+	  ,p.database_proc
+	  ,p.schema_proc
+	  ,p.[procedure]
+	  ,case when p2.manual_load = 1 then null else p2.referenced_database_name end as [procedure_table_database]
+	  ,case when p2.manual_load = 1 then null else p2.referenced_schema_name end as [procedure_table_schema]
+	  ,case when p2.manual_load = 1 then null else p2.referenced_entity_name end as [procedure_table]
+	  ,loadtime = getdate()
+	  ,[source] = 'ETL: p_lataa_data_model'
+	  ,username = USER_NAME()
+FROM #tempView v
+INNER JOIN #tempProc p on v.view_table_database = p.database_proc and v.view_table_schema = p.schema_proc and concat('p_lataa_', v.view_table) = p.[procedure]
+INNER JOIN #tempProc p2 on p2.[procedure] = p.[procedure] and p2.database_proc = p.database_proc and p2.schema_proc = p.schema_proc and (p2.[referenced_entity_name] <> v.view_table or p2.manual_load = 1)
+
+UNION ALL
+
+SELECT DISTINCT
+	   v.[database]
+      ,v.[schema]
+      ,v.[view]
+	  ,v.[view_table_database]
+	  ,v.[view_table_schema]
+	  ,v.[view_table]
+	  ,NULL
+	  ,NULL
+	  ,NULL
+	  ,NULL
+	  ,NULL
+	  ,NULL
+	  ,loadtime = getdate()
+	  ,[source] = 'ETL: p_lataa_data_model'
+	  ,username = USER_NAME()
+FROM #tempView v
+WHERE v.[database] = 'Bibliometriikka' or (v.[view] = 'v_ytl_arvosanat' and v.[schema] = 'dw')
+
+SELECT * INTO #tempTable
+FROM (
+	SELECT 
+	   v.[database]
+      ,v.[schema]
+      ,v.[view]
+	  ,v.[view_table_database]
+	  ,v.[view_table_schema]
+	  ,v.[view_table]
+	FROM #tempView v
+
+	EXCEPT
+
+	SELECT 
+	   dm.[database]
+      ,dm.[schema]
+      ,dm.[view]
+	  ,dm.[view_table_database]
+	  ,dm.[view_table_schema]
+	  ,dm.[view_table]
+	FROM ANTERO.dbo.data_model dm
+) t
+
+INSERT INTO ANTERO.dbo.data_model
+SELECT DISTINCT
+	t.[database],
+	t.[schema],
+	t.[view],
+	t.view_table_database,
+	t.view_table_schema,
+	t.view_table,
+	CASE WHEN UPPER(t.view_table_database) = 'VIPUNENTK' and upper(left(t.view_table,1)) = 'D' THEN 'VipunenTK' WHEN  UPPER(t.view_table_database) = 'VIPUNENTK_DW' and upper(left(t.view_table,1)) = 'D' THEN 'VipunenTK_DW' ELSE p.database_proc END as database_proc,
+	CASE WHEN UPPER(t.view_table_database) in('VIPUNENTK','VIPUNENTK_DW') and upper(left(t.view_table,1)) = 'D' THEN 'dbo' ELSE p.schema_proc END as schema_proc,
+	CASE WHEN UPPER(t.view_table_database) = 'VIPUNENTK' and upper(left(t.view_table,1)) = 'D' THEN 'ETL_tyhjenna_ja_lataa_VipunenTK_dimensiot' WHEN  UPPER(t.view_table_database) = 'VIPUNENTK_DW' and upper(left(t.view_table,1)) = 'D' THEN 'ETL_tyhjenna_ja_lataa_VipunenTK_DW_dimensiot' ELSE p.[procedure] END as [procedure],
+	CASE WHEN UPPER(t.view_table_database) in('VIPUNENTK','VIPUNENTK_DW') and (upper(left(t.view_table,1)) = 'D' or p.[procedure] in ('p_lataa_OPE','ETL_tyhjenna_VipunenTK_faktat'))  THEN NULL ELSE p2.referenced_database_name END as procedure_table_database,
+	CASE WHEN UPPER(t.view_table_database) in('VIPUNENTK','VIPUNENTK_DW') and (upper(left(t.view_table,1)) = 'D' or p.[procedure] in ('p_lataa_OPE','ETL_tyhjenna_VipunenTK_faktat')) THEN NULL ELSE p2.referenced_schema_name END as procedure_table_schema,
+	CASE WHEN UPPER(t.view_table_database) in('VIPUNENTK','VIPUNENTK_DW') and (upper(left(t.view_table,1)) = 'D' or p.[procedure] in ('p_lataa_OPE','ETL_tyhjenna_VipunenTK_faktat')) THEN NULL ELSE p2.referenced_entity_name END as procedure_table,
+	loadtime = getdate(),
+	[source] = 'ETL: p_lataa_data_model',
+	username = USER_NAME()
+FROM #tempTable t
+INNER JOIN #tempProc p on p.referenced_database_name = t.view_table_database and p.schema_proc = t.view_table_schema and p.referenced_entity_name = t.view_table and (upper(left(t.view_table,1)) = 'F' or (upper(left(t.view_table,1)) = 'D' and UPPER(t.view_table_database) in ('VIPUNENTK', 'VIPUNENTK_DW')) )
+INNER JOIN #tempProc p2 on p2.[procedure] = p.[procedure] and p2.database_proc = p.database_proc and p2.schema_proc = p.schema_proc and p2.[referenced_entity_name] <> p.[referenced_entity_name] and p2.[referenced_entity_name] <> t.view_table
+WHERE (p.[procedure] not like 'ETL%' or p.[procedure] like '%dimensiot' or p.[procedure] = 'ETL_tyhjenna_VipunenTK_faktat') and (t.view_table <> 'f_haku_ja_valinta_yoarvosanat' or p.[procedure] = 'p_lataa_f_haku_ja_valinta_yo_arvosanat') and t.view_table <> 'f_koski_esi_ja_perus_oppilaat_ja_paattaneet_lukuvuosittain'
+
+DROP TABLE #tempView
+DROP TABLE #tempProc
+DROP TABLE #tempTable
+
+
+GO
+
+USE [ANTERO]
