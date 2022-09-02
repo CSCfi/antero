4f90d39dbffd32cb95d76d42f129c221c215826d
juhasyrj <juha.syrjala@csc.fi>
2022-09-02 23:44:08 +0300

    CSCVIPUNEN-3019: tuva etl changes - amm

---
+++ db/sql/6374__drop_create_table_f_koski_amm_tutkinnot.sql
@@ -0,0 +1,51 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[f_koski_amm_tutkinnot]    Script Date: 2.9.2022 10:12:42 ******/
+DROP TABLE [dw].[f_koski_amm_tutkinnot]
+GO
+
+/****** Object:  Table [dw].[f_koski_amm_tutkinnot]    Script Date: 2.9.2022 10:12:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[f_koski_amm_tutkinnot](
+	[tilastovuosi] [int] NULL,
+	[d_kalenteri_id] [bigint] NULL,
+	[d_sukupuoli_id] [int] NULL,
+	[d_kieli_aidinkieli_id] [bigint] NULL,
+	[d_ika_id] [bigint] NULL,
+	[d_maatjavaltiot2_kansalaisuus_id] [int] NULL,
+	[d_osa_aikaisuus_id] [int] NULL,
+	[d_erityisopetus_id] [int] NULL,
+	[d_majoitus_id] [int] NULL,
+	[d_kytkin_oppisopimuskoulutus_id] [int] NULL,
+	[d_kytkin_koulutussopimus_id] [int] NULL,
+	[d_amm_sopimusjaksot_id] [int] NULL,
+	[d_kytkin_vankilaopetus_id] [int] NULL,
+	[d_kytkin_henkilostokoulutus_id] [int] NULL,
+	[d_kytkin_tyovoimakoulutus_id] [int] NULL,
+	[d_koulutusluokitus_id] [int] NULL,
+	[d_organisaatioluokitus_oppilaitos_id] [bigint] NULL,
+	[d_organisaatioluokitus_jarj_id] [bigint] NULL,
+	[d_organisaatioluokitus_toimipiste_id] [bigint] NULL,
+	[d_kustannusryhma_id] [int] NULL,
+	[d_suorituksen_tyyppi_id] [int] NULL,
+	[d_opintojenrahoitus_id] [int] NULL,
+	[d_osaamisala_id] [int] NULL,
+	[d_kieli_suorituskieli_id] [bigint] NULL,
+	[d_kytkin_muu_ammatillinen_id] [int] NULL,
+	[valmentava_koodi] [int] NULL,
+	[oppisopimus_osuus] [varchar](13) NOT NULL,
+	[koulutussopimus_osuus] [varchar](13) NOT NULL,
+	[oppija_oid] [varchar](100) NULL,
+	[opiskeluoikeus_oid] [varchar](100) NULL,
+	[loadtime] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+
