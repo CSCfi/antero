1358abee4c43ba3844e20d4f3320795be927c22e
vavainio <valtteri.vainio@csc.fi>
2022-09-23 13:51:06 +0300

    CSCVIPUNEN-3096 - Horizon Europe

--- pdi_integrations/horizon/R/Horizon.R
+++ pdi_integrations/horizon/R/Horizon.R
@@ -1,5 +1,5 @@
 
-Alku <- Sys.time() #Tallennetaan aloitusaika
+Alku <- Sys.time() # Start time
 
 #---------------------------#
 
@@ -27,6 +27,10 @@ if(require("tidyr") == FALSE){
   install.packages("tidyr")
   library("tidyr")
 }
+if(require("dplyr") == FALSE){
+  install.packages("dplyr")
+  library("dplyr")
+}
 
 
 #---------------------------#
@@ -35,23 +39,25 @@ if(require("tidyr") == FALSE){
 
 #---------------------------#
 
-# projects 
+## Horizon 2020
+
+# Projects 
 tf3 <- tempfile()
 td3 <- tempdir()
 download.file("https://cordis.europa.eu/data/cordis-h2020projects-csv.zip",tf3, mode = "wb")
 file.names <- unzip(tf3, exdir = td3)
-data = read.csv(file.names[4], header = TRUE, sep = ";", encoding="UTF-8")
-names(data)[19] <- "projectRcn"
+Projects = read.csv(file.names[4], header = TRUE, sep = ";", encoding="UTF-8")
+names(Projects)[19] <- "projectRcn"
 
-# organisations + EC contribution per project
+# Organisations and EC contribution
 
-data2 = read.csv(file.names[3], header = TRUE, sep = ";", encoding="UTF-8")
+Orgs = read.csv(file.names[3], header = TRUE, sep = ";", encoding="UTF-8")
 
 # country codes
-data3 = read.csv("http://cordis.europa.eu/data/reference/cordisref-countries.csv", header = TRUE, sep = ";", encoding="UTF-8")
+Country = read.csv("http://cordis.europa.eu/data/reference/cordisref-countries.csv", header = TRUE, sep = ";", encoding="UTF-8")
 
 # organisation type codes
-data4 = read.csv("http://cordis.europa.eu/data/reference/cordisref-organizationActivityType.csv", header = TRUE, sep = ";", encoding="UTF-8")
+oTypes = read.csv("http://cordis.europa.eu/data/reference/cordisref-organizationActivityType.csv", header = TRUE, sep = ";", encoding="UTF-8")
 
 
 # programmes
@@ -59,18 +65,39 @@ tf <- tempfile()
 td <- tempdir()
 download.file("https://cordis.europa.eu/data/reference/cordisref-H2020programmes-csv.zip",tf, mode = "wb")
 file.names <- unzip(tf, exdir = td)
-data5 = read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
+Progs <- read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
 
 # topics
 tf2 <- tempfile()
 td2 <- tempdir()
 download.file("https://cordis.europa.eu/data/reference/cordisref-H2020topics-csv.zip",tf2, mode = "wb")
 file.names <- unzip(tf2, exdir = td2)
-data6 = read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
+Topics = read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
 
 #funding schemes
-data7 <- read.csv("https://cordis.europa.eu/data/reference/cordisref-projectFundingSchemeCategory.csv", header = TRUE, sep = ";", encoding="UTF-8")
+fSchemes <- read.csv("https://cordis.europa.eu/data/reference/cordisref-projectFundingSchemeCategory.csv", header = TRUE, sep = ";", encoding="UTF-8")
 
+## Horizon europe
+tfHE <- tempfile()
+tdHE <- tempdir()
+download.file("https://cordis.europa.eu/data/cordis-HORIZONprojects-csv.zip",tfHE, mode = "wb")
+file.names <- unzip(tfHE, exdir = tdHE)
+
+ProjectsHE <- read.csv(file.names[1], header = TRUE, sep = ";", encoding="UTF-8")
+OrgsHE <- read.csv(file.names[2], header = TRUE, sep = ";", encoding="UTF-8")
+LegalBasisHE <- read.csv(file.names[3], header = TRUE, sep = ";", encoding="UTF-8")
+
+tfHE2 <- tempfile()
+tdHE2 <- tempdir()
+download.file("https://cordis.europa.eu/data/reference/cordisref-HORIZONprogrammes-csv.zip",tfHE2, mode = "wb")
+file.names <- unzip(tfHE2, exdir = tdHE2)
+ProgsHE <- read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
+
+tfHE3 <- tempfile()
+tdHE3 <- tempdir()
+download.file("https://cordis.europa.eu/data/reference/cordisref-HORIZONtopics-csv.zip",tfHE3, mode = "wb")
+file.names <- unzip(tfHE3, exdir = tdHE3)
+TopicsHE <- read.csv(file.names, header = TRUE, sep = ";", encoding="UTF-8")
 
 #---------------------------#
 
@@ -78,34 +105,47 @@ data7 <- read.csv("https://cordis.europa.eu/data/reference/cordisref-projectFund
 
 #---------------------------#
 
-data3 <- subset(data3, language == 'en', select=names(data3))
-data4 <- subset(data4, language == 'en', select=names(data4))
-data5 <- subset(data5, language == 'en', select=names(data5))
-data6 <- subset(data6, language == 'en', select=names(data6))
-data7 <- subset(data7, language == 'en', select=names(data7))
+Country <- subset(Country, language == 'en', select=names(Country))
+oTypes <- subset(oTypes, language == 'en', select=names(oTypes))
+Progs <- subset(Progs, language == 'en', select=names(Progs))
+Topics <- subset(Topics, language == 'en', select=names(Topics))
+fSchemes <- subset(fSchemes, language == 'en', select=names(fSchemes))
 
-##### Kirjoitetaan csv-tiedostot #####
+ProjectsHE <- merge(ProjectsHE, LegalBasisHE[,1:2], by.x = "id", by.y = "projectID")
+ProjectsHE$legalBasis <-  as.character(ProjectsHE$legalBasis.x)
+ProjectsHE$legalBasis[ProjectsHE$legalBasis == ""] <-  as.character(ProjectsHE$legalBasis.y[ProjectsHE$legalBasis == ""])
+ProjectsHE = ProjectsHE[,!(names(ProjectsHE) %in% c("legalBasis.y", "legalBasis.x"))]
+
+
+# Horizon 2020 and Horizon Europe data is combined 
+
+Projects <- union_all(Projects, ProjectsHE)
+Orgs <- union_all(Orgs, OrgsHE)
+Progs <- union_all(Progs, ProgsHE)
+Topics <- union_all(Topics, TopicsHE)
+ 
+##### Write csv files #####
 
 options(scipen = 999)
 
-write.csv2(data,"D:/pdi_integrations/data/horizon/horizon_projects.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data2,"D:/pdi_integrations/data/horizon/horizon_organisations.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data3,"D:/pdi_integrations/data/horizon/horizon_country_codes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data4,"D:/pdi_integrations/data/horizon/horizon_org_type_codes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data5,"D:/pdi_integrations/data/horizon/horizon_programmes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data6,"D:/pdi_integrations/data/horizon/horizon_topics.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
-write.csv2(data7,"D:/pdi_integrations/data/horizon/horizon_funding_schemes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Projects,"D:/pdi_integrations/data/horizon/horizon_projects.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Orgs,"D:/pdi_integrations/data/horizon/horizon_organisations.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Country,"D:/pdi_integrations/data/horizon/horizon_country_codes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(oTypes,"D:/pdi_integrations/data/horizon/horizon_org_type_codes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Progs,"D:/pdi_integrations/data/horizon/horizon_programmes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Topics,"D:/pdi_integrations/data/horizon/horizon_topics.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(fSchemes,"D:/pdi_integrations/data/horizon/horizon_funding_schemes.csv",row.names = FALSE, na="", fileEncoding = "UTF-8")
 
 
-##### Varmuuskopioidaan datat #####
+##### Data is saved to other folder #####
 
-write.csv2(data,paste("D:/pdi_integrations/data/horizon/Varmuuskopiot/horizon_projects_", Sys.Date(),".csv", sep=""),row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Projects,paste("D:/pdi_integrations/data/horizon/Varmuuskopiot/horizon_projects_", Sys.Date(),".csv", sep=""),row.names = FALSE, na="", fileEncoding = "UTF-8")
 
-write.csv2(data2,paste("D:/pdi_integrations/data/horizon/Varmuuskopiot/horizon_organisations_", Sys.Date(),".csv", sep=""),row.names = FALSE, na="", fileEncoding = "UTF-8")
+write.csv2(Orgs,paste("D:/pdi_integrations/data/horizon/Varmuuskopiot/horizon_organisations_", Sys.Date(),".csv", sep=""),row.names = FALSE, na="", fileEncoding = "UTF-8")
 
 
-##### Kesto #####
+##### Duration #####
 
-Loppu <- Sys.time() #Tallennetaan aikaa varten lopetusaika
+Loppu <- Sys.time() # End time 
 Kesto <- Loppu-Alku
 print(paste("Ajaminen kesti ",Kesto))
