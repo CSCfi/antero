6f6ba86f2f0a95aa5b470720528afb05006f1b36
juhasyrj <juha.syrjala@csc.fi>
2022-10-28 23:51:06 +0300

    CSCVIPUNEN-2948: migrate etl (ssis-related elsewhere)

---
+++ db/sql/6587__create_table_vipunentk_f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan.sql
@@ -0,0 +1,46 @@
+USE [VipunenTK]
+GO
+
+/****** Object:  Table [dbo].[f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan]    Script Date: 28.10.2022 14:48:06 ******/
+DROP TABLE IF EXISTS [dbo].[f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan]
+GO
+
+/****** Object:  Table [dbo].[f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan]    Script Date: 28.10.2022 14:48:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan](
+	[tilv] [nvarchar](4) NULL,
+	[aineisto] [nvarchar](50) NULL,
+	[lahde] [nvarchar](2) NULL,
+	[lahde_id] [int] NULL,
+	[sektori] [nvarchar](1) NULL,
+	[sektori_id] [int] NULL,
+	[ikar7] [nvarchar](2) NULL,
+	[ikar7_id] [int] NULL,
+	[sp] [nvarchar](2) NULL,
+	[sp_id] [int] NULL,
+	[kansalr1] [nvarchar](2) NULL,
+	[kansalr1_id] [int] NULL,
+	[syntyp2] [nvarchar](2) NULL,
+	[syntyp2_id] [int] NULL,
+	[ophal] [nvarchar](2) NULL,
+	[ophal_id] [int] NULL,
+	[rahlahde_v2] [nvarchar](2) NULL,
+	[rahlahde_v2_id] [int] NULL,
+	[iscfi2013] [nvarchar](4) NULL,
+	[iscfi2013_id] [int] NULL,
+	[kaste_t2] [nvarchar](2) NULL,
+	[kaste_t2_id] [int] NULL,
+	[alvv] [nvarchar](4) NULL,
+	[allk] [nvarchar](2) NULL,
+	[allk_id] [int] NULL,
+	[lkm] [int] NULL
+) ON [PRIMARY]
+
+GO
+
+
---
+++ db/sql/6588__create_proc_vipunentk_dw_p_lataa_f_2_8f_2_9d.sql
@@ -0,0 +1,70 @@
+USE [VipunenTK_DW]
+GO
+
+/****** Object:  StoredProcedure [dbo].[p_lataa_f_1_21_1_22]    Script Date: 21.10.2022 19:22:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+--drop table VipunenTK.dbo.f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan
+--select * into VipunenTK.dbo.f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan from ( ... ) q
+
+CREATE OR ALTER PROCEDURE [dbo].[p_lataa_f_2_8f_2_9d] AS
+
+TRUNCATE TABLE VipunenTK.dbo.f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan;
+
+INSERT VipunenTK.dbo.f_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan
+
+SELECT
+	 tilv
+	,aineisto
+	,f.lahde
+	,lahde_id = coalesce(d24.id,-1)
+	,sektori = left(f.lahde, 1)
+	,sektori_id = coalesce(d23.id,-1)
+
+	,ikar7
+	,ikar7_id = coalesce(d0.id,-1)
+	,sp
+	,sp_id = coalesce(d1.id,-1)
+	,kansalr1
+    ,kansalr1_id = coalesce(d2.id,-1)
+	,syntyp2
+    ,syntyp2_id = coalesce(d3.id,-1)
+
+	,ophal
+	,ophal_id = coalesce(d25.id,-1)
+	,rahlahde_v2
+	,rahlahde_v2_id = coalesce(d26.id,-1)
+	,f.iscfi2013
+	,iscfi2013_id = coalesce(d8.id,-1)
+	,kaste_t2
+	,kaste_t2_id = coalesce(d9.id,-1)
+	,alvv
+	,allk
+	,allk_id = coalesce(d27.kausi_id,-1)
+
+	,lkm
+
+FROM VipunenTK_DW.dbo.sa_2_8f_2_9d_Korkeakouluopiskelijat_ja_tutkinnot_syntyperan_mukaan f 
+
+LEFT JOIN VipunenTK..d_ika d0 on d0.ika_avain = (case when f.ikar7 = 15 then '19655v19' else '19655v'+f.ikar7 end)
+LEFT JOIN VipunenTK..d_sukupuoli AS d1 ON d1.sukupuoli_koodi = f.sp
+LEFT JOIN VipunenTK..d_kansalaisuus_versio1 AS d2 ON d2.kansalaisuus_versio1_koodi = f.kansalr1
+LEFT JOIN VipunenTK..d_syntypera AS d3 ON d3.syntypera_koodi = f.syntyp2
+LEFT JOIN VipunenTK..d_koulutusluokitus AS d8 ON d8.koulutusluokitus_avain = 'ISCFI'+f.iscfi2013
+LEFT JOIN VipunenTK..d_koulutusluokitus AS d9 ON d9.koulutusluokitus_avain = 'KASTET2'+f.kaste_t2
+LEFT JOIN VipunenTK..d_koulutussektori d23 ON d23.koulutussektori_koodi = left(f.lahde, 1)
+LEFT JOIN VipunenTK..d_lahde d24 ON d24.lahde_koodi = f.lahde
+LEFT JOIN VipunenTK..d_opetushallinnon_koulutus d25 ON d25.opetushallinnon_koulutus_koodi = f.ophal
+LEFT JOIN VipunenTK..d_rahoituslahde d26 ON d26.rahoituslahde_koodi = f.rahlahde_v2 --koodi 1 <-> 1,2,3
+LEFT JOIN VipunenTK..d_kausi d27 ON d27.kausi_id = concat('1900',f.allk) --alvv karkeistettu, ohjattu '19XX'
+
+GO
+
