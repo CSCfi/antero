0ba50aafe3f3f188887021b3459aa6cf1b58c89a
juhasyrj <juha.syrjala@csc.fi>
2022-11-02 17:56:50 +0200

    CSCVIPUNEN-3097: alter etl

---
+++ db/sql/6606__alter_view_v_amos_spl_vaikuttavuus_maakunnat.sql
@@ -0,0 +1,32 @@
+USE [ANTERO]
+GO
+
+/****** Object:  View [dw].[v_amos_spl_vaikuttavuus_maakunnat]    Script Date: 21.10.2022 12:03:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER VIEW [dw].[v_amos_spl_vaikuttavuus_maakunnat] AS
+
+SELECT DISTINCT tilv AS Tilastovuosi
+      ,[tilvasmaak] as [Koodi Maakunta]
+      ,(select top 1 maakunta_fi from dw.d_alueluokitus where maakunta_koodi = f.tilvasmaak and maakunta_fi != 'Ulkomaat')
+		AS Maakunta
+      ,[paatoiminen_tyollinen] AS [v Päätoiminen työllinen]
+      ,[tyoton] AS [v Työtön]
+      ,[tyollinen_opiskelija] [v Työllinen opiskelija]
+      ,[tyolliset] AS [v Työllinen]
+      ,[yhteensa] AS [v Yhteensä]
+      ,[tyolliset_per_yhteensa] 
+      ,[indeksi_jakaja]
+      ,[aluekerroin] AS [v Aluekerroin]
+  FROM [dw].[f_amos_spl_vaikuttavuus_maakunnat] f
+
+GO
+
+
