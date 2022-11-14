84f723fd42f01bfdd3095b3136c296d755dbfe07
vavainio <valtteri.vainio@csc.fi>
2022-11-14 16:27:49 +0200

    CSCVIPUNEN-2926 - R scripts and PDI job + transformations

---
+++ pdi_integrations/vipunen/R/Hae_raportit.R
@@ -0,0 +1,233 @@
+# Asennettavat kirjastot ----------------------------------------------------------------------
+
+#install.packages('stringr')
+#install.packages('sjmisc')
+#install.packages('dplyr')
+#install.packages('BBmisc')
+#install.packages('qdapRegex')
+
+# Funktio ----------------------------------------------------------------------
+
+hae_raportit <- function(kieli) {
+  
+  # Tarkistaa haetaanko raportit oikealla kielella. Funktio hyvaksyy suomen (fi), ruotsin (sv) ja englannin (en)
+  if ((kieli %in% c('en','sv','fi')) == TRUE) {
+    
+    # Funktio maarittaa muuttujille arvot valitun kielen mukaan
+    if (kieli == "fi") {
+      kieli_viittaus <- "fi-fi"
+      sivu = "Sivut"
+      nimi = "Raportit"
+    }
+    if (kieli == "sv") {
+      kieli_viittaus <- "sv-fi"
+      sivu = "Sidor"
+      nimi = "Rapporter"
+    }
+    if (kieli == "en") {
+      kieli_viittaus <- "en-gb"
+      sivu = "Pages"
+      nimi = "Reports"
+    }
+    
+    # Etusivun lahdekoodi
+    vipunenrL <- readLines(paste0('https://vipunen.fi/', kieli_viittaus), encoding = "UTF-8")
+    
+    # Haetaan etusivulta paaosiot
+    osiot <- data.frame(strsplit(vipunenrL[which(str_detect(vipunenrL, 'zz2_RootAspMenu'))+1], paste0('/', kieli_viittaus)))
+    osiot <- data.frame(sub(">.*", "", osiot[3:nrow(osiot),1]), stringsAsFactors = FALSE)
+    osiot <- data.frame(substr(osiot[,1], 1,nchar(osiot[,1])-1), stringsAsFactors = FALSE)
+    Encoding(osiot[[1]]) <- "UTF-8"
+    
+    # Luodaan tyhja dataframe raporteille
+    raportit <- data.frame()
+    
+    # For-looppi kay lapi kaikki paaosiot
+    for (i in 1:nrow(osiot)) {
+      osoite <- readLines(textConnection(paste0('https://vipunen.fi/', kieli_viittaus, osiot[i,1])))
+      Encoding(osoite) <- "UTF-8"
+      
+      # Haetaan paaosioiden alasivut paaosion lahdekoodista
+      osiorL <- readLines(osoite)
+      alaosiot <- data.frame(strsplit(osiorL[which(str_detect(osiorL, 'zz4_RootAspMenu'))+1], paste0('/', kieli_viittaus)))
+      alaosiot <- data.frame(sub(">.*", "", alaosiot[2:nrow(alaosiot),1]), stringsAsFactors = FALSE)
+      alaosiot <- data.frame(substr(alaosiot[,1], 1,nchar(alaosiot[,1])-1), stringsAsFactors = FALSE)
+      
+      # Piilotetut sivut, jotka eivät nay lahdekoodissa
+      if (kieli == "fi" & i == 2){
+        alaosiot[nrow(alaosiot)+1,1] <- "/perus/Sivut/Ainevalinnat-2000-2019.aspx"
+      }
+      else if (kieli == "fi" & i == 3){
+        alaosiot[nrow(alaosiot)+1,1] <- "/lukio/Sivut/Ainevalinnat-2010-2017.aspx"
+      }
+      else if (kieli == "fi" & i == 5){
+        alaosiot[nrow(alaosiot)+1,1] <- "/amk/Sivut/taloustiedot-2014-2018.aspx"
+      }
+      else if (kieli == "fi" & i == 6) {
+        alaosiot[nrow(alaosiot)+1,1] <- "/yliopisto/Sivut/Taloustiedot-2012-2018.aspx"
+      }
+      else if (kieli == "sv" & i == 2) {
+        alaosiot[nrow(alaosiot)+1,1] <- "/grundlaggande/Sidor/Ainevalinnat-2000-2019.aspx"
+      }
+      
+      Encoding(alaosiot[[1]]) <- "UTF-8"
+      
+      # For-looppi kay lapi kaikki alasivut
+      for (j in 1:nrow(alaosiot)){
+        
+        # Alasivun lahdekoodi
+        osoite <- readLines(textConnection(paste0('https://vipunen.fi/', kieli_viittaus, alaosiot[j,1])))
+        Encoding(osoite) <- "UTF-8"
+        alaosiorL <- readLines(osoite)
+        
+        # Valitaan lahdekoodista ne rivit joissa on viittaus raporttiin
+        raportit_rivit <- alaosiorL[which(str_detect(alaosiorL, nimi))]
+        
+        # For-looppi kay lapi kaikki valitut rivit
+        for (k in 1:length(raportit_rivit)){
+          
+          # Lisataan raportit-dataframeen, alasivun lahdekoodissa nakyvat raportit
+          rivi <- data.frame(strsplit(raportit_rivit[k], paste0('/',kieli_viittaus,'/')))
+          if (nrow(rivi) > 0){
+            rivi[,2] <- osiot[i,1]
+            rivi[,3] <- alaosiot[j,1]
+            rivi[,4] <- rm_between(rivi[,1], '>', '</span>', extract=TRUE)[[1]][1]
+            names(rivi)[1] <- "Raportti"
+            names(rivi)[2] <- "Osio"
+            names(rivi)[3] <- "Alaosio"
+            raportit <- union_all(rivi, raportit)
+          }
+        }
+      }
+    }
+    
+    # Poistetaan duplikaattiraportit
+    raportit <- unique(raportit)
+    
+    
+    # Selvitetaan ja siistitaan raporttien nakokulmat
+    for (n in 1:nrow(raportit)){
+      raportit[n,4] <- rm_between(raportit[n,1], '>', '</a>', extract=TRUE)[[1]][1]
+    }
+    raportit[,4] <- sub('.*">', '', raportit[,4])
+    raportit[,4] <- sub('<.*', '', raportit[,4])
+    names(raportit)[4] <- "Näkökulma"
+    Encoding(raportit[,4]) <- "UTF-8"
+
+    raportit[,4] <- str_replace_all(raportit[,4], "&#160;", " ")
+    raportit[,4] <- str_replace_all(raportit[,4], "&quot;", "")
+    
+    # Poistetaan turhat rivit ja muotoillaan raportit yhdenmukaisiksi
+    raportit <- raportit[substr(raportit[,1],1,nchar(nimi)) == nimi,]
+    raportit$Raportti <- gsub("web", "Web", raportit$Raportti)
+    
+    # Jaetaan raportit kahteen osaan: ("?Web=1" ja "")
+    raportit1 <- raportit[grep("Web", raportit$Raportti) ,]
+    raportit2 <- raportit[!grepl("Web", raportit$Raportti),]
+    raportit1[,1] <- sub("Web.*", "", raportit1[,1])
+    
+    Vipunen_raportit <- raportit1
+    
+    # Jatetaan toiseen dataframeen vain excel-tiedostot
+    
+    raportit2 <- raportit2[grepl("xls", raportit2$Raportti),]
+    raportit2 <- raportit2[grepl(nimi , raportit2$Raportti),]
+    raportit2 <- raportit2[!grepl("admintest", raportit2$Raportti),]
+    
+    # Muotoillaan raporttien nimet
+    if(nrow(raportit2) > 0){
+      raportit2$Raportti <- sub("xlsb.*", "", raportit2$Raportti[1:nrow(raportit2)])
+      raportit2[substr(raportit2$Raportti, nchar(raportit2$Raportti), nchar(raportit2$Raportti)) == "." , 1] <- paste0(raportit2[substr(raportit2$Raportti, nchar(raportit2$Raportti), nchar(raportit2$Raportti)) == "." , 1], "xlsb")
+      raportit2$Raportti <- sub("xlsx.*", "", raportit2$Raportti[1:nrow(raportit2)])
+      raportit2[substr(raportit2$Raportti, nchar(raportit2$Raportti), nchar(raportit2$Raportti)) == "." , 1] <- paste0(raportit2[substr(raportit2$Raportti, nchar(raportit2$Raportti), nchar(raportit2$Raportti)) == "." , 1], "xlsx")
+      for (i in 1:nrow(raportit2)){
+        raportit2$Raportti[i] <- strsplit(raportit2$Raportti, '/')[[i]][2]
+      }
+    }
+    
+    # Muotoillaan raporttien nimet ja liitetaan dataframeen raportit2-dataframe
+    Vipunen_raportit$Raportti <- sub("Web.*", "", Vipunen_raportit$Raportti[1:nrow(Vipunen_raportit)])
+    Vipunen_raportit$Raportti <- substr(Vipunen_raportit$Raportti, nchar(nimi) + 2 ,nchar(Vipunen_raportit$Raportti)-1)
+    Vipunen_raportit$Raportti[substr(Vipunen_raportit$Raportti,1,1) == "/"] <- str_sub(Vipunen_raportit$Raportti[substr(Vipunen_raportit$Raportti,1,1) == "/"], 2, -2)
+    Vipunen_raportit <- Vipunen_raportit[!grepl(".pdf", Vipunen_raportit$Raportti),]
+    if (nrow(raportit2) > 0){
+      Vipunen_raportit <- union_all(Vipunen_raportit, raportit2)
+    }
+    
+    # Muotoillaan raporttien ja osioiden nimia
+    Vipunen_raportit$Raportti <- gsub("%20", " ", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Raportti <- gsub("%C3%A4", "ä", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Raportti <- gsub("%C3%B6", "ö", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Raportti <- gsub("%C3%B6", "ö", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Raportti <- gsub("%28", "(", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Raportti <- gsub("%29", ")", Vipunen_raportit$Raportti)
+    Vipunen_raportit$Osio <- substr(Vipunen_raportit$Osio, 2,nchar(Vipunen_raportit$Osio))
+    Vipunen_raportit$Alaosio <- gsub(".aspx", "", Vipunen_raportit$Alaosio)
+    Vipunen_raportit$Alaosio <- substr(Vipunen_raportit$Alaosio, str_locate(Vipunen_raportit$Alaosio, sivu)+6, nchar(Vipunen_raportit$Alaosio))
+
+    # Poistetaan duplikaattiraportit
+    Nakokulmat <- Vipunen_raportit[!is.na(Vipunen_raportit[,4]) & Vipunen_raportit[,4] != "",]
+    Vipunen_raportit <- unique(Vipunen_raportit[,1:3])
+    Nakokulmat[,5] <- paste0(Nakokulmat$Raportti,Nakokulmat$Osio, Nakokulmat$Alaosio)
+    Nakokulmat <- Nakokulmat[, 4:5]
+    Vipunen_raportit[,4] <- paste0(Vipunen_raportit$Raportti,Vipunen_raportit$Osio, Vipunen_raportit$Alaosio)
+    names(Vipunen_raportit)[4] <- "Avain"
+    names(Nakokulmat)[2] <- "Avain"
+    Vipunen_raportit <- merge(Vipunen_raportit, Nakokulmat, by.x = "Avain" , by.y = "Avain", all.x = TRUE)
+    Vipunen_raportit <- Vipunen_raportit[,c(2:5)]
+    Encoding(Vipunen_raportit$Osio) <- "unknown"
+    Encoding(Vipunen_raportit$Alaosio) <- "unknown"
+    
+    # Tallennetaan tiedot exceliin
+    write.csv2(Vipunen_raportit[,c(1:4)], paste0("D:/pdi_integrations/data/vipunen/raportit/Raportit_", kieli, ".csv"), row.names = FALSE)
+    return(Vipunen_raportit)
+  }
+  else {
+    return(0)
+  }
+}
+
+# Ajo ----------------------------------------------------------------------
+
+Alku <- Sys.time()
+
+setwd("D:/antero/pdi_integrations/vipunen/R")
+
+if(require("renv") == FALSE) {
+  install.packages("renv", version = "1.4.1" ,repos = "http://cran.us.r-project.org")
+  library("renv")
+}
+
+renv::init(force = TRUE)
+renv::restore()
+
+if(require("stringr") == FALSE) {
+  install.packages("stringr", version = "1.4.1" ,repos = "http://cran.us.r-project.org")
+  library("stringr")
+}
+if(require("dplyr") == FALSE) {
+ install.packages("dplyr",repos = "http://cran.us.r-project.org")
+ library("dplyr")
+}
+
+if(require("BBmisc") == FALSE) {
+  install.packages("BBmisc", version = "1.11" ,repos = "http://cran.us.r-project.org")
+  library("BBmisc")
+}
+if(require("qdapRegex") == FALSE) {
+  install.packages("qdapRegex", version = "0.7.5", repos = "http://cran.us.r-project.org")
+  library("qdapRegex")
+}
+
+
+
+raportit_fi <- hae_raportit("fi")
+
+raportit_sv <- hae_raportit("sv")
+
+raportit_en <-hae_raportit("en")
+
+Loppu <- Sys.time()
+Kesto <- Loppu-Alku
+print(paste("Ajaminen kesti ",Kesto))
+
