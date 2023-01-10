3f938891634130b07291399eefa645266ec83029
juhasyrj <juha.syrjala@csc.fi>
2023-01-10 19:41:39 +0200

    CSCVIPUNEN-3167, CSCVIPUNEN-3168: alter sa procs, update dimension

---
+++ db/sql/6856__drop_create_table_d_opintojen_kulku_priorisoitu_tilanne.sql
@@ -0,0 +1,57 @@
+USE [ANTERO]
+GO
+/****** Object:  Table [dw].[d_opintojen_kulku_priorisoitu_tilanne]    Script Date: 10.1.2023 19:33:33 ******/
+DROP TABLE [dw].[d_opintojen_kulku_priorisoitu_tilanne]
+GO
+/****** Object:  Table [dw].[d_opintojen_kulku_priorisoitu_tilanne]    Script Date: 10.1.2023 19:33:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dw].[d_opintojen_kulku_priorisoitu_tilanne](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[koulutus_kytkin] [varchar](50) NULL,
+	[priorisoitu_tilanne_koodi] [int] NULL,
+	[priorisoitu_tilanne_fi] [varchar](100) NULL,
+	[priorisoitu_tilanne_se] [varchar](100) NULL,
+	[priorisoitu_tilanne_en] [varchar](100) NULL
+) ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ON 
+
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (-1, N'tuntematon', 99, N'Tieto puuttuu', N'Information saknas', N'Missing data')
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (1, N'ammatillinen', 1, N'Valmistunut alkuperäisestä tutkinnosta', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (2, N'ammatillinen', 2, N'Valmistunut toisesta ammatillisesta tutkinnosta', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (3, N'ammatillinen', 3, N'Jatkaa alkuperäisessä tutkinnossa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (4, N'ammatillinen', 4, N'Jatkaa toisessa amm. tutkinnossa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (5, N'ammatillinen', 5, N'Jatkaa TUVA/VALMA-koulutuksessa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (6, N'ammatillinen', 6, N'Jatkaa TELMA-koulutuksessa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (7, N'ammatillinen', 7, N'Jatkaa muussa ammatillisessa koulutuksessa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (8, N'ammatillinen', 8, N'Keskeyttänyt ammatillisen koulutuksen', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (9, N'yleissivistava2aste', 1, N'Suorittanut aloitetun oppimäärän', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (10, N'yleissivistava2aste', 2, N'Suorittanut eri yleissiv. oppimäärän', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (11, N'yleissivistava2aste', 3, N'Jatkaa alkuperäisessä oppimäärässä', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (12, N'yleissivistava2aste', 4, N'Jatkaa toisessa yleissiv. oppimäärässä', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (13, N'yleissivistava2aste', 6, N'Jatkaa ammatillisessa koulutuksessa', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (14, N'yleissivistava2aste', 8, N'Keskeyttänyt toisen asteen', NULL, NULL)
+GO
+INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] ([id], [koulutus_kytkin], [priorisoitu_tilanne_koodi], [priorisoitu_tilanne_fi], [priorisoitu_tilanne_se], [priorisoitu_tilanne_en]) VALUES (15, N'yleissivistava2aste', 5, N'Suorittanut ammatillisen tutkinnon', NULL, NULL)
+GO
+SET IDENTITY_INSERT [dw].[d_opintojen_kulku_priorisoitu_tilanne] OFF
+GO
---
+++ db/sql/6859__alter_proc_koski_sa_p_lataa_temp_koski_opintojen_kulku_3a_ammatillinen.sql
@@ -0,0 +1,187 @@
+USE [Koski_SA]
+GO
+/****** Object:  StoredProcedure [sa].[p_lataa_temp_koski_opintojen_kulku_3a_ammatillinen]    Script Date: 10.1.2023 19:34:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER PROCEDURE [sa].[p_lataa_temp_koski_opintojen_kulku_3a_ammatillinen]  AS
+
+
+DROP TABLE IF EXISTS Koski_SA.sa.temp_opintojen_kulku_3a_priorisoitu_tilanne_ammatillinen
+
+SELECT DISTINCT
+ kohortti_vp
+ ,[oppija_oid]
+ ,opiskeluoikeus_oid
+ ,koulutus_kytkin
+ ,tutkinto_koodi
+ ,oppimaara = NULL
+ ,ika
+ ,ika_aloittaessa
+ ,ika_paattyessa
+ ,alkamispaiva
+ ,paattymispaiva
+ ,lopputulema
+ ,sarja
+ ,priorisoitu_tilanne_amm_koodi
+ ,priorisoitu_tilanne_amm = 
+	CASE
+		WHEN priorisoitu_tilanne_amm_koodi = 1 THEN 'Valmistunut alkuperäisestä tutkinnosta'
+		WHEN priorisoitu_tilanne_amm_koodi = 2 THEN 'Valmistunut toisesta ammatillisesta tutkinnosta'
+		WHEN priorisoitu_tilanne_amm_koodi = 3 THEN 'Jatkaa alkuperäisessä tutkinnossa'
+		WHEN priorisoitu_tilanne_amm_koodi = 4 THEN 'Jatkaa toisessa amm. tutkinnossa'
+		WHEN priorisoitu_tilanne_amm_koodi = 5 THEN 'Jatkaa TUVA/VALMA-koulutuksessa'
+		WHEN priorisoitu_tilanne_amm_koodi = 6 THEN 'Jatkaa TELMA-koulutuksessa'
+		WHEN priorisoitu_tilanne_amm_koodi = 7 THEN 'Jatkaa muussa ammatillisessa koulutuksessa'
+		WHEN priorisoitu_tilanne_amm_koodi = 8 THEN 'Keskeyttänyt ammatillisen koulutuksen' 
+		ELSE 'Tieto puuttuu' 
+	END
+ ,priorisoitu_tilanne_valmentava_koodi = NULL
+ ,priorisoitu_tilanne_valmentava = NULL
+ ,priorisoitu_tilanne_yleissivistava_koodi = NULL
+ ,priorisoitu_tilanne_yleissivistava = NULL
+ ,vaihtanut_jarjestajaa = case when priorisoitu_tilanne_amm_koodi = 8 then 8 else vaihtanut_jarjestajaa end --käytetään prion 8 selitettä myös vaihdoksille
+ ,vaihtanut_oppilaitosta = case when priorisoitu_tilanne_amm_koodi = 8 then 8 else vaihtanut_oppilaitosta end --käytetään prion 8 selitettä myös vaihdoksille
+INTO Koski_SA.sa.temp_opintojen_kulku_3a_priorisoitu_tilanne_ammatillinen
+FROM(
+SELECT
+ kohortti_vp
+ ,[oppija_oid]
+ ,opiskeluoikeus_oid
+ ,koulutus_kytkin
+ ,tutkinto_koodi
+ ,alkamispaiva
+ ,paattymispaiva
+ ,lopputulema
+ ,ika
+ ,ika_aloittaessa
+ ,ika_paattyessa
+ ,sarja
+ ,priorisoitu_tilanne_amm_koodi
+ ,vaihtanut_jarjestajaa = min(vaihtanut_jarjestajaa)
+ ,vaihtanut_oppilaitosta = min(vaihtanut_oppilaitosta)
+FROM(
+SELECT
+ kohortti_vp
+ ,[oppija_oid]
+ ,opiskeluoikeus_oid
+ ,koulutus_kytkin
+ ,tutkinto_koodi
+ ,alkamispaiva
+ ,paattymispaiva
+ ,lopputulema
+ ,ika
+ ,ika_aloittaessa
+ ,ika_paattyessa
+ ,sarja
+ ,priorisoitu_tilanne_amm_koodi
+ ,min_priorisoitu_tilanne_amm_koodi --vaihdosten kytkemiseksi priorisointiin ylemmässä kyselyssä
+  = min(priorisoitu_tilanne_amm_koodi) over(partition by kohortti_vp
+														,[oppija_oid]
+														,opiskeluoikeus_oid
+														,koulutus_kytkin
+														,tutkinto_koodi
+														,alkamispaiva
+														,paattymispaiva
+														,lopputulema
+														,ika
+														,ika_aloittaessa
+														,ika_paattyessa
+														,sarja)
+ ,vaihtanut_jarjestajaa --= MIN(vaihtanut_jarjestajaa)
+ ,vaihtanut_oppilaitosta --= MIN(vaihtanut_oppilaitosta)
+FROM(
+SELECT 
+ q1.kohortti_vp
+ ,q1.[oppija_oid]
+ ,q1.opiskeluoikeus_oid
+ ,q1.koulutus_kytkin
+ ,q1.tutkinto_koodi
+ ,q1.alkamispaiva
+ ,q1.paattymispaiva
+ ,q1.lopputulema
+ ,ika
+ ,ika_aloittaessa
+ ,ika_paattyessa
+ ,sarja
+ ,tarkastuspaiva
+ ,priorisoitu_tilanne_amm_koodi = 
+		CASE
+			WHEN taso1 = 'Valmistunut' OR (valm.tutkinto_koodi = q1.tutkinto_koodi /*JUHAN TESTI, SIIRTO LOPUN RAJAUKSEEN: and valm.vahvistus_paiva BETWEEN q1.alkamispaiva AND tarkastuspaiva*/) THEN 1
+			WHEN q1.tutkinto_koodi != valm.tutkinto_koodi 
+				/*JUHAN TESTI*/ AND valm.tutkinto_koodi not in ('301101','301102','301103','301104') 
+				/*JUHAN TESTI, SIIRTO LOPUN RAJAUKSEEN: and valm.vahvistus_paiva BETWEEN q1.alkamispaiva AND tarkastuspaiva*/ 
+			  THEN 2
+			WHEN taso1 = 'Koulutus kesken' OR (q1.tutkinto_koodi = uusi.tutkinto_koodi AND uusi.tila IN ('lasna', 'loma', 'valiaikaisestikeskeytynyt', 'valmistunut')) THEN 3
+			WHEN uusi.suorituksen_tyyppi = 'ammatillinentutkinto' AND uusi.tila IN ('lasna', 'loma', 'valiaikaisestikeskeytynyt', 'valmistunut') AND q1.tutkinto_koodi != uusi.tutkinto_koodi THEN 4
+		--	WHEN uusi.suorituksen_tyyppi in ('valma', 'telma') AND uusi.tila IN ('lasna', 'loma') THEN 5
+			WHEN (uusi.suorituksen_tyyppi in ('valma') OR uusi.tuva_jarjestamislupa = 'ammatillinen') AND uusi.tila IN ('lasna', 'loma', 'valiaikaisestikeskeytynyt', 'valmistunut') THEN 5
+			WHEN uusi.suorituksen_tyyppi in ('telma') AND uusi.tila IN ('lasna', 'loma', 'valiaikaisestikeskeytynyt', 'valmistunut') THEN 6
+			WHEN uusi.suorituksen_tyyppi = 'muuammatillinenkoulutus'  AND uusi.tila IN ('lasna', 'loma', 'valiaikaisestikeskeytynyt', 'valmistunut') THEN 7 
+			ELSE 8 --ei enää kaatoluokkaa 99, vaan ilmaistaan ekspliittisesti yllä mitkä kelpuutetaan
+		END
+ ,vaihtanut_jarjestajaa =
+		CASE
+			WHEN taso1 = 'Valmistunut' then 0
+			/*JUHAN TESTI*/ --WHEN valm.koulutustoimija_oid IS NULL AND uusi.koulutustoimija_oid IS NULL THEN 0
+			WHEN q1.koulutustoimija_oid = valm.koulutustoimija_oid THEN 0
+			WHEN q1.koulutustoimija_oid != valm.koulutustoimija_oid THEN 1
+			WHEN taso1 = 'Koulutus kesken' then 0
+			WHEN q1.koulutustoimija_oid = uusi.koulutustoimija_oid THEN 0
+			WHEN q1.koulutustoimija_oid != uusi.koulutustoimija_oid THEN 1
+			/*JUHAN TESTI*/ --else 1
+			else 99
+		END
+,vaihtanut_oppilaitosta = 
+		CASE
+			WHEN taso1 = 'Valmistunut' then 0
+			/*JUHAN TESTI*/ --WHEN valm.oppilaitos_oid IS NULL AND uusi.oppilaitos_oid IS NULL THEN 0
+			WHEN q1.oppilaitos_oid = valm.oppilaitos_oid THEN 0
+			WHEN q1.oppilaitos_oid != valm.oppilaitos_oid THEN 1
+			WHEN taso1 = 'Koulutus kesken' then 0
+			WHEN q1.oppilaitos_oid = uusi.oppilaitos_oid THEN 0
+			WHEN q1.oppilaitos_oid != uusi.oppilaitos_oid THEN 1
+			/*JUHAN TESTI*/ --else 1
+			else 99 
+		END
+  FROM Koski_SA.sa.temp_opintojen_kulku_2a_poiminta_ammatillinen q1
+
+  -- Join muihin valmistumisiin kuin alkuperäiseen
+  LEFT JOIN Koski_SA.sa.temp_koski_opintojen_kulku_2a_valmistumiset valm 
+	ON valm.oppija_oid = q1.oppija_oid  
+	  AND valm.opiskeluoikeus_oid != q1.opiskeluoikeus_oid 
+	  /*JUHAN TESTI*/ AND valm.vahvistus_paiva BETWEEN q1.alkamispaiva AND q1.tarkastuspaiva 
+
+  -- Join kaikkiin muihin toisen asteen opiskeluoikeuksiin
+  LEFT JOIN Koski_SA.sa.temp_koski_opintojen_kulku_2c_muut_opiskeluoikeudet uusi 
+	ON uusi.oppija_oid = q1.oppija_oid 
+	  AND uusi.opiskeluoikeus_oid != q1.opiskeluoikeus_oid 
+	  AND q1.tarkastuspaiva BETWEEN uusi.alku AND COALESCE(uusi.loppu, '9999-01-01')
+
+  ) q2
+
+  ) q3
+
+  WHERE priorisoitu_tilanne_amm_koodi = min_priorisoitu_tilanne_amm_koodi
+  
+  GROUP BY kohortti_vp
+	 ,[oppija_oid]
+	 ,opiskeluoikeus_oid
+	 ,sarja
+	 ,priorisoitu_tilanne_amm_koodi
+	 ,koulutus_kytkin
+	 ,tutkinto_koodi
+	 ,alkamispaiva
+	 ,paattymispaiva
+	 ,lopputulema
+	 ,ika
+	 ,ika_aloittaessa
+	 ,ika_paattyessa
+
+ ) q4
+
