b5b1b2c7f4fa412fdd232eb24a657adae7d9641c
pankalai <anssi.pankalainen@csc.fi>
2023-01-10 18:21:18 +0200

    ehoksin nettolaskenta kuntoon

---
+++ db/sql/6854__alter_table_sa_ehoks_osaamisen_hankkimisjaksot_netto.sql
@@ -0,0 +1,23 @@
+USE [ANTERO]
+GO
+/****** Object:  Table [sa].[sa_ehoks_osaamisen_hankkimisjaksot_netto]    Script Date: 10.1.2023 18:09:54 ******/
+DROP TABLE IF EXISTS [sa].[sa_ehoks_osaamisen_hankkimisjaksot_netto]
+GO
+/****** Object:  Table [sa].[sa_ehoks_osaamisen_hankkimisjaksot_netto]    Script Date: 10.1.2023 18:09:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[sa_ehoks_osaamisen_hankkimisjaksot_netto]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [sa].[sa_ehoks_osaamisen_hankkimisjaksot_netto](
+	[vuosi] [int] NOT NULL,
+	[ehoks_id] [int] NOT NULL,
+	[opiskeluoikeus_oid] [varchar](100) NOT NULL,
+	[neljannes] [nvarchar](1) NOT NULL,
+	[neljannes_paivat] [numeric](17, 6) NOT NULL,
+	[osaamisen_hankkimistapa_koodi] [varchar](100) NOT NULL,
+	[tyopaikan_ytunnus] [varchar](9) NULL,
+	[paivat] [decimal](18, 6) NOT NULL
+) ON [PRIMARY]
+END
