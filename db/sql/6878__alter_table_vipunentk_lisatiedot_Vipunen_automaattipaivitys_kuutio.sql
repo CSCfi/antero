062d3602ff2d9438731b02647cd10a175e88b84c
juhasyrj <juha.syrjala@csc.fi>
2023-01-13 19:11:38 +0200

    CSCVIPUNEN-3294: add metadata column to skip pending tabulars

---
+++ db/sql/6878__alter_table_vipunentk_lisatiedot_Vipunen_automaattipaivitys_kuutio.sql
@@ -0,0 +1,10 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+IF NOT EXISTS (
+  SELECT * FROM sys.columns 
+  WHERE object_id = OBJECT_ID(N'[dbo].[[Vipunen_automaattipaivitys_kuutio]]') 
+	AND name = 'skip'
+)
+ALTER TABLE [dbo].[Vipunen_automaattipaivitys_kuutio]
+ADD [skip] int null default 0 with values
