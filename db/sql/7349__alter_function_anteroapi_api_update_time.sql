a203c6ddf68f096f39d9ce153374283f8518135b
vavainio <valtteri.vainio@csc.fi>
2023-02-08 14:01:56 +0200

    AnteroAPI - Alter function api_update_time - Updated time

---
+++ db/sql/7349__alter_function_anteroapi_api_update_time.sql
@@ -0,0 +1,30 @@
+USE [AnteroAPI]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[api_update_time]    Script Date: 8.2.2023 14:00:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER FUNCTION [dbo].[api_update_time] (@Date datetime)
+RETURNS DATE
+AS
+BEGIN
+
+    RETURN (SELECT 
+				case 
+					when @Date > cast(concat(cast(@Date  as date), ' 01:36:00:000') as datetime)
+					then DATEADD(DAY, 1, cast(@Date  as date))
+					else cast(@Date  as date)
+				end
+			)
+END;
+
+GO
+
+
+USE [ANTERO]
