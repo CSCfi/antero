e236fc6c3f12f5302d7e735cd3c7210849af9562
vavainio <valtteri.vainio@csc.fi>
2023-04-06 09:23:46 +0300

    Altered API procedures - Replaced ";" with ","

---
+++ db/sql/7563__alter_proc_anteroapi_p_lataa_api_lukio_opiskelijat_vuosi_tutkinto_esilataus.sql
@@ -0,0 +1,48 @@
+USE [AnteroAPI]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_api_lukio_opiskelijat_vuosi_tutkinto_esilataus]    Script Date: 6.4.2023 9:16:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dw].[p_lataa_api_lukio_opiskelijat_vuosi_tutkinto_esilataus] AS
+
+TRUNCATE TABLE [dw].[api_lukio_opiskelijat_vuosi_tutkinto];
+
+INSERT INTO [dw].[api_lukio_opiskelijat_vuosi_tutkinto]
+
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY f.vuosi
+							  ,REPLACE(f.tavoitetutkinto,';', ',')
+							  ,d1.organisaatio_fi) as defaultorder
+	,f.vuosi as tilastovuosi
+	,REPLACE(f.tavoitetutkinto,';', ',') as tutkinto
+	,d1.organisaatio_fi as koulutuksenJarjestaja
+	,count(distinct case when f.uusi_opiskelija = 1 then f.oppija_oid end) as uudetOpiskelijatLkm
+	,count(distinct f.oppija_oid) as opiskelijatLkm
+	,d1.organisaatio_koodi as koodiKoulutuksenJarjestaja
+	,CASE 
+		WHEN tavoitetutkinto = 'Ylioppilastutkinto' then '301101'
+		WHEN tavoitetutkinto = 'Ei tavoitetutkintoa' then '-1'
+		ELSE COALESCE(d2.koulutusluokitus_koodi, '-1')
+	END AS koodiTutkinto
+FROM ANTERO.dw.f_koski_lukio_opiskelijat_kuukausittain f
+LEFT JOIN ANTERO.dw.d_organisaatioluokitus d1 ON d1.organisaatio_oid = f.koulutustoimija_oid
+LEFT JOIN ANTERO.dw.d_koulutusluokitus d2 ON d2.koulutusluokitus_fi = f.tavoitetutkinto
+GROUP BY 
+f.vuosi, 
+REPLACE(f.tavoitetutkinto,';', ','), 
+d1.organisaatio_fi,
+d1.organisaatio_koodi,
+CASE 
+	WHEN tavoitetutkinto = 'Ylioppilastutkinto' then '301101'
+	WHEN tavoitetutkinto = 'Ei tavoitetutkintoa' then '-1'
+	ELSE COALESCE(d2.koulutusluokitus_koodi, '-1')
+END
+
+GO
+
+USE [ANTERO]
---
+++ db/sql/7564__alter_proc_anteroapi_p_lataa_api_koulutusluokitus_esilataus.sql
@@ -0,0 +1,72 @@
+USE [AnteroAPI]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_api_koulutusluokitus_esilataus]    Script Date: 6.4.2023 9:20:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [dw].[p_lataa_api_koulutusluokitus_esilataus] AS
+
+TRUNCATE TABLE [dw].[api_koulutusluokitus]
+
+INSERT INTO [dw].[api_koulutusluokitus]
+
+SELECT koulutusluokitus_avain
+,koulutusluokitus_koodi
+,REPLACE(koulutusluokitus_fi, ';', ',') as koulutusluokitus_fi
+,REPLACE(koulutusluokitus_sv, ';', ',') as koulutusluokitus_sv
+,REPLACE(koulutusluokitus_en, ';', ',') as koulutusluokitus_en
+,koulutusastetaso1_koodi
+,koulutusastetaso1_fi
+,koulutusastetaso1_sv
+,koulutusastetaso1_en
+,koulutusastetaso2_koodi
+,koulutusastetaso2_fi
+,koulutusastetaso2_sv
+,koulutusastetaso2_en
+,koulutusalataso1_koodi
+,koulutusalataso1_fi
+,koulutusalataso1_sv
+,koulutusalataso1_en
+,koulutusalataso2_koodi
+,koulutusalataso2_fi
+,koulutusalataso2_sv
+,koulutusalataso2_en
+,koulutusalataso3_koodi
+,koulutusalataso3_fi
+,koulutusalataso3_sv
+,koulutusalataso3_en
+,okmohjauksenala_koodi
+,okmohjauksenala_fi
+,okmohjauksenala_sv
+,okmohjauksenala_en
+,koulutussektori_koodi
+,koulutussektori_fi
+,koulutussektori_sv
+,koulutussektori_en
+,tutkintotyyppi_koodi
+,tutkintotyyppi_fi
+,tutkintotyyppi_sv
+,tutkintotyyppi_en
+,[Rahoitusmallialat amk 2021-2024_koodi] = d1.Ammattikorkeakoulut_tutkintotavoitteet_koodi
+,[Rahoitusmallialat amk 2021-2024_fi] = d1.Ammattikorkeakoulut_tutkintotavoitteet
+,[Rahoitusmallialat amk 2021-2024_sv] = d1.Ammattikorkeakoulut_tutkintotavoitteet_SV
+,[Rahoitusmallialat amk 2021-2024_en] = d1.Ammattikorkeakoulut_tutkintotavoitteet_EN
+,[Rahoitusmallialat yo 2021-2024_koodi] = d1.Yliopistot_tutkintotavoitteet2021_koodi
+,[Rahoitusmallialat yo 2021-2024_fi] = d1.Yliopistot_tutkintotavoitteet2021
+,[Rahoitusmallialat yo 2021-2024_sv] = d1.Yliopistot_tutkintotavoitteet2021_SV
+,[Rahoitusmallialat yo 2021-2024_en] = d1.Yliopistot_tutkintotavoitteet2021_EN
+
+FROM ANTERO.dw.d_koulutusluokitus f
+LEFT JOIN VipunenTK_lisatiedot.dbo.isced_suomi_ohjauksenala d1 
+  on f.koulutusastetaso1_koodi in ('6','7','8') and d1.iscfi2013_koodi = coalesce(nullif(f.koulutusalataso3_koodi, '9999'), '-1')
+
+
+GO
+
+USE [ANTERO]
