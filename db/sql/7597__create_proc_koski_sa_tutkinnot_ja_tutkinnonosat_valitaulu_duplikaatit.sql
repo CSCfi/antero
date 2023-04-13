86656f04780f355c261899131a735da2ead7f80a
pankalai <anssi.pankalainen@csc.fi>
2023-04-13 15:50:04 +0300

    CSCVIPUNEN-3166: tietokantamuutokset, mm. duplikaatti-sarakkeen lisäys välitauluun

---
+++ db/sql/7597__create_proc_koski_sa_tutkinnot_ja_tutkinnonosat_valitaulu_duplikaatit.sql
@@ -0,0 +1,251 @@
+USE [Koski_SA]
+GO
+/****** Object:  StoredProcedure [dbo].[p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu_duplikaatit]    Script Date: 13.4.2023 15:16:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu_duplikaatit] AS
+
+--
+--Duplikaattitutkintojen (sama tutkinto suoritettu samalla järjestäjällä saman vuoden aikana vähintään kahdesti) tunnistus
+--
+
+DROP TABLE IF EXISTS #cte;
+DROP TABLE IF EXISTS #dupl;
+
+--Perusjoukko temp-tauluun
+SELECT 
+	vt.id
+	,oppija_oid
+	,opiskeluoikeus_oid
+	,vt.paatason_suoritus_id
+	,vuosi
+	,koulutustoimija_oid = oa.jarj_oid
+	,vahvistus_paiva
+	,tutkinto_koodi
+	,osaamisala_koodiarvo
+	,tutkintonimike_koodiarvo
+	,dupl = null
+	,oa2.avain
+	,rnk_paiva = row_number () over (
+		partition by oppija_oid,oa.jarj_oid,tutkinto_koodi,vahvistus_paiva,tutkintonimike_koodiarvo,osaamisala_koodiarvo 
+		order by vt.id 
+	)
+	,cnt_tutknimike = count(tn.tutkintonimike_koodiarvo) over (
+		partition by oppija_oid,oa.jarj_oid,vt.paatason_suoritus_id,tutkinto_koodi,vahvistus_paiva
+	)
+INTO #cte
+FROM dbo.tutkinnot_ja_tutkinnonosat_valitaulu vt
+LEFT JOIN sa.sa_koski_paatason_suoritus_tutkintonimike tn on tn.paatason_suoritus_id = vt.paatason_suoritus_id
+LEFT JOIN ANTERO.dw.d_amos_spl_jarjestaja_linkki jl on jl.oid_avain = vt.koulutustoimija_oid
+OUTER APPLY (select jarj_oid = coalesce(jl.oid_amos_spl,jl.oid_avain)) oa
+OUTER APPLY (select avain = concat(oppija_oid,'|',oa.jarj_oid,'|',vuosi,'|',tutkinto_koodi)) oa2
+WHERE paataso=1 and suorittaa_koko_tutkintoa=1
+and oa2.avain in (
+	select concat(oppija_oid,'|',coalesce(spl.oid_amos_spl,spl.oid_avain),'|',vuosi,'|',tutkinto_koodi)
+	from dbo.tutkinnot_ja_tutkinnonosat_valitaulu vt2 
+	left join ANTERO.dw.d_amos_spl_jarjestaja_linkki spl on spl.oid_avain=vt2.koulutustoimija_oid
+	where 1=1
+	and vt2.suorittaa_koko_tutkintoa=vt.suorittaa_koko_tutkintoa 
+	and vt2.paataso=vt.paataso
+	group by oppija_oid,coalesce(spl.oid_amos_spl,spl.oid_avain),vuosi,tutkinto_koodi 
+	having count(*)>1
+)
+
+
+/*
+--Testitapauksien luonti
+INSERT INTO #cte (id,oppija_oid,opiskeluoikeus_oid,paatason_suoritus_id,vuosi,koulutustoimija_oid,vahvistus_paiva,tutkinto_koodi,osaamisala_koodiarvo,tutkintonimike_koodiarvo,rnk_paiva,cnt_tutknimike)
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-30 00:00:00.000','351407',null,null,1,1),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407',null,null,1,1);
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',1,1),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454',null,1,1);
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',1,1),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',2,1);
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-30 00:00:00.000','351407','2454','10111',1,1),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',1,2),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10113',1,2);
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',1,1),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-30 00:00:00.000','351407','2454','10111',1,2),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-30 00:00:00.000','351407','2454','10113',1,2);
+VALUES (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10111',1,2),
+	   (999998,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211461',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2454','10113',1,2),
+	   (999999,'1.2.246.562.24.94592415178','1.2.246.562.15.27730102773','21211462',2018,'1.2.246.562.10.45042175963','2018-05-31 00:00:00.000','351407','2453','10111',1,1);
+
+--Testiopiskelija
+select * from #cte where oppija_oid='1.2.246.562.24.94592415178'
+*/
+
+
+--Duplikaattien päättely
+SELECT distinct
+	id
+	,oppija_oid,opiskeluoikeus_oid,koulutustoimija_oid,tutkinto_koodi,vuosi,vahvistus_paiva,rnk_paiva
+	,dupl =
+		--Ei duplikaatti
+		----Jos molemmat epätyhjiä
+		----Jos toinen tyhjä ja toinen epätyhjä
+		----Jos molemmat tyhjiä
+		case 	
+			--Molemmat epätyhjiä
+			when 
+				c1.osaamisala_koodiarvo is not null and c1.tutkintonimike_koodiarvo is not null 
+				and ( 
+					--ei aiempaa samaa osaamisalaa
+					not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva <= c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.osaamisala_koodiarvo = c1.osaamisala_koodiarvo
+						and c2.rnk_paiva = 1
+					) or 
+					--ei aiempaa samaa tutkintonimikettä
+					not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva <= c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.tutkintonimike_koodiarvo = c1.tutkintonimike_koodiarvo
+						and c2.rnk_paiva = 1
+					) or
+					--aiemmin sama osaamisala ja sama tutkintonimike mutta vähemmän tutkintonimikkeitä
+					exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva <= c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.tutkintonimike_koodiarvo = c1.tutkintonimike_koodiarvo
+						and c2.osaamisala_koodiarvo = c1.osaamisala_koodiarvo
+						and c2.cnt_tutknimike < c1.cnt_tutknimike
+					) 
+				)
+				--ei myöhemmin sama osaamisala ja sama tutkintonimike ja enemmän tutkintonimikkeitä
+				and not exists (
+					select 1 
+					from #cte as c2 
+					where c2.avain = c1.avain
+					and c2.vahvistus_paiva >= c1.vahvistus_paiva 
+					and c2.id != c1.id
+					and c2.tutkintonimike_koodiarvo = c1.tutkintonimike_koodiarvo
+					and c2.osaamisala_koodiarvo = c1.osaamisala_koodiarvo
+					and c2.cnt_tutknimike > c1.cnt_tutknimike
+				) 
+				then 0
+
+			--Toinen tyhjä ja toinen epätyhjä
+			when 
+				(
+					c1.osaamisala_koodiarvo is null and c1.tutkintonimike_koodiarvo is not null 
+					--ei aiempaa samaa tutkintonimikettä
+					and not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva <= c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.tutkintonimike_koodiarvo = c1.tutkintonimike_koodiarvo
+						and (c2.cnt_tutknimike > c1.cnt_tutknimike or (c2.cnt_tutknimike = c1.cnt_tutknimike and c2.rnk_paiva = 1))
+					) 
+					--ei myöhempää samaa tutkintonimikettä ja (enemmän tutkintonimikkeitä tai osaamisala epätyhjä)
+					and not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva > c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.tutkintonimike_koodiarvo = c1.tutkintonimike_koodiarvo
+						and ((c2.cnt_tutknimike = c1.cnt_tutknimike and c2.osaamisala_koodiarvo is not null) or c2.cnt_tutknimike > c1.cnt_tutknimike)
+					) 
+				) 
+				or
+				(
+					c1.tutkintonimike_koodiarvo is null and c1.osaamisala_koodiarvo is not null 
+					--ei aiempaa samaa osaamisala
+					and not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.vahvistus_paiva <= c1.vahvistus_paiva 
+						and c2.id != c1.id
+						and c2.osaamisala_koodiarvo = c1.osaamisala_koodiarvo
+						and c2.rnk_paiva = 1
+					) 
+					--ei myöhempää samaa osaamisalaa ja epätyhjää tutkintonimikettä
+					and not exists (
+						select 1 
+						from #cte as c2 
+						where c2.avain = c1.avain
+						and c2.id != c1.id
+						and c2.osaamisala_koodiarvo = c1.osaamisala_koodiarvo
+						and c2.tutkintonimike_koodiarvo is not null
+					) 
+				) 
+				then 0
+
+			--Molemmat tyhjiä
+			when 
+				c1.osaamisala_koodiarvo is null and c1.tutkintonimike_koodiarvo is null 
+				--ei aiempaa suoritusta tai samalta päivältä korkeampaa prioriteettia
+				and not exists (
+					select 1 
+					from #cte as c2 
+					where c2.avain = c1.avain
+					and (c2.vahvistus_paiva < c1.vahvistus_paiva or (c2.vahvistus_paiva = c1.vahvistus_paiva and c2.rnk_paiva < c1.rnk_paiva))
+					and c2.id != c1.id
+				)
+				--ei sellaista aiempaa tai myöhempää suoritusta jossa oa tai tn epätyhjä
+				and not exists (
+					select 1 
+					from #cte as c2 
+					where c2.avain = c1.avain
+					and c2.id != c1.id
+					and (c2.osaamisala_koodiarvo is not null or c2.tutkintonimike_koodiarvo is not null)
+				) 
+				then 0
+		
+			else 1
+
+		end
+INTO #dupl
+FROM #cte as c1
+
+
+/* 
+--Tietty oppija
+SELECT d.id,d.oppija_oid,d.opiskeluoikeus_oid,c.paatason_suoritus_id,d.vuosi,d.koulutustoimija_oid,d.vahvistus_paiva,d.tutkinto_koodi,c.osaamisala_koodiarvo,c.tutkintonimike_koodiarvo,d.dupl 
+FROM #dupl d
+JOIN #cte c on c.id = d.id and c.rnk_paiva=d.rnk_paiva
+WHERE 1=1
+and d.oppija_oid = '1.2.246.562.24.10480571181'
+ORDER BY vuosi desc,oppija_oid,koulutustoimija_oid,vahvistus_paiva,tutkinto_koodi,d.rnk_paiva
+
+--Sellaiset, joilla kaikki suoritukset menee duplikaateiksi
+SELECT d.*,c.osaamisala_koodiarvo,c.tutkintonimike_koodiarvo FROM (
+	SELECT *,min_dupl=min(dupl) over (partition by oppija_oid,vuosi,koulutustoimija_oid,tutkinto_koodi) FROM #dupl
+) d
+JOIN #cte c on c.id = d.id and c.rnk_paiva=d.rnk_paiva
+WHERE min_dupl=1
+ORDER BY oppija_oid
+
+--Id:t jotka saa sekä arvon dupl=0 että dupl=1
+SELECT id FROM #dupl GROUP BY id HAVING count(distinct dupl)>1
+
+--Koko setti
+SELECT * FROM #dupl ORDER BY vuosi desc,id
+*/
+
+UPDATE vt
+SET vt.duplikaatti = 1
+FROM dbo.tutkinnot_ja_tutkinnonosat_valitaulu vt
+LEFT JOIN #dupl d on d.id = vt.id
+WHERE d.dupl = 1
+
+
+DROP TABLE #cte;
+DROP TABLE #dupl;
---
+++ db/sql/7599__alter_proc_amos_tutkinnot_ja_tutkinnonosat_koko.sql
@@ -0,0 +1,27 @@
+USE [ANTERO]
+GO
+/****** Object:  StoredProcedure [dw].[p_lataa_amos_tutkinnot_ja_tutkinnonosat_koko]    Script Date: 13.3.2023 16:26:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER PROCEDURE [dw].[p_lataa_amos_tutkinnot_ja_tutkinnonosat_koko] @minVuosi int = 2018, @maxVuosi int = null AS
+
+
+EXEC Koski_SA.dbo.p_lataa_tutkinnot_ja_tutkinnonosat_esivalmistelu @minVuosi,@maxVuosi
+
+EXEC Koski_SA.dbo.p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu
+EXEC Koski_SA.dbo.p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu_duplikaatit
+EXEC Koski_SA.dbo.p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu_tasot_yhdistetty
+EXEC Koski_SA.dbo.p_lataa_tutkinnot_ja_tutkinnonosat_valitaulu_tasot_yhdistetty_luvat
+
+EXEC dw.p_lataa_f_amos_tutkinnot_ja_tutkinnonosat
+
+EXEC dw.p_lataa_f_amos_tutkinnot_ja_tutkinnonosat_painotetut
+EXEC dw.p_lataa_f_amos_tutkinnot_ja_tutkinnonosat_painotetut_unpivot
+EXEC dw.p_lataa_f_amos_tutkinnot_ja_tutkinnonosat_painotetut_unpivot_generoi_hyvaksytty_korotustekija
+
