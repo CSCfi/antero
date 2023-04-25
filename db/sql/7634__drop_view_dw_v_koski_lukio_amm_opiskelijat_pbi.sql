e3ecd2e216e4bbb69e42582f079e0d70283a6cff
MarkusKyl <markus.kylaheiko@cgi.com>
2023-04-25 15:44:28 +0300

    CSCVIPUNEN-3136: Drop view

---
+++ db/sql/7634__drop_view_dw_v_koski_lukio_amm_opiskelijat_pbi.sql
@@ -0,0 +1,8 @@
+USE [ANTERO]
+GO
+
+/****** Object:  View [dw].[v_koski_lukio_amm_opiskelijat_pbi]    Script Date: 25.4.2023 15:40:09 ******/
+DROP VIEW IF EXISTS [dw].[v_koski_lukio_amm_opiskelijat_pbi]
+GO
+
+
