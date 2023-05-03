caeb8ce176212d36cd907045843fad486b7c39b4
akoskini <annamari.koskiniemi@csc.fi>
2023-05-03 13:37:21 +0300

    CSCVIPUNEN-3446: create and alter procs and views

---
+++ db/sql/7652__create_proc_p_lataa_f_varda_vakajarjestajat_toimipaikat.sql
@@ -0,0 +1,118 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_varda_vakajarjestajat_toimipaikat]    Script Date: 3.5.2023 12:25:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER PROCEDURE [dw].[p_lataa_f_varda_vakajarjestajat_toimipaikat]
+AS
+
+DECLARE @alkuVuosi as int
+
+SET @alkuVuosi = 2022 --2020
+
+TRUNCATE TABLE ANTERO.dw.f_varda_vakajarjestajat_toimipaikat
+
+WHILE @alkuVuosi <= YEAR(getdate())
+
+BEGIN
+	-- Faktataulun lataus
+
+	INSERT INTO ANTERO.dw.f_varda_vakajarjestajat_toimipaikat ( 
+		tilastovuosi,
+		[toimipaikka_id],
+		[vakajarjestaja_oid],
+		vakajarjestaja_id,
+        d_alueluokitus_vakajarjestaja_kunta_id,
+        [toimipaikka_oid],
+        [d_alueluokitus_toimipaikka_kunta_id],
+        [d_varda_toimipaikka_kasvatusopillinen_jarjestelma_id],
+        [d_varda_toimipaikka_toimintamuoto_id],
+		[d_varda_yritysluokitus_id],
+		d_varda_toimipaikkaluokitus_id,
+		d_varda_varhaiskasvatuspaikkaluokitus_id,
+		d_organisaatioluokitus_vakajarjestaja_id,
+        [varhaiskasvatuspaikat],
+		d_yritysmuoto_id,
+        [alkamis_pvm],
+        [paattymis_pvm],
+        [source]
+	)
+
+	SELECT 
+		CASE
+			WHEN s2.alkamis_pvm <= cast(concat(@alkuVuosi, '-12-31') as date) and s2.paattymis_pvm >= cast(concat(@alkuVuosi, '-12-31') as date) THEN @alkuVuosi
+			ELSE NULL 
+		END as tilastovuosi,
+	    s2.toimipaikka_id as toimipaikka_id,
+		s.organisaatio_oid as vakajarjestaja_oid,
+		s.vakajarjestaja_id as vakajarjestaja_id,
+        coalesce(d1.id, '-1') as d_alueluokitus_vakajarjestaja_kotikunta_id,
+        s2.organisaatio_oid as toimipaikka_oid,
+        coalesce(d2.id, '-1') as d_alueluokitus_toimipaikka_kunta_id,
+        coalesce(d3.id, '-1') as d_varda_toimipaikka_kasvatusopillinen_jarjestelma_id,
+        coalesce(d4.id, '-1') as d_varda_toimipaikka_toimintamuoto_id,
+        coalesce(d5.id, '-1') as d_varda_yritysluokitus_id,
+		coalesce(oa.id, '-1') as d_varda_toimipaikkaluokitus_id,
+		coalesce(d7.id, '-1') as d_varda_varhaiskasvatuspaikkaluokitus_id,
+		coalesce(d6.id, '-1') as d_organisaatioluokitus_vakajarjestaja_id,
+		s2.varhaiskasvatuspaikat,
+		   --pitää miettiä, mitä dimensiota käytetään ja tehdä tarvittaessa lataus, jossa myös ryhmittely. JS 12.5.2022
+		   /*Case WHEN yritysmuoto = 'OSAKEYHTIO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Osakeyhtiö')
+				 WHEN yritysmuoto = 'KOMMANDIITTIYHTIO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Kommandiittiyhtiö')
+				 WHEN yritysmuoto = 'SAATIO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Säätiö')
+				 WHEN yritysmuoto = 'PAIKALLISYHTEISO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Paikallisyhteisö')
+				 WHEN yritysmuoto = 'YKSITYINEN_ELINKEINONHARJOITTAJA' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Yksityinen elinkeinonharjoittaja')
+				 WHEN yritysmuoto = 'ORTODOKSINEN_KIRKKO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Ortodoksinen kirkko')
+				 WHEN yritysmuoto = 'OSUUSKUNTA' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Osuuskunta')
+				 WHEN yritysmuoto = 'USKONNOLLINEN_YHDYSKUNTA' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Uskonnollinen yhdyskunta')
+				 WHEN yritysmuoto = 'MUU_YHDISTYS' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Muu yhdistys')
+				 WHEN yritysmuoto = 'EI_YRITYSMUOTOA' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Ei yritysmuotoa')
+				 WHEN yritysmuoto = 'KUNTA' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Kunta')
+				 WHEN yritysmuoto = 'AVOIN_YHTIO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Avoin yhtiö')
+				 WHEN yritysmuoto = 'EV_LUT_KIRKKO' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Ev.lut.kirkko')
+				 WHEN yritysmuoto = 'AATTEELLINEN_YHDISTYS' THEN (select id from dw.d_yritysmuoto where nimi_fi = 'Aatteellinen yhdistys')
+				 ELSE*/ 
+				 --end 
+		-1 as d_yritysmuoto_id,
+		s2.alkamis_pvm,
+        s2.paattymis_pvm as paattymis_pvm,
+        'p_lataa_f_varda_vakajarjestajat_toimipaikat' as source
+	FROM varda_sa.sa.sa_varda_vakajarjestaja s
+	INNER JOIN varda_sa.sa.sa_varda_toimipaikka s2 on s2.vakajarjestaja_id = s.vakajarjestaja_id
+	LEFT JOIN dw.d_alueluokitus d1 on d1.kunta_koodi = s.kunta_koodi
+	LEFT JOIN dw.d_alueluokitus d2 on d2.kunta_koodi = s2.kunta_koodi
+	LEFT JOIN dw.d_varda_kasvatusopillinenjarjestelma d3 on d3.koodi = s2.kasvatusopillinen_jarjestelma_koodi
+	LEFT JOIN dw.d_varda_toimintamuoto d4 on d4.koodi = s2.toimintamuoto_koodi
+	INNER JOIN dw.d_varda_yritysluokitus d5 on d5.koodi = s.yritysmuoto --ei haluta mm. kuolinpesiä mukaan. AK 30.8.2022
+	LEFT JOIN dw.d_organisaatioluokitus d6 on d6.organisaatio_oid = s.organisaatio_oid
+	LEFT JOIN dw.d_varda_varhaiskasvatuspaikkaluokitus d7 on d7.koodi = s2.varhaiskasvatuspaikat
+	OUTER APPLY (
+		SELECT d0.id FROM (
+			SELECT 
+				s0.vakajarjestaja_id, 
+				COUNT(DISTINCT s02.organisaatio_oid) as lkm FROM varda_sa.sa.sa_varda_vakajarjestaja s0
+			INNER JOIN varda_sa.sa.sa_varda_toimipaikka s02 on s02.vakajarjestaja_id = s0.vakajarjestaja_id
+			WHERE s02.alkamis_pvm <= cast(concat(@alkuVuosi, '-12-31') as date) and s02.paattymis_pvm >= cast(concat(@alkuVuosi, '-12-31') as date)
+			GROUP BY s0.vakajarjestaja_id) lkm
+		LEFT JOIN [dw].[d_varda_toimipaikkaluokitus] d0 on d0.koodi = lkm.lkm
+		WHERE lkm.vakajarjestaja_id = s.vakajarjestaja_id
+	) oa
+	WHERE s2.alkamis_pvm <= cast(concat(@alkuVuosi, '-12-31') as date) and s2.paattymis_pvm >= cast(concat(@alkuVuosi, '-12-31') as date)
+		
+SET @alkuVuosi =  @alkuVuosi + 1
+
+END
+
+
+
+
+GO
+
+
---
+++ db/sql/7658__create_proc_p_lataa_f_varda_jarjestamismuoto.sql
@@ -0,0 +1,74 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_varda_jarjestamismuoto]    Script Date: 3.5.2023 12:22:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+CREATE OR ALTER PROCEDURE [dw].[p_lataa_f_varda_jarjestamismuoto]
+AS
+
+DECLARE @alkuVuosi as int
+
+SET @alkuVuosi = 2022 --2020
+
+TRUNCATE TABLE dw.f_varda_jarjestamismuoto
+
+WHILE @alkuVuosi <= YEAR(getdate())
+
+BEGIN
+
+	INSERT INTO dw.f_varda_jarjestamismuoto (
+		tilastovuosi,
+		d_varda_jarjestamismuoto_id_jm01,
+		d_varda_jarjestamismuoto_id_jm02,
+		d_varda_jarjestamismuoto_id_jm03,
+		d_varda_jarjestamismuoto_id_jm04,
+		d_varda_jarjestamismuoto_id_jm05,
+		alkamis_pvm,
+		paattymis_pvm,
+		toimipaikka_id,
+		tilastovuosi_toimipaikka_oid,
+		source
+	)
+
+	SELECT
+		@alkuVuosi as tilastovuosi,
+		d1a.id as d_varda_jarjestamismuoto_id_jm01,
+		d1b.id as d_varda_jarjestamismuoto_id_jm02,
+		d1c.id as d_varda_jarjestamismuoto_id_jm03,
+		d1d.id as d_varda_jarjestamismuoto_id_jm04,
+		d1e.id as d_varda_jarjestamismuoto_id_jm05,
+		s.alkamis_pvm,
+	    Case
+			WHEN s.paattymis_pvm = '' THEN '9999-01-01'
+			ELSE CAST(s.paattymis_pvm as date)
+		END as paattymis_pvm,
+		s.toimipaikka_id,
+		concat(@alkuVuosi, '_', s.organisaatio_oid) as tilastovuosi_toimipaikka_oid,
+		'p_lataa_f_varda_jarjestamismuoto' as source
+	  FROM varda_sa.sa.sa_varda_toimipaikka s
+	  LEFT JOIN dw.d_varda_jarjestamismuoto d1a on d1a.koodi = 'jm01' and lower(s.jarjestamismuoto_koodi) like '%jm01%'
+	  LEFT JOIN dw.d_varda_jarjestamismuoto d1b on d1b.koodi = 'jm02' and lower(s.jarjestamismuoto_koodi) like '%jm02%'
+	  LEFT JOIN dw.d_varda_jarjestamismuoto d1c on d1c.koodi = 'jm03' and lower(s.jarjestamismuoto_koodi) like '%jm03%'
+	  LEFT JOIN dw.d_varda_jarjestamismuoto d1d on d1d.koodi = 'jm04' and lower(s.jarjestamismuoto_koodi) like '%jm04%'
+	  LEFT JOIN dw.d_varda_jarjestamismuoto d1e on d1e.koodi = 'jm05' and lower(s.jarjestamismuoto_koodi) like '%jm05%'
+	  WHERE s.alkamis_pvm <= cast(concat(@alkuVuosi, '-12-31') as date) and (s.paattymis_pvm >= cast(concat(@alkuVuosi, '-12-31') as date) or s.paattymis_pvm = '')
+
+SET @alkuVuosi =  @alkuVuosi + 1
+
+END
+
+
+
+
+GO
+
+
