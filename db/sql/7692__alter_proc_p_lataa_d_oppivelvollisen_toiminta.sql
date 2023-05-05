12072ae359a93b87b86fb6bb73341afa40934075
vavainio <valtteri.vainio@csc.fi>
2023-05-05 08:11:09 +0300

    CSCVIPUNEN-3200 - Alter proc p_lataa_d_oppivelvollisen_toiminta

---
+++ db/sql/7692__alter_proc_p_lataa_d_oppivelvollisen_toiminta.sql
@@ -0,0 +1,88 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_d_oppivelvollisen_toiminta]    Script Date: 5.5.2023 7:59:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP VIEW IF EXISTS [dw].[v_koski_oppivelvolliset]
+
+GO
+
+ALTER PROCEDURE  [dw].[p_lataa_d_oppivelvollisen_toiminta] AS
+
+TRUNCATE TABLE dw.d_oppivelvollisen_toiminta
+
+IF NOT EXISTS (select * from ANTERO.dw.d_oppivelvollisen_toiminta where id=-1) 
+begin
+	set identity_insert dw.d_oppivelvollisen_toiminta on;
+	insert into dw.d_oppivelvollisen_toiminta (
+		id,
+		koodi,
+		selite_fi,
+		selite_sv,
+		selite_en,
+		selite2_fi,
+		selite2_sv,
+		selite2_en,
+		jarjestys,
+		jarjestys2,
+		source
+	)
+	select
+		-1,
+		koodi,
+		nimi,
+		nimi_sv,
+		nimi_en,
+		nimi,
+		nimi_sv,
+		nimi_en,
+		999,
+		999,
+		source
+	from sa.sa_koodistot
+	where koodisto='vipunenmeta'
+	and koodi='-1'
+end
+
+else begin
+	update d
+	set
+		koodi = s.koodi,
+		selite_fi = s.nimi,
+		selite_sv = s.nimi_sv,
+		selite_en = s.nimi_en,
+		selite2_fi = s.nimi,
+		selite2_sv = s.nimi_sv,
+		selite2_en = s.nimi_en,
+		d.source=s.source
+	from dw.d_oppivelvollisen_toiminta d
+	join sa.sa_koodistot s on s.koodi=d.koodi
+	where s.koodisto='vipunenmeta'
+	and s.koodi='-1'
+
+end
+set identity_insert dw.d_oppivelvollisen_toiminta off;
+
+INSERT dw.d_oppivelvollisen_toiminta (koodi, selite_fi, selite_sv, selite_en, selite2_fi, selite2_sv, selite2_en, jarjestys, jarjestys2, source)
+VALUES 
+('1','Oppivelvollisuus päättynyt','Läroplikten har upphört','Oppivelvollisuus päättynyt EN*','Suorittanut toisen asteen tutkinnon','Har avlagt examen på andra stadiet','Suorittanut toisen asteen tutkinnon EN*',1,1,'manuaalinen'),
+('2','Oppivelvollisuus päättynyt','Läroplikten har upphört','Oppivelvollisuus päättynyt EN*','Täyttänyt 18 vuotta','Fyllt 18 år','Täyttänyt 18 vuotta EN*',1,2,'manuaalinen'),
+('3','Suorittaa oppivelvollisuutta','Utför läroplikt','Suorittaa oppivelvollisuutta EN*','Tutkintoon johtavassa ammatillisessa koulutuksessa','I yrkesutbildning som leder till examen','Tutkintoon johtavassa ammatillisessa koulutuksessa EN*',2,3,'manuaalinen'),
+('4','Suorittaa oppivelvollisuutta','Utför läroplikt','Suorittaa oppivelvollisuutta EN*','Tutkintoon johtavassa lukiokoulutuksessa','I gymnasieutbildning som leder till examen','Tutkintoon johtavassa lukiokoulutuksessa EN*',2,4,'manuaalinen'),
+('5','Suorittaa oppivelvollisuutta','Utför läroplikt','Suorittaa oppivelvollisuutta EN*','Suorittaa perusopetuksen oppimäärää', 'Har tillfälligt avbrutit läroplikten','Suorittaa perusopetuksen oppimäärää EN*',2,5,'manuaalinen'),
+('6','Suorittaa oppivelvollisuutta','Utför läroplikt','Suorittaa oppivelvollisuutta EN*','Esiopetuksessa','I förskoleundervisning','Esiopetuksessa EN*',2,6,'manuaalinen'),
+('7','Suorittaa oppivelvollisuutta','Utför läroplikt','Suorittaa oppivelvollisuutta EN*','Tutkintoon johtamattomassa koulutuksessa (kelpaa oppivelvollisuuden suorittamiseen)','I utbildning som inte leder till examen (lämplig för fullgörande av läroplikten)','Tutkintoon johtamattomassa koulutuksessa (kelpaa oppivelvollisuuden suorittamiseen) EN*',2,7,'manuaalinen'),
+('8','Ei suorita oppivelvollisuutta','Fullgör inte läroplikt','Ei suorita oppivelvollisuutta EN*','Vapautettu oppivelvollisuudesta','Befriad från läroplikt','Vapautettu oppivelvollisuudesta EN*',3,8,'manuaalinen'),
+('9','Ei suorita oppivelvollisuutta','Fullgör inte läroplikt','Ei suorita oppivelvollisuutta EN*','Keskeyttänyt oppivelvollisuuden suorittamisen väliaikaisesti','Har tillfälligt avbrutit läroplikten','Keskeyttänyt oppivelvollisuuden suorittamisen väliaikaisesti EN*',3,9,'manuaalinen'),
+('10','Ei suorita oppivelvollisuutta','Fullgör inte läroplikt','Ei suorita oppivelvollisuuttaEN*','Tutkintoon johtamattomassa koulutuksessa (ei kelpaa oppivelvollisuuden suorittamiseen)','I utbildning som inte leder till examen (inte lämplig för fullgörande av läroplikten)','Tutkintoon johtamattomassa koulutuksessa (ei kelpaa oppivelvollisuuden suorittamiseen) EN*',3,10,'manuaalinen'),
+('11','Ei suorita oppivelvollisuutta','Fullgör inte läroplikt','Ei suorita oppivelvollisuutta EN*','Ei koulutuksessa','Deltar inte i utbildning','Ei koulutuksessa EN*',3,11,'manuaalinen')
+
+
+GO
+
+USE [ANTERO]
