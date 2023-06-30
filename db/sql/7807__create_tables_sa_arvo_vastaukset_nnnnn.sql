1f29b47669d7ee6f725b7272dbff4f861708544c
villehoo <ville.hamalainen@csc.fi>
2023-06-30 14:25:23 +0300

    CSCVIPUNEN-3430 fix addding new vastaus-tables

--- db/sql/7807__create_tables_sa_arvo_vastaukset_nnnnn.sql
+++ db/sql/7807__create_tables_sa_arvo_vastaukset_nnnnn.sql
@@ -1,16 +1,4 @@
-USE [ANTERO]
-GO
-
-/****** Object:  Table [dw].[f_koski_hoivaavustajat]    Script Date: 29.6.2023 21:59:42 ******/
-DROP TABLE [dw].[f_koski_hoivaavustajat]
-GO
-
-/****** Object:  Table [dw].[f_koski_hoivaavustajat]    Script Date: 29.6.2023 21:59:42 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
+USE ARVO_SA
 IF NOT EXISTS (
     SELECT *
     FROM INFORMATION_SCHEMA.COLUMNS
@@ -87,6 +75,7 @@ IF NOT EXISTS (
     WHERE TABLE_SCHEMA = 'sa' AND TABLE_NAME = 'sa_arvo_vastaukset_avop'
     )
 BEGIN
+
 	CREATE TABLE [sa].[sa_arvo_vastaukset_avop](
 		[id] [int] IDENTITY(1,1) NOT NULL,
 		[vastausid] [int] NULL,
@@ -112,6 +101,7 @@ BEGIN
 		[id] ASC
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 END
 
 GO
@@ -122,6 +112,7 @@ IF NOT EXISTS (
     WHERE TABLE_SCHEMA = 'sa' AND TABLE_NAME = 'sa_arvo_vastaukset_kandi'
     )
 BEGIN
+
 	CREATE TABLE [sa].[sa_arvo_vastaukset_kandi](
 		[id] [int] IDENTITY(1,1) NOT NULL,
 		[vastausid] [int] NULL,
@@ -147,6 +138,7 @@ BEGIN
 		[id] ASC
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 END
 
 GO
@@ -157,31 +149,33 @@ IF NOT EXISTS (
     WHERE TABLE_SCHEMA = 'sa' AND TABLE_NAME = 'sa_arvo_vastaukset_muut'
     )
 BEGIN
-	CREATE TABLE [sa].[sa_arvo_vastaukset_muut](
-		[id] [int] IDENTITY(1,1) NOT NULL,
-		[vastausid] [int] NULL,
-		[monivalintavaihtoehto_fi] [nvarchar](max) NULL,
-		[monivalintavaihtoehto_sv] [nvarchar](max) NULL,
-		[monivalintavaihtoehto_en] [nvarchar](max) NULL,
-		[vastaajaid] [int] NULL,
-		[kysymysid] [int] NULL,
-		[kyselykertaid] [int] NULL,
-		[koulutustoimija] [nvarchar](max) NULL,
-		[numerovalinta] [int] NULL,
-		[kyselyid] [int] NULL,
-		[vastaajatunnusid] [int] NULL,
-		[vapaateksti] [nvarchar](max) NULL,
-		[loadtime] [datetime] NULL,
-		[source] [varchar](255) NULL,
-		[username] [nvarchar](30) NULL,
-		[vaihtoehto] [varchar](100) NULL,
-		[vastausaika] [datetime] NULL,
-		[kysymysversio] [int] NULL,
-	 CONSTRAINT [PK__sa_arvo_vastaukset_muut] PRIMARY KEY CLUSTERED
-	(
-		[id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [sa].[sa_arvo_vastaukset_muut](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[vastausid] [int] NULL,
+	[monivalintavaihtoehto_fi] [nvarchar](max) NULL,
+	[monivalintavaihtoehto_sv] [nvarchar](max) NULL,
+	[monivalintavaihtoehto_en] [nvarchar](max) NULL,
+	[vastaajaid] [int] NULL,
+	[kysymysid] [int] NULL,
+	[kyselykertaid] [int] NULL,
+	[koulutustoimija] [nvarchar](max) NULL,
+	[numerovalinta] [int] NULL,
+	[kyselyid] [int] NULL,
+	[vastaajatunnusid] [int] NULL,
+	[vapaateksti] [nvarchar](max) NULL,
+	[loadtime] [datetime] NULL,
+	[source] [varchar](255) NULL,
+	[username] [nvarchar](30) NULL,
+	[vaihtoehto] [varchar](100) NULL,
+	[vastausaika] [datetime] NULL,
+	[kysymysversio] [int] NULL,
+ CONSTRAINT [PK__sa_arvo_vastaukset_muut] PRIMARY KEY CLUSTERED
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 END
 
@@ -193,32 +187,72 @@ IF NOT EXISTS (
     WHERE TABLE_SCHEMA = 'sa' AND TABLE_NAME = 'sa_arvo_vastaukset_tep'
     )
 BEGIN
-	CREATE TABLE [sa].[sa_arvo_vastaukset_tep](
-		[id] [int] IDENTITY(1,1) NOT NULL,
-		[vastausid] [int] NULL,
-		[monivalintavaihtoehto_fi] [nvarchar](max) NULL,
-		[monivalintavaihtoehto_sv] [nvarchar](max) NULL,
-		[monivalintavaihtoehto_en] [nvarchar](max) NULL,
-		[vastaajaid] [int] NULL,
-		[kysymysid] [int] NULL,
-		[kyselykertaid] [int] NULL,
-		[koulutustoimija] [nvarchar](max) NULL,
-		[numerovalinta] [int] NULL,
-		[kyselyid] [int] NULL,
-		[vastaajatunnusid] [int] NULL,
-		[vapaateksti] [nvarchar](max) NULL,
-		[loadtime] [datetime] NULL,
-		[source] [varchar](255) NULL,
-		[username] [nvarchar](30) NULL,
-		[vaihtoehto] [varchar](100) NULL,
-		[vastausaika] [datetime] NULL,
-		[kysymysversio] [int] NULL,
-	 CONSTRAINT [PK__sa_arvo_vastaukset_tep] PRIMARY KEY CLUSTERED
-	(
-		[id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [sa].[sa_arvo_vastaukset_tep](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[vastausid] [int] NULL,
+	[monivalintavaihtoehto_fi] [nvarchar](max) NULL,
+	[monivalintavaihtoehto_sv] [nvarchar](max) NULL,
+	[monivalintavaihtoehto_en] [nvarchar](max) NULL,
+	[vastaajaid] [int] NULL,
+	[kysymysid] [int] NULL,
+	[kyselykertaid] [int] NULL,
+	[koulutustoimija] [nvarchar](max) NULL,
+	[numerovalinta] [int] NULL,
+	[kyselyid] [int] NULL,
+	[vastaajatunnusid] [int] NULL,
+	[vapaateksti] [nvarchar](max) NULL,
+	[loadtime] [datetime] NULL,
+	[source] [varchar](255) NULL,
+	[username] [nvarchar](30) NULL,
+	[vaihtoehto] [varchar](100) NULL,
+	[vastausaika] [datetime] NULL,
+	[kysymysversio] [int] NULL,
+ CONSTRAINT [PK__sa_arvo_vastaukset_tep] PRIMARY KEY CLUSTERED
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 END
+
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_amis] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_amis__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_amis] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_amis__username]  DEFAULT (suser_name()) FOR [username]
+
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_amk_uraseuranta] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_amk_uraseuranta__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_amk_uraseuranta] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_amk_uraseuranta__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_avop] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_avop__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_avop] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_avop__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_kandi] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_kandi__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_kandi] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_kandi__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_muut] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_muut__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_muut] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_muut__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_tep] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_tep__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_arvo_vastaukset_tep] ADD  CONSTRAINT [DF__sa_arvo_vastaukset_tep__username]  DEFAULT (suser_name()) FOR [username]
+GO
 
 USE ANTERO
