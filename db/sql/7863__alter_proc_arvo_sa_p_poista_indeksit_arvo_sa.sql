a8ea0e6f81cd11bafe9a3aae59c8854920d5cd99
juhasyrj <juha.syrjala@csc.fi>
2023-07-12 18:16:19 +0300

    CSCVIPUNEN-3430: alter procs - indexes for new vastaukset-tables

---
+++ db/sql/7863__alter_proc_arvo_sa_p_poista_indeksit_arvo_sa.sql
@@ -0,0 +1,29 @@
+USE [Arvo_SA]
+GO
+
+/****** Object:  StoredProcedure [sa].[p_poista_indeksit_arvo_sa]    Script Date: 12.7.2023 18:12:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER PROCEDURE [sa].[p_poista_indeksit_arvo_sa] AS
+
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaajat] ON [sa].[sa_arvo_vastaajat]
+DROP INDEX IF EXISTS [NC__sa_arvo_kohteet] ON [sa].[sa_arvo_kohteet]
+DROP INDEX IF EXISTS [NC__sa_arvo_kohteet_taustatiedot] ON [sa].[sa_arvo_kohteet_taustatiedot]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset] ON [sa].[sa_arvo_vastaukset] --kaikki
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_amis] ON [sa].[sa_arvo_vastaukset_amis]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_amk_uraseuranta] ON [sa].[sa_arvo_vastaukset_amk_uraseuranta]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_avop] ON [sa].[sa_arvo_vastaukset_avop]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_kandi] ON [sa].[sa_arvo_vastaukset_kandi]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_muut] ON [sa].[sa_arvo_vastaukset_muut]
+DROP INDEX IF EXISTS [NC__sa_arvo_vastaukset_tep] ON [sa].[sa_arvo_vastaukset_tep]
+
+GO
+
+
