9a64174157233c703557179eebff4ec09d051091
vavainio <valtteri.vainio@csc.fi>
2023-08-25 16:00:00 +0300

    CSCVIPUNEN-3523 - Drop and create tables

---
+++ db/sql/7979__drop_create_table_f_eter_indicators.sql
@@ -0,0 +1,109 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[f_eter_indicators]    Script Date: 25.8.2023 15:35:51 ******/
+DROP TABLE [dw].[f_eter_indicators]
+GO
+
+/****** Object:  Table [dw].[f_eter_indicators]    Script Date: 25.8.2023 15:35:51 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[f_eter_indicators](
+	[ETER_ID] [varchar](255) NULL,
+	[Flag Share of women students ISCED 6] [varchar](255) NULL,
+	[Share of women students ISCED 6] [float] NULL,
+	[Flag Share of women students ISCED 5-7] [varchar](255) NULL,
+	[Flag Share of foreigners students ISCED 6] [varchar](255) NULL,
+	[Share of foreigners students ISCED 6] [float] NULL,
+	[Flag Share of foreigners students ISCED 5-7] [varchar](255) NULL,
+	[Share of mobile students ISCED 7] [float] NULL,
+	[Flag Share of mobile students ISCED 7] [varchar](255) NULL,
+	[Flag Herfindahl index students ISCED 5-7] [varchar](255) NULL,
+	[Share of women students ISCED 7] [float] NULL,
+	[Share of women students ISCED 5-7] [float] NULL,
+	[Share of foreigners students ISCED 7] [float] NULL,
+	[Share of foreigners students ISCED 5-7] [float] NULL,
+	[Flag Share of mobile students ISCED 6] [varchar](255) NULL,
+	[Share of mobile students ISCED 5-7] [float] NULL,
+	[Herfindahl index students ISCED 5-7] [float] NULL,
+	[Flag STEM orientation (students)] [varchar](255) NULL,
+	[Flag Share of women students ISCED 7] [varchar](255) NULL,
+	[Flag Share of foreigners students ISCED 7] [varchar](255) NULL,
+	[Share of mobile students ISCED 6] [float] NULL,
+	[Flag Share of mobile students ISCED 5-7] [varchar](255) NULL,
+	[STEM orientation (students)] [float] NULL,
+	[Share of women graduates ISCED 6] [float] NULL,
+	[Flag Share of women graduates ISCED 5-7] [varchar](255) NULL,
+	[Share of women graduates ISCED 5-7] [float] NULL,
+	[Share of foreigners graduates ISCED 6] [float] NULL,
+	[Flag Share of foreigners graduates ISCED 5-7] [varchar](255) NULL,
+	[Share of foreigners graduates ISCED 5-7] [float] NULL,
+	[Flag Share of mobile graduates ISCED 6] [varchar](255) NULL,
+	[Share of mobile graduates ISCED 7] [float] NULL,
+	[STEM orientation (graduates)] [varchar](255) NULL,
+	[Flag STEM orientation (graduates)] [varchar](255) NULL,
+	[Flag Share of women graduates ISCED 6] [varchar](255) NULL,
+	[Flag Share of women graduates ISCED 7] [varchar](255) NULL,
+	[Flag Share of foreigners graduates ISCED 6] [varchar](255) NULL,
+	[Flag Share of foreigners graduates ISCED 7] [varchar](255) NULL,
+	[Share of mobile graduates ISCED 6] [float] NULL,
+	[Flag Share of mobile graduates ISCED 7] [varchar](255) NULL,
+	[Flag Share of mobile graduates ISCED 5-7] [varchar](255) NULL,
+	[Master degree orientation] [varchar](255) NULL,
+	[Share of women graduates ISCED 7] [float] NULL,
+	[Share of foreigners graduates ISCED 7] [float] NULL,
+	[Share of mobile graduates ISCED 5-7] [float] NULL,
+	[Flag Master degree orientation] [varchar](255) NULL,
+	[Share of women academic staff] [float] NULL,
+	[Share of foreigners academic staff] [float] NULL,
+	[Flag Academic staff / total staff (FTE)] [varchar](255) NULL,
+	[Flag Share of women academic staff] [varchar](255) NULL,
+	[Flag Share of women full professors] [varchar](255) NULL,
+	[Flag Share of foreigners academic staff] [varchar](255) NULL,
+	[Flag Herfindahl index academic staff] [varchar](255) NULL,
+	[Full professors / academic staff (HC)] [float] NULL,
+	[Academic staff / total staff (FTE)] [float] NULL,
+	[Share of women full professors] [float] NULL,
+	[Herfindahl index academic staff] [float] NULL,
+	[Flag Full professors / academic staff (HC)] [varchar](255) NULL,
+	[Flag Share of women PhD graduates] [varchar](255) NULL,
+	[Flag Share of foreigners PhD graduates] [varchar](255) NULL,
+	[Share of mobile PhD students] [float] NULL,
+	[Flag Share of mobile PhD students] [varchar](255) NULL,
+	[Herfindahl index PhD graduates] [float] NULL,
+	[PhD intensity] [float] NULL,
+	[Flag PhD intensity] [varchar](255) NULL,
+	[Researchers mobility intensity] [float] NULL,
+	[Share of women PhD students] [float] NULL,
+	[Share of women PhD graduates] [float] NULL,
+	[Share of foreigners PhD students] [float] NULL,
+	[Share of foreigners PhD graduates] [float] NULL,
+	[Share of mobile PhD graduates] [float] NULL,
+	[Flag Herfindahl index PhD graduates] [varchar](255) NULL,
+	[Flag Researchers mobility intensity] [varchar](255) NULL,
+	[Flag Share of women PhD students] [varchar](255) NULL,
+	[Flag Share of foreigners PhD students] [varchar](255) NULL,
+	[Flag Share of mobile PhD graduates] [varchar](255) NULL,
+	[Total core budget / Total current revenues] [float] NULL,
+	[Student fees funding / Total current revenues] [float] NULL,
+	[Flag Student fees funding / Total current revenues] [varchar](255) NULL,
+	[Flag Total core budget / Total current revenues] [varchar](255) NULL,
+	[Flag Total third party funding / Total current revenues] [varchar](255) NULL,
+	[Total third party funding / Total current revenues] [float] NULL,
+	[Flag Erasmus credit mobility outgoing] [varchar](255) NULL,
+	[Erasmus credit mobility incoming] [float] NULL,
+	[Erasmus credit mobility outgoing] [float] NULL,
+	[Flag Erasmus credit mobility incoming] [varchar](255) NULL,
+	[Notes on indicators] [varchar](max) NULL,
+	[username] [varchar](100) NULL,
+	[source] [varchar](100) NULL,
+	[loadtime] [datetime] NULL
+) ON [PRIMARY]
+
+GO
+
+USE [ANTERO]
---
+++ db/sql/7980__drop_create_table_f_eter_revenue.sql
@@ -0,0 +1,74 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[f_eter_revenue]    Script Date: 25.8.2023 15:37:06 ******/
+DROP TABLE [dw].[f_eter_revenue]
+GO
+
+/****** Object:  Table [dw].[f_eter_revenue]    Script Date: 25.8.2023 15:37:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[f_eter_revenue](
+	[ETER_ID] [varchar](255) NULL,
+	[Basic government allocation (NC)] [float] NULL,
+	[Basic government allocation (PPP)] [float] NULL,
+	[Basic government allocation (EURO)] [float] NULL,
+	[Other core budget (EURO)] [float] NULL,
+	[Other core budget (NC)] [float] NULL,
+	[Other core budget (PPP)] [float] NULL,
+	[Total core budget (PPP)] [float] NULL,
+	[Total core budget (EURO)] [float] NULL,
+	[Total core budget (NC)] [float] NULL,
+	[Flag Public third party funding] [varchar](255) NULL,
+	[Flag other core budget] [varchar](255) NULL,
+	[Public third party funding (NC)] [float] NULL,
+	[Public third party funding (PPP)] [float] NULL,
+	[Public third party funding (EURO)] [float] NULL,
+	[Flag Total core budget] [varchar](255) NULL,
+	[Third party funding from abroad (NC)] [float] NULL,
+	[Third party funding from abroad (PPP)] [float] NULL,
+	[Third party funding from abroad (EURO)] [float] NULL,
+	[Third party funding undivided (EURO)] [float] NULL,
+	[Third party funding undivided (NC)] [float] NULL,
+	[Third party funding undivided (PPP)] [float] NULL,
+	[Flag Private third party funding] [varchar](255) NULL,
+	[Private third party funding (NC)] [float] NULL,
+	[Private third party funding (EURO)] [float] NULL,
+	[Private third party funding (PPP)] [float] NULL,
+	[Total third party funding (PPP)] [float] NULL,
+	[Total third party funding (EURO)] [float] NULL,
+	[Total third party funding (NC)] [float] NULL,
+	[Student fees funding (PPP)] [float] NULL,
+	[Student fees funding (EURO)] [float] NULL,
+	[Student fees funding (NC)] [float] NULL,
+	[Flag Third party funding undivided] [varchar](255) NULL,
+	[Flag Revenue unclassified] [varchar](255) NULL,
+	[Tuition fees] [float] NULL,
+	[Flag Total third party funding] [varchar](255) NULL,
+	[Revenue unclassified (NC)] [float] NULL,
+	[Revenue unclassified (PPP)] [float] NULL,
+	[Revenue unclassified (EURO)] [float] NULL,
+	[Non-recurring revenues (NC)] [float] NULL,
+	[Non-recurring revenues (PPP)] [float] NULL,
+	[Non-recurring revenues (EURO)] [float] NULL,
+	[Flag Student fees funding] [varchar](255) NULL,
+	[Total Current revenues (EURO)] [float] NULL,
+	[Total Current revenues (NC)] [float] NULL,
+	[Total Current revenues (PPP)] [float] NULL,
+	[Flag Total current revenues] [varchar](255) NULL,
+	[Notes on revenues] [varchar](max) NULL,
+	[Flag Basic government allocationt] [varchar](255) NULL,
+	[Flag Third party funding abroad] [varchar](255) NULL,
+	[Flag Non-recurring revenues] [varchar](255) NULL,
+	[username] [varchar](100) NULL,
+	[source] [varchar](100) NULL,
+	[loadtime] [datetime] NULL
+) ON [PRIMARY]
+
+GO
+
+USE [ANTERO]
