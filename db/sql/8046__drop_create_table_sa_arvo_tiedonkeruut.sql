d551972617a7e49c94212c8e5466a81dcbc73e66
villehoo <ville.hamalainen@csc.fi>
2023-09-11 12:24:36 +0300

    new arvo table and new columns

--- db/sql/8046__drop_create_table_sa_arvo_tiedonkeruut.sql
+++ db/sql/8046__drop_create_table_sa_arvo_tiedonkeruut.sql
@@ -12,22 +12,19 @@ GO
 
 
 CREATE TABLE [sa].[sa_arvo_tiedonkeruut](
-	(
-		[id] [bigint] IDENTITY(1,1) NOT NULL,
+	  [id] [bigint] IDENTITY(1,1) NOT NULL,
 	  tunniste VARCHAR(200) NULL,
 	  vuosi INT,
 	  tarkenne VARCHAR(200),
 	  kasittelyajan_paattymispvm DATETIME,
 	  loadtime DATETIME,
 	  source VARCHAR(300),
-	  username VARCHAR(150)
-	)
-	;
- CONSTRAINT [PK__sa_arvo_tiedonkeruut] PRIMARY KEY CLUSTERED
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	  username VARCHAR(150),
+	 CONSTRAINT [PK__sa_arvo_tiedonkeruut] PRIMARY KEY CLUSTERED
+	(
+	 [id] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
 
 GO
 ALTER TABLE [sa].[sa_arvo_tiedonkeruut] ADD  CONSTRAINT [DF__sa_arvo_tiedonkeruut__loadtime]  DEFAULT (getdate()) FOR [loadtime]
