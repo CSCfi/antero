330ec54090c8a77f677828343d0e613532a2e45a
juhasyrj <juha.syrjala@csc.fi>
2023-10-03 15:33:38 +0300

    CSCVIPUNEN-2605: alter, drop proc

---
+++ db/sql/8131__drop_proc_arvo_sa_p_poista_indeksit_arvo_sa.sql
@@ -0,0 +1,6 @@
+USE [Arvo_SA]
+GO
+
+/****** Object:  StoredProcedure [sa].[p_poista_indeksit_arvo_sa]    Script Date: 3.10.2023 13:24:41 ******/
+DROP PROCEDURE IF EXISTS [sa].[p_poista_indeksit_arvo_sa]
+GO
