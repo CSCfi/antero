c3b7424d6c1af66a64adb68240695bd261726ddc
juhasyrj <juha.syrjala@csc.fi>
2023-10-04 21:10:07 +0300

    CSCVIPUNEN-3606, 3607, 3451: alter proc (drop old), alter views

---
+++ db/sql/8159__drop_proc_koski_sa_p_lataa_esi_ja_perus_oppilaat_ja_paattaneet_valitaulu_jaadytys.sql
@@ -0,0 +1,12 @@
+USE [Koski_SA]
+GO
+
+/****** Object:  StoredProcedure [dbo].[p_lataa_esi_ja_perus_oppilaat_ja_paattaneet_valitaulu]    Script Date: 4.10.2023 20:36:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [dbo].[p_lataa_esi_ja_perus_oppilaat_ja_paattaneet_valitaulu_jaadytys]
+GO
