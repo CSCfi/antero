fb50d078278713a331ca806156f6267fbf3feadb
villehoo <ville.hamalainen@csc.fi>
2023-10-11 10:04:43 +0300

    CSCVIPUNEN-3612 add new columns

--- db/sql/8188__drop_create_table_arvo_sa_kysymykset.sql
+++ db/sql/8188__drop_create_table_arvo_sa_kysymykset.sql
@@ -19,33 +19,22 @@ CREATE TABLE [sa].[sa_arvo_kysymykset](
 	[taustakysymyksen_tyyppi] [nvarchar](max) NULL,
 	[vastaustyyppi] [nvarchar](max) NULL,
 	[valtakunnallinen] [bit] NULL,
-	[source] [varchar](100) NULL,
-	[username] [varchar](100) NULL,
-	[loadtime] [datetime] NULL,
 	[teema] [nvarchar](max) NULL,
 	[kysymysversio] [int] NULL,
 	[jatkokysymys_kysymysversio] [int] NULL,
+	[source] [varchar](100) NULL,
+	[username] [varchar](100) NULL,
+	[loadtime] [datetime] NULL,
  CONSTRAINT [PK__sa_arvo_kysymykset] PRIMARY KEY CLUSTERED
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-END
-GO
-IF NOT EXISTS
- (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[DF__sa_arvo_kysymykset__username]') AND type = 'D')
-BEGIN
+
+
 ALTER TABLE [sa].[sa_arvo_kysymykset] ADD  CONSTRAINT [DF__sa_arvo_kysymykset__username]  DEFAULT (suser_name()) FOR [username]
-END
 
-GO
-
-IF NOT EXISTS
- (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[DF__sa_arvo_kysymykset__loadtime]') AND type = 'D')
-BEGIN
 ALTER TABLE [sa].[sa_arvo_kysymykset] ADD  CONSTRAINT [DF__sa_arvo_kysymykset__loadtime]  DEFAULT (getdate()) FOR [loadtime]
-END
-GO
 
 
 USE [ANTERO]
