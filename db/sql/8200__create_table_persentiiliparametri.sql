6d630aeb94e01654f182f6735633bbdf047082d1
juhasyrj <juha.syrjala@csc.fi>
2023-10-13 09:56:26 +0300

    CSCVIPUNEN-3432: create table

---
+++ db/sql/8200__create_table_persentiiliparametri.sql
@@ -0,0 +1,215 @@
+USE [ANTERO]
+GO
+/****** Object:  Table [dbo].[persentiiliparametri]    Script Date: 13.10.2023 9:55:08 ******/
+DROP TABLE IF EXISTS [dbo].[persentiiliparametri]
+GO
+/****** Object:  Table [dbo].[persentiiliparametri]    Script Date: 13.10.2023 9:55:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[persentiiliparametri](
+	[n] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (1)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (2)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (3)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (4)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (5)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (6)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (7)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (8)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (9)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (10)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (11)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (12)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (13)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (14)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (15)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (16)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (17)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (18)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (19)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (20)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (21)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (22)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (23)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (24)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (25)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (26)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (27)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (28)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (29)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (30)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (31)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (32)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (33)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (34)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (35)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (36)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (37)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (38)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (39)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (40)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (41)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (42)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (43)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (44)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (45)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (46)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (47)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (48)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (49)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (50)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (51)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (52)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (53)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (54)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (55)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (56)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (57)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (58)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (59)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (60)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (61)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (62)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (63)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (64)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (65)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (66)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (67)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (68)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (69)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (70)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (71)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (72)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (73)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (74)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (75)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (76)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (77)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (78)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (79)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (80)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (81)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (82)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (83)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (84)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (85)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (86)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (87)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (88)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (89)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (90)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (91)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (92)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (93)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (94)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (95)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (96)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (97)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (98)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (99)
+GO
+INSERT [dbo].[persentiiliparametri] ([n]) VALUES (100)
+GO
