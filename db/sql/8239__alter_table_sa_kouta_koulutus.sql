054d0b638d49ded0724f7edf5bd77fec0eac2faa
villehoo <ville.hamalainen@csc.fi>
2023-10-27 13:54:28 +0300

    CSCVIPUNEN-3480 uusi sarake

---
+++ db/sql/8239__alter_table_sa_kouta_koulutus.sql
@@ -0,0 +1,12 @@
+USE ANTERO
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'sa'
+    AND TABLE_NAME = 'sa_kouta_koulutus'
+    AND COLUMN_NAME = 'opetuskieliKoodiUrit'
+)
+BEGIN
+    ALTER TABLE sa_kouta_koulutus
+    ADD [opetuskieliKoodiUrit] VARCHAR(250);
+END
