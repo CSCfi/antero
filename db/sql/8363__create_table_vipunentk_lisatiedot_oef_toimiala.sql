d6cdebc35c046794332b0ba01925f97c1ff24ab6
mrissane <mika.rissanen@csc.fi>
2023-12-12 15:15:04 +0200

    CSCVIPUNEN-3698

---
+++ db/sql/8363__create_table_vipunentk_lisatiedot_oef_toimiala.sql
@@ -0,0 +1,32 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  Table [dbo].[oef_toimiala]    Script Date: 12.12.2023 15:01:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE IF EXISTS [dbo].[oef_toimiala]
+GO
+
+CREATE TABLE [dbo].[oef_toimiala](
+	[id] [int] NOT NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[oef_toimiala_koodi] [nvarchar](10) NULL,
+	[oef_toimiala] [nvarchar](250) NULL,
+	[oef_toimiala_SV] [nvarchar](250) NULL,
+	[oef_toimiala_EN] [nvarchar](250) NULL,
+	[jarjestys_oef_toimiala] [nvarchar](50) NULL,
+	[virhetilanne] [nchar](1) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+
+GO
+
+
