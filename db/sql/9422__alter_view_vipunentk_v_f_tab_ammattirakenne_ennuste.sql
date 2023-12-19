2e241dfe39d0c9eeb24cce00c267d0177b35a326
vavainio <valtteri.vainio@csc.fi>
2023-12-19 15:33:19 +0200

    CSCVIPUNEN-3564 - Sarakemuokkauksia

---
+++ db/sql/9422__alter_view_vipunentk_v_f_tab_ammattirakenne_ennuste.sql
@@ -0,0 +1,28 @@
+USE [VipunenTK]
+GO
+
+/****** Object:  View [dbo].[v_f_tab_ammattirakenne_ennuste]    Script Date: 19.12.2023 15:31:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER   VIEW [dbo].[v_f_tab_ammattirakenne_ennuste] AS
+
+SELECT 
+	[Tilastovuosi] = [tilv],
+	[Ammattiryhmä 1. taso] = d1.oef_ammattiryhma1,
+	[Ammattiryhmä 2. taso] = d1.oef_ammattiryhma2,
+	[Toimiala] = d2.oef_toimiala,
+	[Toimiala_koodi] = d2.oef_toimiala_koodi,
+	[Ikä tilastovuonna] =f.ika,
+	lkm,
+	vieraskieliset_lkm
+FROM [VipunenTK].[dbo].[f_ammattirakenne_ennuste] f
+LEFT JOIN dbo.d_oef_ammatti d1 on d1.id=f.oef_ammatti_id
+LEFT JOIN dbo.d_oef_toimiala d2 on d2.id=f.oef_toimiala_id
+
+GO
+
+USE [ANTERO]
