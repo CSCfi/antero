dc9eda4448c2395208d4b05b66d996b6c9db51db
mrissane <mika.rissanen@csc.fi>
2024-02-09 14:10:55 +0200

    CSCVIPUNEN-3804

---
+++ db/sql/9585__alter_table_VipunenTK_d_tutkintoon_johtamaton_koulutus.sql
@@ -0,0 +1,12 @@
+Use VipunenTK
+
+ALTER TABLE [dbo].[d_tutkintoon_johtamaton_koulutus] ALTER COLUMN [tutkintoon_johtamaton_koulutus] [nvarchar](250) NULL
+GO
+
+ALTER TABLE [dbo].[d_tutkintoon_johtamaton_koulutus] ALTER COLUMN [tutkintoon_johtamaton_koulutus_SV] [nvarchar](250) NULL
+GO
+
+ALTER TABLE [dbo].[d_tutkintoon_johtamaton_koulutus] ALTER COLUMN [tutkintoon_johtamaton_koulutus_EN] [nvarchar](250) NULL
+GO
+
+
