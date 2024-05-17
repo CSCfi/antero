eaeb48787712d3c88d2794880c61f178769924aa
juhasyrj <juha.syrjala@csc.fi>
2024-05-17 13:41:20 +0300

    CSCVIPUNEN-2602: uusi dimensio, unpvt-vaihe ja kolmen muuttujat kuljetus SQL-latausten läpi

---
+++ db/sql/9862__drop_create_table_f_koski_lukio_opiskelijat_kuukausittain.sql
@@ -0,0 +1,68 @@
+USE [ANTERO]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[f_koski_lukio_opiskelijat_kuukausittain]') AND type in (N'U'))
+DROP TABLE [dw].[f_koski_lukio_opiskelijat_kuukausittain]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[f_koski_lukio_opiskelijat_kuukausittain](
+	[opiskeluoikeus_oid] [varchar](150) NULL,
+	[oppija_oid] [varchar](150) NULL,
+	[vuosi] [int] NULL,
+	[kuukausi] [int] NULL,
+	[lukukausi] [varchar](14) NULL,
+	[lukuvuosi] [varchar](25) NULL,
+	[aloitusvuosi] [int] NULL,
+	[aloituskuukausi] [int] NULL,
+	[aloitusajankohta] [varchar](33) NULL,
+	[aloitus_kuukauden_aikana_1_0] [int] NOT NULL,
+	[koulutustoimija_oid] [varchar](200) NULL,
+	[toimipiste_oid] [varchar](255) NULL,
+	[oppilaitos_oid] [varchar](255) NULL,
+	[suorituskieli_koodiarvo] [nvarchar](255) NULL,
+	[koulutusmuoto] [varchar](100) NULL,
+	[tavoitetutkinto] [nvarchar](200) NULL,
+	[koulutus] [nvarchar](200) NULL,
+	[majoitus] [int] NULL,
+	[sisaoppilaitosmainen_majoitus] [int] NULL,
+	[ika] [int] NULL,
+	[oppimaara] [varchar](38) NOT NULL,
+	[aidinkieli] [varchar](50) NULL,
+	[kansalaisuus] [varchar](3) NULL,
+	[syntymaaika] [nvarchar](30) NULL,
+	[Sukupuoli] [varchar](50) NULL,
+	[d_alueluokitus_kotikunta_id] [int] NULL,
+	[jarj_tavoitetutkinto] [int] NOT NULL,
+	[jarj_koulutus] [int] NOT NULL,
+	[uusi_opiskelija] [int] NOT NULL,
+	[yhdistelmatutkinto] [varchar](27) NOT NULL,
+	[jarj_yhdistelmatutkinto] [int] NOT NULL,
+	[eronnut] [int] NOT NULL,
+	[jatkaa_kj] [int] NULL,
+	[jatkaa_oppilaitos] [int] NULL,
+	[jatkaa_tutkintotyyppi] [int] NULL,
+	[jatkaa_ammatillinen] [int] NULL,
+	[jatkaa_yleissivistava] [int] NULL,
+	[jatkaa_valmentava] [int] NULL,
+	[jatkaa_lukio_valmentava] [int] NULL,
+	[jatkaa_amm_valmentava] [int] NULL,
+	[jatkaa_muu_ammatillinen] [int] NULL,
+	[jatkaa_amm_tutkintokoulutus] [int] NULL,
+	[jatkaa_oppimaara] [int] NULL,
+	[vaihto_oppimaara] [int] NULL,
+	[jatkaa_aikuistenops] [int] NULL,
+	[jatkaa_nuortenops] [int] NULL,
+	[jatkaa_lukioaine] [int] NULL,
+	[jatkaa_lukio_ulkomainen] [int] NULL,
+	[d_keskeyttaminen_vaiheittainen_id] [int] NULL,
+	d_keskeyttaminen_opiskelijan_toiminta_id [int] NULL,
+	[kotikunta_manner_suomessa] [int] NOT NULL,
+	[oppivelvollinen] [int] NOT NULL
+) ON [PRIMARY] 
+GO
---
+++ db/sql/9863__create_proc_koski_sa_p_lataa_toisen_asteen_keskeyttaneet_unpvt.sql
@@ -0,0 +1,119 @@
+USE [Koski_SA]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER PROCEDURE [dbo].[p_lataa_toisen_asteen_keskeyttaneet_unpvt] AS
+
+--Huom. d.jatkaa_toinen_aste_boo yhdessä komplementtinsa kanssa takaavat eronneen sisältyvän väh. yhteen kategoriaan. 
+--Huom. Sama tieto voi esiintyä eri avaimella ja selitteellä sektorista riippuen.
+
+DROP TABLE IF EXISTS [dbo].[toisen_asteen_keskeyttaneet_unpvt]
+
+SELECT 
+	[oppija_oid]
+    ,[opiskeluoikeus_oid]
+    ,[viimeinen_lasna]
+    ,[vuosi]
+    ,[kuukausi]
+
+	,keskeyttaminen_kategoria = kategoria
+
+INTO  [dbo].[toisen_asteen_keskeyttaneet_unpvt]
+
+FROM (
+	
+	SELECT 
+		[oppija_oid]
+		,[opiskeluoikeus_oid]
+		,[viimeinen_lasna]
+		,[vuosi]
+		,[kuukausi]
+
+		,d.jatkaa_tutkinto_boo
+		,jatkaa_nuortenops
+		,jatkaa_aikuistenops
+		,jatkaa_tutkintotyyppi
+		,d.jatkaa_koulutusala_taso3_boo
+		,d.jatkaa_koulutusala_taso2_boo
+		,d.jatkaa_koulutusala_taso1_boo
+		,jatkaa_muu_ammatillinen
+		,jatkaa_amm_valmentava
+		,jatkaa_lukio_valmentava
+		,jatkaa_valmentava
+		,jatkaa_amm_tutkintokoulutus
+		,jatkaa_ammatillinen
+		,jatkaa_yleissivistava
+		,d.jatkaa_toinen_aste_boo
+		,jatkaa_oppilaitos
+		,jatkaa_kj
+		,jatkaa_kj_siirtyi_amm = jatkaa_kj * jatkaa_ammatillinen
+		,keskeyttanyt_amm_tutkintokoulutus = 1 - jatkaa_amm_tutkintokoulutus
+		,keskeyttanyt_amm_koulutus = 1 - jatkaa_ammatillinen
+		,keskeyttanyt_toinen_aste = 1 - d.jatkaa_toinen_aste_boo
+		,vaihtoi_tutkinto_siirtyi_amm = (1 - d.jatkaa_tutkinto_boo) * jatkaa_amm_tutkintokoulutus
+		,vaihtoi_amm_tutkinto = jatkaa_amm_tutkintokoulutus * (1 - d.jatkaa_tutkinto_boo) --sisältötupla edellisen kanssa
+		,vaihtoi_amm_tutkintotyyppi = jatkaa_amm_tutkintokoulutus * (1 - jatkaa_tutkintotyyppi)
+		,vaihtoi_amm_koulutusala_taso_3 = jatkaa_amm_tutkintokoulutus * (1 - d.jatkaa_koulutusala_taso3_boo)
+		,vaihtoi_amm_koulutusala_taso_2 = jatkaa_amm_tutkintokoulutus * (1 - d.jatkaa_koulutusala_taso2_boo)
+		,vaihtoi_amm_koulutusala_taso_1 = jatkaa_amm_tutkintokoulutus * (1 - d.jatkaa_koulutusala_taso1_boo)
+		,vaihtoi_lukio = [jatkaa_yleissivistava] * (1 - [jatkaa_oppilaitos])
+		,vaihtoi_lukio_sama_kj = [jatkaa_yleissivistava] * (1 - [jatkaa_oppilaitos]) * [jatkaa_kj]
+		,vaihtoi_kj_siirtyi_amm = (1 - [jatkaa_kj]) * [jatkaa_ammatillinen]
+		,vaihtoi_kj_jatkaa_lukio = (1 - [jatkaa_kj]) * [jatkaa_yleissivistava]
+		,vaihtoi_kj_sama_amm_tutkinto = (1 - [jatkaa_kj]) * jatkaa_amm_tutkintokoulutus * d.jatkaa_tutkinto_boo
+		,vaihtoi_kj_eri_amm_tutkinto = (1 - [jatkaa_kj]) * jatkaa_amm_tutkintokoulutus * (1 - d.jatkaa_tutkinto_boo)
+		,vaihtoi_kj_sama_lukio_tutkinto = (1 - [jatkaa_kj]) * jatkaa_yleissivistava * d.jatkaa_tutkinto_boo
+
+	FROM [Koski_SA].[dbo].[toisen_asteen_keskeyttaneet] sa
+	LEFT JOIN ANTERO.dw.d_keskeyttaminen_vaiheittainen d ON d.keskeyttaminen_vaiheittainen_koodi = sa.keskeyttaminen_vaiheittainen_koodi
+
+) q
+
+UNPIVOT (
+	arvo FOR kategoria in (
+		jatkaa_tutkinto_boo
+		,[jatkaa_nuortenops]
+		,[jatkaa_aikuistenops]
+		,[jatkaa_tutkintotyyppi]
+		,jatkaa_koulutusala_taso3_boo
+		,jatkaa_koulutusala_taso2_boo
+		,jatkaa_koulutusala_taso1_boo
+		,[jatkaa_muu_ammatillinen]
+		,[jatkaa_amm_valmentava]
+		,[jatkaa_lukio_valmentava]
+		,[jatkaa_valmentava]
+		,[jatkaa_ammatillinen]
+		,[jatkaa_yleissivistava]
+		,jatkaa_toinen_aste_boo
+		,[jatkaa_oppilaitos]
+		,[jatkaa_kj]
+		,jatkaa_kj_siirtyi_amm
+		,keskeyttanyt_amm_tutkintokoulutus
+		,keskeyttanyt_amm_koulutus
+		,keskeyttanyt_toinen_aste
+		,vaihtoi_tutkinto_siirtyi_amm
+		,vaihtoi_amm_tutkinto
+		,vaihtoi_amm_tutkintotyyppi
+		,vaihtoi_amm_koulutusala_taso_3
+		,vaihtoi_amm_koulutusala_taso_2
+		,vaihtoi_amm_koulutusala_taso_1
+		,vaihtoi_lukio
+		,vaihtoi_lukio_sama_kj
+		,vaihtoi_kj_siirtyi_amm
+		,vaihtoi_kj_jatkaa_lukio
+		,vaihtoi_kj_sama_amm_tutkinto
+		,vaihtoi_kj_eri_amm_tutkinto
+		,vaihtoi_kj_sama_lukio_tutkinto
+	)
+) unpvt
+
+WHERE unpvt.arvo = 1
+
+GO
