ffe28c802ce3af1f794a794dfad141beaa8645b8
villehoo <ville.hamalainen@csc.fi>
2024-05-28 16:02:45 +0300

    CSCVIPUNEN-3861 New table

---
+++ db/sql/9889__create_table_sa_odw_arvosanatiedot.sql
@@ -0,0 +1,36 @@
+USE KOSKI_SA
+GO
+
+IF  EXISTS
+  (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[sa].[sa_odw_arvosanatiedot]' )
+    AND type in (N'U'))
+  DROP TABLE [sa].[sa_odw_arvosanatiedot]
+  GO
+
+CREATE TABLE sa.sa_odw_arvosanatiedot
+(
+  	[id] [int] IDENTITY(1,1) NOT NULL,
+  HenkiloOid VARCHAR(100)
+  , Valmistumispaivamaara DATETIME2
+  , Aine VARCHAR(100)
+  , Arvosana VARCHAR(20)
+  , Yksilollistaminen VARCHAR(15)
+  , luokkataso VARCHAR(15)
+  ,[loadtime] [datetime] NOT NULL
+	,[source] [varchar](100) NOT NULL
+	,[username] [varchar](30) NOT NULL
+  ,CONSTRAINT [PK_d_perhe] PRIMARY KEY CLUSTERED
+(
+ [id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+ALTER TABLE [sa].[sa_odw_arvosanatiedot] ADD  CONSTRAINT [DF__sa_odw_arvosanatiedot__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [sa].[sa_odw_arvosanatiedot] ADD  CONSTRAINT [DF__sa_odw_arvosanatiedot__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+USE ANTERO
