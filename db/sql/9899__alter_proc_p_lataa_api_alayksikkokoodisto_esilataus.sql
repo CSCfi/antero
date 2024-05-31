a8c51a0ab798004bf563718984f746e6143a26c9
villehoo <ville.hamalainen@csc.fi>
2024-05-31 15:59:59 +0300

    CSCVIPUNEN-3936 muutos api-esilataus-proseduuriin

---
+++ db/sql/9899__alter_proc_p_lataa_api_alayksikkokoodisto_esilataus.sql
@@ -0,0 +1,11 @@
+USE AnteroAPI
+GO
+
+IF NOT EXISTS (
+  SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'api' AND TABLE_NAME = 'api_alayksikkokoodisto' AND COLUMN_NAME = 'tiedejatutkimusfi_nakyvyyden_rajaus')
+BEGIN
+     ALTER TABLE [AnteroAPI].[api].[api_alayksikkokoodisto] ADD [tiedejatutkimusfi_nakyvyyden_rajaus] [NVARCHAR](1) NULL
+END
+
+USE ANTERO
