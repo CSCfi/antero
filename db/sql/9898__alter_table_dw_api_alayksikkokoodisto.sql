76c4e772fb5ad75617662e37a6c1e7d786284aec
villehoo <ville.hamalainen@csc.fi>
2024-05-31 15:54:31 +0300

    CSCVIPUNEN-3936 uusi sarake (fixed)

--- db/sql/9898__alter_table_dw_api_alayksikkokoodisto.sql
+++ db/sql/9898__alter_table_dw_api_alayksikkokoodisto.sql
@@ -5,7 +5,7 @@ IF NOT EXISTS (
   SELECT * FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'dw' AND TABLE_NAME = 'api_alayksikkokoodisto' AND COLUMN_NAME = 'tiedejatutkimusfi_nakyvyyden_rajaus')
 BEGIN
-     ALTER TABLE [ANTERO].[dw].[api_alayksikkokoodisto] ADD [tiedejatutkimusfi_nakyvyyden_rajaus] [NVARCHAR](1) NULL
+     ALTER TABLE [AnteroAPI].[dw].[api_alayksikkokoodisto] ADD [tiedejatutkimusfi_nakyvyyden_rajaus] [NVARCHAR](1) NULL
 END
 
 USE Antero
