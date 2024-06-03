a7ac30d3339a72c64adb2013b07fcf6e3cf5f51b
Jkoho <johanna.koho@csc.fi>
2024-06-03 08:29:11 +0300

    CSCVIPUNEN-3950: Käännösten päivitys tauluun

---
+++ db/sql/9900__update_table_d_vuosiluokka_tai_koulutus.sql
@@ -0,0 +1,7 @@
+USE [ANTERO]
+GO
+UPDATE [ANTERO].[dw].[d_vuosiluokka_tai_koulutus] SET 
+      [vuosiluokka_sv]='Undervisning som förbereder för den grundläggande utbildningen'
+	 ,[vuosiluokkaryhma_sv]='Undervisning som förbereder för den grundläggande utbildningen'
+      ,[koulutus_sv]='Undervisning som förbereder för den grundläggande utbildningen'
+	WHERE vuosiluokka_sv='Undervisning som förebereder för den grundläggande utbildningen'
