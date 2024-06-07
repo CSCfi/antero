a119241c20e0999b19f11f4c7f474b43b9324598
akoskini <annamari.koskiniemi@csc.fi>
2024-06-07 12:20:08 +0300

    CSCVIPUNEN-3890: create or alter tables

---
+++ db/sql/9909__create_table_antero_d_perhe.sql
@@ -0,0 +1,39 @@
+USE [ANTERO]
+GO
+
+/****** Object:  Table [dw].[d_perhe]    Script Date: 22.5.2024 14:28:21 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[d_perhe]') AND type in (N'U'))
+DROP TABLE [dw].[d_perhe]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dw].[d_perhe](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[perhe_koodi] [varchar](10) NOT NULL,
+	[perhe_fi] [varchar](100) NOT NULL,
+	[perhe_sv] [varchar](100) NOT NULL,
+	[perhe_en] [varchar](100) NOT NULL,
+	[jarjestys_perhe] [int] NOT NULL,
+	[loadtime] [datetime] NOT NULL,
+	[source] [varchar](100) NOT NULL,
+	[username] [varchar](30) NOT NULL,
+	[kommentti] [varchar](100) NULL,
+ CONSTRAINT [PK_d_perhe] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dw].[d_perhe] ADD  CONSTRAINT [DF__d_perhe__loadtime]  DEFAULT (getdate()) FOR [loadtime]
+GO
+
+ALTER TABLE [dw].[d_perhe] ADD  CONSTRAINT [DF__d_perhe__username]  DEFAULT (suser_name()) FOR [username]
+GO
+
+
