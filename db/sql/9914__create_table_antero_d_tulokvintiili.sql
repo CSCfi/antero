5e0306c7d301c0783d85dfb65117e56cbd3c6b59
akoskini <annamari.koskiniemi@csc.fi>
2024-06-07 14:22:08 +0300

    CSCVIPUNEN-3890: drop create dimensiotaulut

--- db/sql/9914__create_table_antero_d_tulokvintiili.sql
+++ db/sql/9914__create_table_antero_d_tulokvintiili.sql
@@ -1,18 +1,15 @@
 USE [ANTERO]
 GO
 
-ALTER TABLE [dw].[d_tulokvintiili] DROP CONSTRAINT [DF__d_tulokvintiili__username]
-GO
-
 ALTER TABLE [dw].[d_tulokvintiili] DROP CONSTRAINT [DF__d_tulokvintiili__loadtime]
 GO
 
-/****** Object:  Table [dw].[d_tulokvintiili]    Script Date: 7.6.2024 13:13:42 ******/
+/****** Object:  Table [dw].[d_tulokvintiili]    Script Date: 7.6.2024 14:20:20 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dw].[d_tulokvintiili]') AND type in (N'U'))
 DROP TABLE [dw].[d_tulokvintiili]
 GO
 
-/****** Object:  Table [dw].[d_tulokvintiili]    Script Date: 7.6.2024 13:13:42 ******/
+/****** Object:  Table [dw].[d_tulokvintiili]    Script Date: 7.6.2024 14:20:20 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -38,9 +35,6 @@ CREATE TABLE [dw].[d_tulokvintiili](
 GO
 
 ALTER TABLE [dw].[d_tulokvintiili] ADD  CONSTRAINT [DF__d_tulokvintiili__loadtime]  DEFAULT (getdate()) FOR [loadtime]
-GO
-
-ALTER TABLE [dw].[d_tulokvintiili] ADD  CONSTRAINT [DF__d_tulokvintiili__username]  DEFAULT (suser_name()) FOR [username]
 GO
 
 
