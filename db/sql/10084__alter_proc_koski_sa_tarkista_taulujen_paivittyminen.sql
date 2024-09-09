798a43ad72a39c6373c679aadcc21ba0b26da107
juhasyrj <juha.syrjala@csc.fi>
2024-09-09 13:38:40 +0300

    CSCVIPUNEN-3992: muokataan tarkistusproseduuria täysin tyhjien taulujen varalta

---
+++ db/sql/10084__alter_proc_koski_sa_tarkista_taulujen_paivittyminen.sql
@@ -0,0 +1,45 @@
+USE [Koski_SA]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [sa].[tarkista_taulujen_paivittyminen] AS
+
+DECLARE @pvmVahintaan date = DATEADD(day, -7, getdate())
+
+DECLARE @maxMuutosPvm date = 
+(
+	SELECT max(oo.aikaleima)
+	FROM [sa].[sa_koski_opiskeluoikeus] oo 
+	JOIN [sa].[sa_koski_opiskeluoikeus_aikajakso] ooa ON ooa.opiskeluoikeus_oid = oo.opiskeluoikeus_oid 
+	JOIN [sa].[sa_koski_henkilo] h ON h.oppija_oid = oo.oppija_oid
+)
+DECLARE @maxLatausPvm1 date = (SELECT max(imp_created) FROM [sa].[sa_koski_henkilo])
+DECLARE @maxLatausPvm2 date = (SELECT max(imp_created) FROM [sa].[sa_koski_opiskeluoikeus])
+DECLARE @maxLatausPvm3 date = (SELECT max(imp_created) FROM [sa].[sa_koski_opiskeluoikeus_aikajakso])
+DECLARE @maxLatausPvm4 date = (SELECT max(imp_created) FROM [sa].[sa_koski_paatason_suoritus])
+DECLARE @maxLatausPvm5 date = (SELECT max(imp_created) FROM [sa].[sa_koski_osasuoritus])
+
+DECLARE @minLatausPvm date = 
+(
+	SELECT min(pvm) 
+	FROM (
+		SELECT coalesce(@maxMuutosPvm, '2000-01-01') pvm
+		UNION SELECT coalesce(@maxLatausPvm1, '2000-01-01')
+		UNION SELECT coalesce(@maxLatausPvm2, '2000-01-01')
+		UNION SELECT coalesce(@maxLatausPvm3, '2000-01-01')
+		UNION SELECT coalesce(@maxLatausPvm4, '2000-01-01')
+		UNION SELECT coalesce(@maxLatausPvm5, '2000-01-01')
+	) q
+)
+
+IF (@minLatausPvm < @pvmVahintaan)
+RAISERROR (N'Jokin sa-päätauluista tai sen sisältö ei ole päivittynyt viikkoon, tarkista ETL.', 11, 1)
+
+GO
