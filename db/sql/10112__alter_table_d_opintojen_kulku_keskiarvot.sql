daedc95edba094041fd38d6655c22ba36c85a4fa
juhasyrj <juha.syrjala@csc.fi>
2024-09-17 14:30:47 +0300

    uusi yritys, update erikseen

--- db/sql/10112__alter_table_d_opintojen_kulku_keskiarvot.sql
+++ db/sql/10112__alter_table_d_opintojen_kulku_keskiarvot.sql
@@ -34,16 +34,3 @@ BEGIN
 		,[selite2_en] varchar(20)
 		,[jarj_keskiarvo2] int
 END
-
-IF NOT EXISTS (
-	SELECT 1
-	FROM [dw].[d_opintojen_kulku_keskiarvot]
-	WHERE tyyppi = 'keskiarvo'
-	AND nullif([selite2_fi], '') is not null
-)
-BEGIN
-	UPDATE [dw].[d_opintojen_kulku_keskiarvot]
-	SET [selite2_fi] = concat(replace(left(selite_fi, 3), ',5', ',0'), ' - ', cast(left(selite_fi, 1) as int) + 1, case when left(selite_fi, 1) = '9' then '' else ',0' end)
-		,[jarj_keskiarvo2] = 10 - left(selite_fi, 1)
-	WHERE tyyppi = 'keskiarvo'
-END
