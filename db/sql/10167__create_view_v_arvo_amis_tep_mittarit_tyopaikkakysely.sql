c61e23cb11ac031c911663d1d741c78b71cf194d
juhasyrj <juha.syrjala@csc.fi>
2024-09-27 17:20:11 +0300

    CSCVIPUNEN-4049: luodaan SQL-proseduurit mittarijoukkojen aputauluille

---
+++ db/sql/10167__create_view_v_arvo_amis_tep_mittarit_tyopaikkakysely.sql
@@ -0,0 +1,42 @@
+USE [ANTERO]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER VIEW [dw].[v_arvo_amis_tep_mittarit_tyopaikkakysely] AS 
+
+SELECT DISTINCT
+	[koodi]
+	,[koodi_piilosuodatin] = koodi
+    ,[selite_fi]
+    ,[selite_sv]
+    ,[selite_en]
+    ,[jarjestys]
+
+FROM [dbo].[translations]
+
+WHERE 1=1
+AND tyyppi = 'mittari'
+AND koodi IN (
+	'kyselyn_kohteet_tyopaikkojen_lkm',
+	'kyselyn_kohteet_tyopaikkajaksojen_lkm',
+	'kyselyn_kohteet_tyopaikkajaksojen_paivien_lkm',
+	'vastattu_tyopaikkojen_lkm',
+	'vastattu_tyopaikkajaksojen_lkm',
+	'vastattu_tyopaikkajaksojen_paivien_lkm',
+	'vastausosuus_tyopaikkojen_lkm',
+	'vastausosuus_tyopaikkajaksojen_lkm',
+	'vastausosuus_tyopaikkajaksojen_paivien_lkm',
+	'kyselyn_kohteiden_keskimaarainen_kesto',
+	'vastattujen_tyopaikkajaksojen_keskimaarainen_kesto',
+	'keskiarvo_painotettu_tyopaikkajaksojen_kestolla',
+	'keskihajonta_painotettu_tyopaikkajaksojen_kestolla'
+)
+
+GO
