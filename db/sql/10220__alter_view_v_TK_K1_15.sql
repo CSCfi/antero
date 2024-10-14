bb126722b5f29a4d534452f49e58ac927e4d1501
juhasyrj <juha.syrjala@csc.fi>
2024-10-14 11:51:09 +0300

    CSCVIPUNEN-4104: lisätään sa-näkymään uusi vuosi

---
+++ db/sql/10220__alter_view_v_TK_K1_15.sql
@@ -0,0 +1,67 @@
+USE [ANTERO]
+GO
+
+/****** Object:  View [dw].[v_TK_K1_15]    Script Date: 11.10.2024 10:06:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER VIEW [dw].[v_TK_K1_15] AS 
+
+--huom. kala_t2 nimivastineet noudettu dimensiosta, koska ovat aineistossa englanniksi. JS 24.6.2024
+
+SELECT
+	[tilv]
+	,[ikar8]
+	,[jarj]
+	,[jarjnim]
+	,[tutktyyp]
+	,[koulk] = nullif([koulk], 'NULL')
+	,[alvv]
+	,[rahoitus]
+	,[pohjakoulutus]
+	,[kaste_t2_e]
+	,[kaste_t2ni_e] = coalesce(nullif([kaste_t2ni_e], '9999'), 'Ei perusasteen jälkeistä koulutusta tai tutkinto tuntematon')
+	,[kala_t2_e]
+	,[kala_t2ni_e] = d.iscfinarrow2013
+	,[pohjakoulutus_ulkom]
+	,[kaste_t2_e_ulkom]
+	,[kaste_t2ni_e_ulkom] = coalesce(nullif([kaste_t2ni_e_ulkom], '9999'), 'Ei perusasteen jälkeistä koulutusta tai tutkinto tuntematon')
+	,[kala_t2_e_ulkom]
+	,[kala_t2ni_e_ulkom] = db.iscfinarrow2013
+	,[opiskelijavuodet]
+FROM [TK_H9098_CSC].[dbo].[TK_K1_15_sopv_24_2023] t
+LEFT JOIN VipunenTK.dbo.d_koulutusluokitus d ON d.koulutusluokitus_avain = 'ISCFINARROW' + [kala_t2_e]
+LEFT JOIN VipunenTK.dbo.d_koulutusluokitus db ON db.koulutusluokitus_avain = 'ISCFINARROW' + [kala_t2_e_ulkom]
+
+UNION ALL
+
+SELECT
+	[tilv]
+	,[ikar8]
+	,[jarj]
+	,[jarjnim]
+	,[tutktyyp]
+	,[koulk] = nullif([koulk], 'NULL')
+	,[alvv]
+	,[rahoitus]
+	,[pohjakoulutus]
+	,[kaste_t2_e]
+	,[kaste_t2ni_e] = coalesce(nullif([kaste_t2ni_e], '9999'), 'Ei perusasteen jälkeistä koulutusta tai tutkinto tuntematon')
+	,[kala_t2_e]
+	,[kala_t2ni_e] = d.iscfinarrow2013
+	,[pohjakoulutus_ulkom]
+	,[kaste_t2_e_ulkom]
+	,[kaste_t2ni_e_ulkom] = coalesce(nullif([kaste_t2ni_e_ulkom], '9999'), 'Ei perusasteen jälkeistä koulutusta tai tutkinto tuntematon')
+	,[kala_t2_e_ulkom]
+	,[kala_t2ni_e_ulkom] = db.iscfinarrow2013
+	,[opiskelijavuodet]
+FROM [TK_H9098_CSC].[dbo].[TK_K1_15_sopv_24_2022] t
+LEFT JOIN VipunenTK.dbo.d_koulutusluokitus d ON d.koulutusluokitus_avain = 'ISCFINARROW' + [kala_t2_e]
+LEFT JOIN VipunenTK.dbo.d_koulutusluokitus db ON db.koulutusluokitus_avain = 'ISCFINARROW' + [kala_t2_e_ulkom]
+
+GO
