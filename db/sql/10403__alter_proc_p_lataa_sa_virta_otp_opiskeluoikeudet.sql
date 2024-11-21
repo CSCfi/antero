bb55e6d1c163ea605219a4ffc5f8ccf3cc6d584c
vavainio <valtteri.vainio@csc.fi>
2024-11-21 08:46:40 +0200

    CSCVIPUNEN-4150 - Alter proc p_lataa_sa_virta_otp_opiskeluoikeudet

---
+++ db/sql/10403__alter_proc_p_lataa_sa_virta_otp_opiskeluoikeudet.sql
@@ -0,0 +1,45 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [sa].[p_lataa_sa_virta_otp_opiskeluoikeudet]    Script Date: 21.11.2024 8:45:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+ALTER   PROCEDURE [sa].[p_lataa_sa_virta_otp_opiskeluoikeudet] AS
+
+TRUNCATE TABLE [ANTERO].[sa].[sa_virta_otp_opiskeluoikeudet]
+
+INSERT INTO [ANTERO].[sa].[sa_virta_otp_opiskeluoikeudet]
+SELECT [Tilastovuosi]
+      ,[Opiskeluoikeuksien lukumäärä]
+      ,[Ensimmäinen korkeakoulu]
+      ,[Viimeinen korkeakoulu]
+      ,[Korkeakoulujen lukumäärä]
+      ,[Yliopistoja]
+      ,[AMKeja]
+      ,[YO+AMK]
+      ,[Useampi opiskeluoikeus samalta alalta]
+      ,[Useampi opiskeluoikeus samasta yliopistosta]
+      ,[Useampi opiskeluoikeus samasta AMKista]
+      ,[Vuosia ensimmäisestä opiskeluoikeudesta]
+      ,[Vuosia viimeisestä opiskeluoikeudesta]
+      ,[Lkm]
+      ,[1. op.oikeuden koulutuskoodi]
+      ,[Viim. op.oikeuden koulutuskoodi]
+	  ,[eka_op]
+      ,[muut_op]
+	  ,[uusi_oo_edell_lv]
+      ,[syntymavuosi]
+      ,getdate() as loadtime
+FROM [VirtaSiirto].[sa].[sa_virta_otp_opiskeluoikeudet]
+
+GO
+
+
