02b0ebb9bbd6f5f99f2340b90ec7ee8a0d1b03b3
akoskini <annamari.koskiniemi@csc.fi>
2024-12-03 12:35:23 +0200

    CSCVIPUNEN-4081: alter view, added fields.

---
+++ db/sql/10422__alter_view_v_lapset_ja_vakaan_osallistuneet_2_26.sql
@@ -0,0 +1,94 @@
+USE [ANTERO]
+GO
+
+/****** Object:  View [dw].[v_lapset_ja_vakaan_osallistuneet_2_26]    Script Date: 3.12.2024 12:17:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE OR ALTER     VIEW [dw].[v_lapset_ja_vakaan_osallistuneet_2_26] AS
+
+SELECT 
+
+-- Laskentamuuttujat
+
+	 [Tilastovuosi]						= v.tilv
+	,[id]								= v.rivinumero
+	,[Maakunta]							= d1.maakunta_fi
+	,[Ikä]								= d2.ika_fi
+	,case when v.ika < 1 then 'alle 1-vuotiaat'
+		when v.ika >= 1 and v.ika <= 2 then '1-2-vuotiaat'
+		when v.ika > 2 and v.ika <= 5 then '3-5-vuotiaat'
+		when v.ika = 6 then '6-vuotiaat' 
+		else 'Tieto puuttuu'
+		end as [Ikäryhmä]
+	,[Sukupuoli]						= d5.sukupuoli_fi
+	,[Syntyperä]						= d6.syntypera
+	,[Osallistunut varhaiskasvatukseen] = d3.kytkin_fi
+	,[Kuulunut väestöön 31.12.]			= d4.kytkin_fi
+	,[Tuloluokka]						= d7.tulokvintiili_fi
+	,[Perhe]							= d8.perhe_fi
+	,case when v.pekoko = 'P' then ' ' 
+		else v.pekoko
+	 end as [Perheen koko]		
+	,case when v.pekoko = 'P' then ' ' 
+		when v.pekoko > 6 then '7 ja yli'
+		else v.pekoko
+	 end as [Perheen koko (suppea)]
+
+-- apumuuttujat
+	,[Apusarake Lukumäärä]				= v.lkm 
+
+-- Koodimuuttujat
+
+	,[Koodit Maakunta]					= d1.maakunta_koodi 
+
+-- Järjestysmuuttujat
+
+	,[Järjestys Osallistunut varhaiskasvatukseen]	= d3.jarjestys_kytkin_koodi
+	,[Järjestys Kuulunut väestöön 31.12]			= d4.jarjestys_kytkin_koodi
+	,[Järjestys Syntyperä]				= d6.jarjestys
+	,case when v.perhe = 'P' then 99 else d8.jarjestys_perhe 
+	 end as [Järjestys Perhe]			
+							
+
+FROM [VipunenTK_SA].[dbo].[v_sa_2_26_0-6v_ja_vakaan_osallistuneet_2021_2022] v
+	OUTER APPLY (select top 1 * from dw.d_alueluokitus d1 where d1.maakunta_koodi = v.maakunta) d1
+	LEFT JOIN [ANTERO].[dw].[d_ika] d2
+	ON d2.id = v.ika
+	LEFT JOIN [ANTERO].[dw].[d_kytkin] d3
+	ON d3.id = v.vakaos
+	LEFT JOIN [ANTERO].[dw].[d_kytkin] d4
+	ON d4.id = v.vaesto
+	LEFT JOIN [ANTERO].[dw].[d_sukupuoli] d5
+	ON d5.id = v.sp
+	LEFT JOIN [VipunenTK].[dbo].[d_syntypera] d6
+	ON d6.syntypera_koodi = v.syntyp2
+	LEFT JOIN [ANTERO].[dw].[d_tulokvintiili] d7
+	ON d7.tulokvintiili_koodi = v.tuloluokka
+	LEFT JOIN [ANTERO].[dw].[d_perhe] d8
+	ON d8.perhe_koodi = v.perhe
+
+
+
+
+GO
+
+
