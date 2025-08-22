0334f105a5d379fdbbbc04e95b791824a7ed7578
juhasyrj <juha.syrjala@csc.fi>
2025-08-22 21:37:32 +0300

    CSCVIPUNEN-4199: lisätään kalenteriin päivämäärän sisältävät rahoituskaudet

---
+++ db/sql/11050__alter_table_d_kalenteri.sql
@@ -0,0 +1,27 @@
+use [ANTERO]
+go 
+
+IF NOT EXISTS (
+  SELECT * 
+  FROM sys.columns 
+  WHERE object_id = OBJECT_ID(N'[dw].[d_kalenteri]') 
+  AND name = 'rahoituskausi_amm_1'
+)
+BEGIN
+	alter table [dw].[d_kalenteri]
+	add [rahoituskausi_amm_1] nvarchar(50)
+END
+;
+
+IF NOT EXISTS (
+  SELECT * 
+  FROM sys.columns 
+  WHERE object_id = OBJECT_ID(N'[dw].[d_kalenteri]') 
+  AND name = 'rahoituskausi_amm_2'
+)
+BEGIN
+	alter table [dw].[d_kalenteri]
+	add [rahoituskausi_amm_2] nvarchar(50)
+END
+
+GO
