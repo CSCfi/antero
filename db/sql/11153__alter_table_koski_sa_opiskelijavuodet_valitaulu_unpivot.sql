6affaf47c277fce41ef2568849e36532a3f16293
juhasyrj <juha.syrjala@csc.fi>
2025-09-08 12:11:53 +0300

    CSCVIPUNEN-4467: sallitaan työvoimakoulutuksen null-arvo (puuttuva rahoituskoodi)

---
+++ db/sql/11153__alter_table_koski_sa_opiskelijavuodet_valitaulu_unpivot.sql
@@ -0,0 +1,6 @@
+use Koski_SA
+go
+
+alter table [dbo].[opiskelijavuodet_valitaulu_unpivot]
+alter column [tyovoimakoulutus] int null
+;
---
+++ db/sql/11154__alter_table_koski_sa_opiskelijavuodet_valitaulu_unpivot_luvat.sql
@@ -0,0 +1,6 @@
+use Koski_SA
+go
+
+alter table [dbo].[opiskelijavuodet_valitaulu_unpivot_luvat]
+alter column [tyovoimakoulutus] int null
+;
