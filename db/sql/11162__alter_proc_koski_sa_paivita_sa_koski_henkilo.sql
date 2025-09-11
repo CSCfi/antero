8a5ee81131574eb851145658448c33e60dab2e9e
juhasyrj <juha.syrjala@csc.fi>
2025-09-11 18:29:24 +0300

    CSCVIPUNEN-4290: transportoidaan 0-tuhatluvulla syntyneet yleisimpien aikafunktioiden toiminta-alueelle 1900-luvun alkuun

---
+++ db/sql/11162__alter_proc_koski_sa_paivita_sa_koski_henkilo.sql
@@ -0,0 +1,24 @@
+USE [Koski_SA]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [sa].[paivita_sa_koski_henkilo] AS
+
+update sa.sa_koski_henkilo
+set kansalaisuus = concat('246,', replace(kansalaisuus, ',246', ''))
+where kansalaisuus like '%,246%'
+;
+
+update sa.sa_koski_henkilo
+set syntymaaika = '1900-01-01'
+where syntymaaika < '1000-01-01'
+;
+
+GO
