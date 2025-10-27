508babe79333318e32ae8dc982c0c5b98051619c
juhasyrj <juha.syrjala@csc.fi>
2025-10-27 15:15:33 +0200

    CSCVIPUNEN-4642: populoidaan uusi sarake

--- db/sql/11311__alter_table_sa_amos_siirtotiedosto_okm8.sql
+++ db/sql/11311__alter_table_sa_amos_siirtotiedosto_okm8.sql
@@ -10,15 +10,3 @@ IF NOT EXISTS (
 alter table [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8]
 add [Toiminnanohjauksen kokeilu] nvarchar(50) 
 go
-
-update t 
-set [Toiminnanohjauksen kokeilu] = oa.[Toiminnanohjauksen_kokeilu] 
-from [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8] t
-outer apply (
-    select top 1 [Toiminnanohjauksen_kokeilu]
-    from [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8_toiminnanohjauksen_kokeilu]
-    where [koul_jarj] = t.[koul_jarj]
-    order by voimaan_alkaen desc
-) oa
-where t.voimaan_alkaen >= '2025-01-01'
-and [Toiminnanohjauksen_kokeilu] is null
---
+++ db/sql/11312__insert_table_sa_amos_siirtotiedosto_okm8.sql
@@ -0,0 +1,14 @@
+use antero
+go
+
+update t 
+set [Toiminnanohjauksen kokeilu] = oa.[Toiminnanohjauksen_kokeilu] 
+from [sa].[sa_amos_siirtotiedosto_okm8] t
+outer apply (
+    select top 1 [Toiminnanohjauksen_kokeilu]
+    from [sa].[sa_amos_siirtotiedosto_okm8_toiminnanohjauksen_kokeilu]
+    where [koul_jarj] = t.[koul_jarj]
+    order by voimaan_alkaen desc
+) oa
+where t.voimaan_alkaen >= '2025-01-01'
+and [Toiminnanohjauksen_kokeilu] is null
