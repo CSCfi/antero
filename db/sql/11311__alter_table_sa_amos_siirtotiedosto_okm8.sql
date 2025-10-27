d9c6bec67ae2889b4556c1133f62557b16419302
juhasyrj <juha.syrjala@csc.fi>
2025-10-27 15:09:56 +0200

    CSCVIPUNEN-4642: lisätään ja populoidaan sa-tauluun sarake

---
+++ db/sql/11311__alter_table_sa_amos_siirtotiedosto_okm8.sql
@@ -0,0 +1,24 @@
+use ANTERO
+go
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE name = N'Toiminnanohjauksen kokeilu'
+    AND object_ID = object_ID(N'sa.sa_amos_siirtotiedosto_okm8')
+)
+alter table [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8]
+add [Toiminnanohjauksen kokeilu] nvarchar(50) 
+;
+
+update t 
+set [Toiminnanohjauksen kokeilu] = oa.[Toiminnanohjauksen_kokeilu] 
+from [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8] t
+outer apply (
+    select top 1 [Toiminnanohjauksen_kokeilu]
+    from [ANTERO].[sa].[sa_amos_siirtotiedosto_okm8_toiminnanohjauksen_kokeilu]
+    where [koul_jarj] = t.[koul_jarj]
+    order by voimaan_alkaen desc
+) oa
+where t.voimaan_alkaen >= '2025-01-01'
+and [Toiminnanohjauksen_kokeilu] is null
