25ad4426673c8db0618f15904f05b5ae14419184
juhasyrj <juha.syrjala@csc.fi>
2025-10-29 18:09:53 +0200

    CSCVIPUNEN-4642: muuttujalisäys kuutioiden lähdenäkymiin

---
+++ db/sql/11327__alter_view_v_amos_palautteet.sql
@@ -0,0 +1,118 @@
+USE [ANTERO]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER VIEW [dw].[v_amos_palautteet] AS 
+
+SELECT 
+	[Toteuma/Estimaatti] = case when v.generoitu = 1 then 'Estimaatti' else 'Toteuma' end
+	,[Varainhoitovuosi]
+	,[Rahoituskausi]
+	,[Rahoituskausi (palautteet)] = [Rahoituskausi]
+	,[Suorituskausi]
+	,[Tilastovuosi]
+	,[Tilastokuukausi]
+	,[Järjestäjärakenne]
+
+	,[Kokonaisuus] = 'Opiskelijapalaute'
+
+	,[Kysely]
+	,[Kyselyn kohderyhmä]
+
+	,[Tutkinto]
+	,[Tutkintotyyppi]
+	,[Koulutusala, taso 1]
+	,[Koulutusala, taso 2]
+	,[Koulutusala, taso 3]
+
+	,[Koulutuksen järjestäjä]
+	,[Koulutuksen järjestäjän kunta]
+	,[Koulutuksen järjestäjän maakunta]
+	,[Toiminnanohjauksen kokeilu]
+	,[Koul. järjestäjän PILKE-alue (kokeilu)]
+	,[Oppilaitos]
+	,[Oppilaitoksen kunta]
+	,[Oppilaitoksen maakunta]
+
+	--koodit
+	,[Koodit Koulutuksen järjestäjä]
+	,[Koodit Tutkinto]
+
+	--jarjestys
+	,[jarjestys Tilastokuukausi]
+	,[jarj Koulutuksen järjestäjän maakunta]
+	,[jarjestys Toiminnanohjauksen kokeilu]
+	,[jarjestys Koul. järjestäjän PILKE-alue (kokeilu)]
+	,[jarjestys Oppilaitoksen maakunta]
+	,[jarjestys Tutkintotyyppi]
+	,[jarjestys Koulutusala, taso 1]
+	,[jarjestys Koulutusala, taso 2]
+	,[jarjestys Koulutusala, taso 3]
+
+	--apusarakkeet
+	,pisteet = 1.0 * pisteet
+	,painotetut_pisteet
+
+FROM dw.v_amos_opiskelijapalaute_uusi v
+
+UNION ALL
+
+SELECT 
+	[Toteuma/Estimaatti] = case when generoitu = 1 then 'Estimaatti' else 'Toteuma' end
+	,varainhoitovuosi
+	,rahoituskausi
+	,[Rahoituskausi (palautteet)] = rahoituskausi
+	,suorituskausi
+	,tilastovuosi
+	,tilastokuukausi
+	,[Järjestäjärakenne] = case when sisaltyy_amos_spl = 1 then 'Viimeisin suoritepäätös' else 'Poistuneet järjestäjät' end
+	
+	,[Kokonaisuus] = 'Työelämäpalaute'
+
+	,[Kysely] = case when kysely_fi like '%työpaikkakysely%' then 'Työpaikkakysely' else 'Työpaikkaohjaajakysely' end
+	,[Kyselyn kohderyhmä] = 'Tieto puuttuu'
+
+	,[Tutkinto]
+	,[Tutkintotyyppi]
+	,[Koulutusala, taso 1]
+	,[Koulutusala, taso 2]
+	,[Koulutusala, taso 3]
+	
+	,koulutuksen_jarjestaja_fi
+	,koulutuksen_jarjestajan_kunta_fi
+	,koulutuksen_jarjestajan_maakunta_fi
+	,[Toiminnanohjauksen kokeilu]
+	,[Koul. järjestäjän PILKE-alue (kokeilu)]
+	,[Oppilaitos]
+	,[Oppilaitoksen kunta]
+	,[Oppilaitoksen maakunta]
+
+	--koodit
+	,koodit_koulutuksen_jarjestaja
+	,[Koodit Tutkinto]
+
+	--jarjestys
+	,jarjestys_tilastokuukausi
+	,jarjestys_koulutuksen_jarjestajan_maakunta
+	,[jarjestys Toiminnanohjauksen kokeilu]
+	,[jarjestys Koul. järjestäjän PILKE-alue (kokeilu)]
+	,[jarjestys Oppilaitoksen maakunta]
+	,[jarjestys Tutkintotyyppi]
+	,[jarjestys Koulutusala, taso 1]
+	,[jarjestys Koulutusala, taso 2]
+	,[jarjestys Koulutusala, taso 3]
+
+	--apusarakkeet
+	,apusarake_pisteet
+	,apusarake_painotetut_pisteet
+
+FROM dw.v_amos_tyoelamapalaute v2
+
+GO
