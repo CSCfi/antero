2b39022817d5aa8e3db1e37247343256da8a363b
vavainio <valtteri.vainio@csc.fi>
2025-11-17 18:06:22 +0200

    CSCVIPUNEN-4651

--- db/sql/11387__alter_proc_anteroapi_p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus.sql
+++ db/sql/11387__alter_proc_anteroapi_p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus.sql
@@ -1,7 +1,7 @@
 USE [AnteroAPI]
 GO
 
-/****** Object:  StoredProcedure [dw].[p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus]    Script Date: 17.11.2025 12.57.53 ******/
+/****** Object:  StoredProcedure [dw].[p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus]    Script Date: 17.11.2025 17.44.22 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,7 +11,8 @@ GO
 
 
 
-CREATE OR ALTER PROCEDURE [dw].[p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus] as
+
+ALTER   PROCEDURE [dw].[p_lataa_api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet_esilataus] as
 
 DROP TABLE IF EXISTS ANTERO.sa.temp_haku_ja_valinta_api
 
@@ -37,6 +38,37 @@ FROM ANTERO.dw.f_haku_ja_valinta_hakeneet_toinen_aste f
 LEFT JOIN ANTERO.dw.d_haku d2 ON d2.id = f.d_haku_id
 LEFT JOIN ANTERO.dw.f_haku_ja_valinta_aloituspaikat_ja_pistemaarat f2 on f.d_hakukohde_id = f2.d_hakukohde_id
 WHERE f.koulutuksen_alkamisvuosi > 2013 and d2.korkeakouluhaku=0 and d2.hakutapa_koodi in ('01','03')
+GROUP BY f.koulutuksen_alkamisvuosi,f.d_kausi_koulutuksen_alkamiskausi_id,f.d_hakukohde_id,f.d_haku_id,f.d_kieli_koulutus_id,f.d_koulutusluokitus_hakukohde_id,f.d_alueluokitus_hakukohde_id,f.d_organisaatio_koulutuksen_jarjestaja_id,f.d_organisaatio_oppilaitos_id,f.d_organisaatio_toimipiste_id
+
+INSERT INTO ANTERO.sa.temp_haku_ja_valinta_api
+SELECT
+	f.koulutuksen_alkamisvuosi,
+	f.d_kausi_koulutuksen_alkamiskausi_id,
+	f.d_hakukohde_id,
+	f.d_haku_id,
+	f.d_kieli_koulutus_id,
+	f.d_koulutusluokitus_hakukohde_id,
+	f.d_alueluokitus_hakukohde_id,
+	f.d_organisaatio_koulutuksen_jarjestaja_id,
+	f.d_organisaatio_oppilaitos_id,
+	f.d_organisaatio_toimipiste_id,
+	MIN(f.aloituspaikat) as aloituspaikat_lkm,
+	0 as kaikki_hakijat_lkm,
+	0 as ensisijaiset_hakijat_lkm,
+	0 as valitut_lkm,
+	0 as paikan_vastaanottaneet_lkm,
+	0 as aloittaneet_lkm
+FROM ANTERO.dw.f_haku_ja_valinta_aloituspaikat_ja_pistemaarat f
+LEFT JOIN ANTERO.dw.d_haku d2 ON d2.id = f.d_haku_id
+LEFT JOIN ANTERO.dw.d_hakukohde d3 on d3.id = f.d_hakukohde_id
+WHERE f.koulutuksen_alkamisvuosi > 2013 and d2.korkeakouluhaku=0 and d2.hakutapa_koodi in ('01','03') AND f.aloituspaikat is not null
+AND NOT EXISTS (
+	SELECT TOP 1 haku_oid 
+	FROM ANTERO.dw.f_haku_ja_valinta_hakeneet_toinen_aste f2 
+	LEFT JOIN ANTERO.dw.d_hakukohde d0a on d0a.id = f2.d_hakukohde_id
+	LEFT JOIN ANTERO.dw.d_haku d0b ON d0b.id = f.d_haku_id
+	WHERE d0a.oid = d3.oid and f.koulutuksen_alkamisvuosi > 2013 and d2.korkeakouluhaku=0 and d2.hakutapa_koodi in ('01','03')
+) 
 GROUP BY f.koulutuksen_alkamisvuosi,f.d_kausi_koulutuksen_alkamiskausi_id,f.d_hakukohde_id,f.d_haku_id,f.d_kieli_koulutus_id,f.d_koulutusluokitus_hakukohde_id,f.d_alueluokitus_hakukohde_id,f.d_organisaatio_koulutuksen_jarjestaja_id,f.d_organisaatio_oppilaitos_id,f.d_organisaatio_toimipiste_id
 
 TRUNCATE TABLE AnteroAPI.dw.api_perusopetuksen_jalkeinen_koulutus_hakeneet_ja_paikan_vastaanottaneet
@@ -85,4 +117,5 @@ LEFT JOIN ANTERO.dw.d_kieli d27 ON d27.id=f.d_kieli_koulutus_id
 DROP TABLE IF EXISTS ANTERO.sa.temp_haku_ja_valinta_api
 
 GO
+
 
