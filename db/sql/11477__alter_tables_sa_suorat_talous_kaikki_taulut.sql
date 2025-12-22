dec5a01856d1a91bfbc1e9490880867fac1c48d3
villehoo <ville.hamalainen@csc.fi>
2025-12-22 11:28:10 +0200

    CSCVIPUNEN-4427 mahdollisten suurten talouslukujen takia int ->bigint – lisayksia

---
+++ db/sql/11477__alter_tables_sa_suorat_talous_kaikki_taulut.sql
@@ -0,0 +1,17 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_amos_tase_ja_tunnusluvut_kustannuskysely]    Script Date: 22.12.2025 8.15.30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER TABLE sa_suorat_amk_talous_3_tuloslaskelman_toiminnot ALTER COLUMN TUOTOT BIGINT
+
+
+ALTER TABLE sa.sa_suorat_yo_talous_3_tuloslaskelman_toiminnot ALTER COLUMN [KKVALTRAH] BIGINT
+
+ALTER TABLE sa.sa_suorat_yo_talous_3_tuloslaskelman_toiminnot ALTER COLUMN [KULKAT] BIGINT
