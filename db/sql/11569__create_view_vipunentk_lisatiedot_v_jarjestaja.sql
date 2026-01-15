44a4ec61c136258549b9b13e36b8de4782a9b527
Mika Rissanen <mika.rissanen@csc.fi>
2026-01-15 15:09:15 +0200

    CSCVIPUNEN-4681

---
+++ db/sql/11569__create_view_vipunentk_lisatiedot_v_jarjestaja.sql
@@ -0,0 +1,42 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  View [dbo].[v_jarjestaja]    Script Date: 15.1.2026 15.06.21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE OR ALTER VIEW [dbo].[v_jarjestaja] AS
+SELECT [id]
+      ,[koulutuksen_jarjestajakoodi]
+      ,[alkaa]
+      ,[paattyy]
+      ,[tilv]
+      ,[korvaavakoodi]
+      ,[koulutuksen_jarjestajaselite]
+      ,[koulutuksen_jarjestajaselite_SV]
+      ,[koulutuksen_jarjestajaselite_EN]
+      ,[omistajatyyppikoodi]
+      ,[omistajatyyppiselite]
+      ,[omistajatyyppiselite_SV]
+      ,[omistajatyyppiselite_EN]
+      ,[koulutuksen_jarjestajan_kielikoodi]
+      ,[koulutuksen_jarjestajan_kieliselite]
+      ,[koulutuksen_jarjestajan_kieliselite_SV]
+      ,[koulutuksen_jarjestajan_kieliselite_EN]
+      ,[sijaintikuntakoodi]
+      ,[jarjestys]
+      ,[jarjestys_omistajatyyppi]
+      ,[jarjestys_koulutuksen_jarjestajan_kieli]
+      ,[virhetilanne]
+      ,[tietolahde]
+  FROM [VipunenTK_lisatiedot].[dbo].[jarjestaja]
+
+GO
+
+
