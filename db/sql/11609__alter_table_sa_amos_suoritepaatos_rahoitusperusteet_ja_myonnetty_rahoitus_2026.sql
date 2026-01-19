24bc99a4aa2a56e502bd22f7a924a3c8fd82bfec
juhasyrj <juha.syrjala@csc.fi>
2026-01-19 19:22:18 +0200

    if exists

--- db/sql/11609__alter_table_sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.sql
+++ db/sql/11609__alter_table_sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.sql
@@ -1,5 +1,12 @@
 use antero
 go
 
-EXEC sp_rename 'sa.sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.[Perusrahoitus tavoitteellisten opiskelijavuosien perusteella, €]', 'Tavoitteelliset opiskelijavuodet, €', 'COLUMN'
+IF EXISTS(
+	SELECT 1 FROM sys.columns 
+    WHERE Name = N'Perusrahoitus tavoitteellisten opiskelijavuosien perusteella, €'
+    AND Object_ID = Object_ID(N'sa.sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026')
+)	
+BEGIN
+	EXEC sp_rename 'sa.sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.[Perusrahoitus tavoitteellisten opiskelijavuosien perusteella, €]', 'Tavoitteelliset opiskelijavuodet, €', 'COLUMN'
+END
 ;
