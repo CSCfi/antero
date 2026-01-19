c8315d18b50ec26240030aceba2aacce227a2f16
juhasyrj <juha.syrjala@csc.fi>
2026-01-19 19:14:17 +0200

    CSCVIPUNEN-4845: sarakkeen nimenmuutos

---
+++ db/sql/11609__alter_table_sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.sql
@@ -0,0 +1,5 @@
+use antero
+go
+
+EXEC sp_rename 'sa.sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.[Perusrahoitus tavoitteellisten opiskelijavuosien perusteella, €]', 'Tavoitteelliset opiskelijavuodet, €', 'COLUMN'
+;
