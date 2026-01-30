f21a4c228727d3fba927543154cad9c743f40a7d
Mika Rissanen <mika.rissanen@csc.fi>
2026-01-30 10:53:36 +0200

    CSCVIPUNEN-4853

---
+++ db/sql/11627__alter_view_vipunentk_sa_v_sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain.sql
@@ -0,0 +1,285 @@
+USE [VipunenTK_SA]
+GO
+
+/****** Object:  View [dbo].[v_sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]    Script Date: 23.1.2026 9.52.06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+ALTER VIEW [dbo].[v_sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain] AS
+
+/*
+Aineisto muuttui K-aineistoksi 2016. K-koodia ei lisätty näkymän nimeen. /18.1.2016 CSC Jarmo
+
+Drop table [VipunenTK_DW].[dbo].[sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]
+Select * into [VipunenTK_DW].[dbo].[sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain] FROM VipunenTK_SA.[dbo].[v_sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]
+Truncate table [VipunenTK_DW].[dbo].[sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]
+Insert into [VipunenTK_DW].[dbo].[sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]
+Select * FROM VipunenTK_SA.[dbo].[v_sa_5_2_Yliopistojen_tutkimustyovuodet_yliopistoittain]
+*/
+
+SELECT [tilv] = CAST(kaikkivuodet.tilv as nvarchar(4))
+	  ,[tilv_date] = Cast(kaikkivuodet.tilv+'0101' as date)
+      ,[tunn] = CAST(kaikkivuodet.tunn as nvarchar(10))
+      ,[tiede] = CAST(kaikkivuodet.tiede as nvarchar(10))
+      ,[vryhma] = CAST(kaikkivuodet.vryhma as nvarchar(10))
+	  ,[tuturap] = CAST(kaikkivuodet.tuturap as nvarchar(10))
+      ,[ikar4] = CAST(ltrim(rtrim(kaikkivuodet.ikar4)) as nvarchar(10))
+      ,[sp] = CAST(kaikkivuodet.sp as nvarchar(10))
+      ,[tutktaso] = null --ei käyttökelpoinen, jatkossa erikseen aineistosta 5.2b tai TK:n rajapinnasta
+	  ,[kaste_t2] = null --ei käyttökelpoinen, jatkossa erikseen aineistosta 5.2b tai TK:n rajapinnasta
+      ,[lkm_tutkimustyovuodet] = CAST(kaikkivuodet.ttv_lkm as decimal(8,4))
+	  ,[apuraha] = CAST(kaikkivuodet.apuraha as decimal(8,4))
+      ,[tietolahde] = CAST(kaikkivuodet.tietolahde as nvarchar(50))
+      ,[rivinumero] = CAST(kaikkivuodet.rivinumero as int)
+FROM (
+
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA07_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso]
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_12'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_12]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso]
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_13'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_13]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso]
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_14'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_14]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso]
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_15'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_15]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso]
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_16'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_16]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2]
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_17'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_17]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2]
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_18'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_18]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2]
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_19'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_19]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL --ei käyttökelpoinen, jatkossa erikseen aineistosta 5.2b tai TK:n rajapinnasta
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_20'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_20]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma]
+	  ,[tuturap] = NULL
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_21'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_21]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma] = NULL
+	  ,[tuturap]
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_22'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_22]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma] = NULL
+	  ,[tuturap]
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_23'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_23]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma] = NULL
+	  ,[tuturap]
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_24'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_24]
+
+UNION ALL
+
+SELECT [tilv]
+      ,[tunn]
+      ,[tiede] = 'TA10_'+tiede
+      ,[vryhma] = NULL
+	  ,[tuturap]
+      ,[ikar4]
+      ,[sp]
+      ,[tutktaso] = NULL
+	  ,[kaste_t2] = NULL
+      ,[ttv_lkm]
+	  ,[apuraha]
+      ,[tietolahde]='TK_K5_2_sopv_25'
+      ,[rivinumero] = ROW_NUMBER() OVER(ORDER BY tilv)
+FROM [TK_H9098_CSC].[dbo].[TK_K5_2_sopv_25]
+
+) kaikkivuodet
+
+GO
+
+
