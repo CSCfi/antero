6f19c294fb7ed0af516999b767b2b810cbb73f41
juhasyrj <juha.syrjala@csc.fi>
2026-02-09 14:43:23 +0200

    CSCVIPUNEN-4845, CSCVIPUNEN-4846: lisätään metatiedoille sarakkeet

---
+++ db/sql/11639__alter_table_sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026.sql
@@ -0,0 +1,32 @@
+use antero 
+go
+
+if not exists (
+	SELECT * 
+	FROM sys.columns 
+	WHERE object_id = OBJECT_ID(N'[sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]') 
+	AND name = 'tiedostonimi'
+)
+alter table [sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]
+add tiedostonimi varchar(255) null
+go
+
+if not exists (
+	SELECT * 
+	FROM sys.columns 
+	WHERE object_id = OBJECT_ID(N'[sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]') 
+	AND name = 'lastmodified'
+)
+alter table [sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]
+add lastmodified datetime2(7) null
+go
+
+if not exists (
+	SELECT * 
+	FROM sys.columns 
+	WHERE object_id = OBJECT_ID(N'[sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]') 
+	AND name = 'imp_date'
+)
+alter table [sa].[sa_amos_suoritepaatos_rahoitusperusteet_ja_myonnetty_rahoitus_2026]
+add imp_date datetime2(7) null
+go
