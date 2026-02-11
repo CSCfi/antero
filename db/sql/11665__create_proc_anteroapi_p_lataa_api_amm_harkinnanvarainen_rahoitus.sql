ebe346f5b15e0a830ba753080b30460b80524644
juhasyrj <juha.syrjala@csc.fi>
2026-02-11 20:23:05 +0200

    CSCVIPUNEN-4845, CSCVIPUNEN-4846: ed. skriptien nimenmuutos, API-tietojoukon lataukset

---
+++ db/sql/11665__create_proc_anteroapi_p_lataa_api_amm_harkinnanvarainen_rahoitus.sql
@@ -0,0 +1,87 @@
+USE [AnteroAPI]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER PROCEDURE [dw].[p_lataa_api_amm_harkinnanvarainen_rahoitus] AS
+
+DECLARE @pvm date
+
+BEGIN
+	EXEC [dw].[p_lataa_api_amm_harkinnanvarainen_rahoitus_esilataus]
+END
+
+IF EXISTS (select 1 from [dw].[api_amm_harkinnanvarainen_rahoitus])
+AND (
+  EXISTS (
+	select [vuosi],[suoritepaatos],[suoritepaatoksen_tyyppi],[talousarvio],[kayttotarkoituksen_kohdennus],[koulutuksen_jarjestaja],[koulutuksen_jarjestajan_omistajatyyppi],[koulutuksen_jarjestajan_opetuskieli],[koulutuksen_jarjestajan_kunta],[koodit_koulutuksen_jarjestaja],[koodit_koulutuksen_jarjestajan_kunta],koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu,toiminnanohjauksen_kokeilu_kylla_ei,harkinnanvarainen_rahoitus_euroa
+	from [dw].[api_amm_harkinnanvarainen_rahoitus]
+	except
+	select [vuosi],[suoritepaatos],[suoritepaatoksen_tyyppi],[talousarvio],[kayttotarkoituksen_kohdennus],[koulutuksen_jarjestaja],[koulutuksen_jarjestajan_omistajatyyppi],[koulutuksen_jarjestajan_opetuskieli],[koulutuksen_jarjestajan_kunta],[koodit_koulutuksen_jarjestaja],[koodit_koulutuksen_jarjestajan_kunta],koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu,toiminnanohjauksen_kokeilu_kylla_ei,harkinnanvarainen_rahoitus_euroa
+	from [api].[amm_harkinnanvarainen_rahoitus]
+  )
+  OR EXISTS (
+	select [vuosi],[suoritepaatos],[suoritepaatoksen_tyyppi],[talousarvio],[kayttotarkoituksen_kohdennus],[koulutuksen_jarjestaja],[koulutuksen_jarjestajan_omistajatyyppi],[koulutuksen_jarjestajan_opetuskieli],[koulutuksen_jarjestajan_kunta],[koodit_koulutuksen_jarjestaja],[koodit_koulutuksen_jarjestajan_kunta],koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu,toiminnanohjauksen_kokeilu_kylla_ei,harkinnanvarainen_rahoitus_euroa
+	from [api].[amm_harkinnanvarainen_rahoitus]
+	except
+	select [vuosi],[suoritepaatos],[suoritepaatoksen_tyyppi],[talousarvio],[kayttotarkoituksen_kohdennus],[koulutuksen_jarjestaja],[koulutuksen_jarjestajan_omistajatyyppi],[koulutuksen_jarjestajan_opetuskieli],[koulutuksen_jarjestajan_kunta],[koodit_koulutuksen_jarjestaja],[koodit_koulutuksen_jarjestajan_kunta],koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu,toiminnanohjauksen_kokeilu_kylla_ei,harkinnanvarainen_rahoitus_euroa
+	from [dw].[api_amm_harkinnanvarainen_rahoitus]
+  )
+)
+BEGIN
+	
+	SET @pvm = dbo.api_update_time(getdate())
+  
+	TRUNCATE TABLE [api].[amm_harkinnanvarainen_rahoitus]
+
+	INSERT INTO [api].[amm_harkinnanvarainen_rahoitus] (
+		[defaultorder]
+		,[vuosi]
+		,[suoritepaatos]
+		,[suoritepaatoksen_tyyppi]
+		,[talousarvio]
+		,[kayttotarkoituksen_kohdennus]
+		,[koulutuksen_jarjestaja]
+		,[koulutuksen_jarjestajan_omistajatyyppi]
+		,[koulutuksen_jarjestajan_opetuskieli]
+		,[koulutuksen_jarjestajan_kunta]
+		,[koodit_koulutuksen_jarjestaja]
+		,[koodit_koulutuksen_jarjestajan_kunta]
+		,koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu
+		,toiminnanohjauksen_kokeilu_kylla_ei
+		,[harkinnanvarainen_rahoitus_euroa]
+		,tietojoukko_paivitetty_pvm
+	)
+	SELECT 
+		[defaultorder]
+		,[vuosi]
+		,[suoritepaatos]
+		,[suoritepaatoksen_tyyppi]
+		,[talousarvio]
+		,[kayttotarkoituksen_kohdennus]
+		,[koulutuksen_jarjestaja]
+		,[koulutuksen_jarjestajan_omistajatyyppi]
+		,[koulutuksen_jarjestajan_opetuskieli]
+		,[koulutuksen_jarjestajan_kunta]
+		,[koodit_koulutuksen_jarjestaja]
+		,[koodit_koulutuksen_jarjestajan_kunta]
+		,koodit_koulutuksen_jarjestajan_pilke_alue_kokeilu
+		,toiminnanohjauksen_kokeilu_kylla_ei
+		,[harkinnanvarainen_rahoitus_euroa]
+		,[tietojoukko_paivitetty_pvm] = @pvm
+	FROM [dw].[api_amm_harkinnanvarainen_rahoitus]
+
+END
+
+IF NOT EXISTS (select 1 from [dw].[api_amm_harkinnanvarainen_rahoitus])
+BEGIN
+	RAISERROR('Source table has no rows.',11,1)
+END
+
+GO
