eb9a84febe5e2b3ad11d472ab93bea9ffa42b3a2
Mika Rissanen <mika.rissanen@csc.fi>
2026-04-17 16:32:15 +0300

    CSCVIPUNEN-4887

---
+++ db/sql/11974__create_table_vipunentk_lisatiedot_koulutusala_vst.sql
@@ -0,0 +1,35 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  Table [dbo].[koulutusala_vst]    Script Date: 17.4.2026 16.26.14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[koulutusala_vst]') AND type in (N'U'))
+
+CREATE TABLE [dbo].[koulutusala_vst](
+	[id] [int] NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[koulutusala_koodi] [nvarchar](10) NULL,
+	[koulutusala_taso1] [nvarchar](250) NULL,
+	[koulutusala_taso1_SV] [nvarchar](250) NULL,
+	[koulutusala_taso1_EN] [nvarchar](250) NULL,
+	[koulutusala_taso2] [nvarchar](250) NULL,
+	[koulutusala_taso2_SV] [nvarchar](250) NULL,
+	[koulutusala_taso2_EN] [nvarchar](250) NULL,
+	[taso1_jarjestys] [nvarchar](50) NULL,
+	[taso2_jarjestys] [nvarchar](50) NULL,
+	[virhetilanne] [nchar](1) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+
+GO
+
+
