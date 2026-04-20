2864e6b995e1c5d815f3ee448b0872e91888071f
Mika Rissanen <mika.rissanen@csc.fi>
2026-04-20 15:31:00 +0300

    CSCVIPUNEN-4887

---
+++ db/sql/11983__create_table_vipunentk_lisatiedot_kieli_vst.sql
@@ -0,0 +1,30 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  Table [dbo].[kieli_vst]    Script Date: 20.4.2026 15.23.07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[kieli_vst]') AND type in (N'U'))
+
+CREATE TABLE [dbo].[kieli_vst](
+	[id] [int] NOT NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[kieli_koodi] [nvarchar](2) NULL,
+	[kieli] [nvarchar](50) NULL,
+	[kieli_SV] [nvarchar](50) NULL,
+	[kieli_EN] [nvarchar](50) NULL,
+	[jarjestys] [smallint] NULL,
+	[virhetilanne] [nchar](1) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
