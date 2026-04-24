8a52126f0cb73d9dc94d50613cb641c370deeb14
Mika Rissanen <mika.rissanen@csc.fi>
2026-04-24 15:12:55 +0300

    CSCVIPUNEN-4887

---
+++ db/sql/12016__create_table_vipunentk_lisatiedot_paaasiallinen_toiminta_vst.sql
@@ -0,0 +1,30 @@
+USE [VipunenTK_lisatiedot]
+GO
+
+/****** Object:  Table [dbo].[paaasiallinen_toiminta_vst]    Script Date: 24.4.2026 15.07.39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[paaasiallinen_toiminta_vst]') AND type in (N'U'))
+
+CREATE TABLE [dbo].[paaasiallinen_toiminta_vst](
+	[id] [int] NOT NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[paaasiallinen_toiminta_koodi] [nvarchar](10) NULL,
+	[paaasiallinen_toiminta] [nvarchar](250) NULL,
+	[paaasiallinen_toiminta_SV] [nvarchar](250) NULL,
+	[paaasiallinen_toiminta_EN] [nvarchar](250) NULL,
+	[jarjestys] [nvarchar](50) NULL,
+	[virhetilanne] [nchar](10) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
