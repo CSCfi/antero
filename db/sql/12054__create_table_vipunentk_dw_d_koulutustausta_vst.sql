825419afa4553f29f823211ced0e5953908c5f93
Mika Rissanen <mika.rissanen@csc.fi>
2026-04-29 14:27:40 +0300

    CSCVIPUNEN-4887

---
+++ db/sql/12054__create_table_vipunentk_dw_d_koulutustausta_vst.sql
@@ -0,0 +1,30 @@
+USE [VipunenTK_DW]
+GO
+
+/****** Object:  Table [dbo].[d_koulutustausta_vst]    Script Date: 29.4.2026 14.07.50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[d_koulutustausta_vst]') AND type in (N'U'))
+
+CREATE TABLE [dbo].[d_koulutustausta_vst](
+	[id] [int] NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[koulutustausta_koodi] [nvarchar](2) NULL,
+	[koulutustausta] [nvarchar](250) NULL,
+	[koulutustausta_SV] [nvarchar](250) NULL,
+	[koulutustausta_EN] [nvarchar](250) NULL,
+	[jarjestys] [nvarchar](50) NULL,
+	[virhetilanne] [nchar](1) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
