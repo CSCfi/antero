90327c36086dac92f269acd28baf1bd65c93e3df
Mika Rissanen <mika.rissanen@csc.fi>
2026-04-29 10:43:11 +0300

    CSCVIPUNEN-4887

---
+++ db/sql/12047__create_table_vipunentk_d_oppimaara_vst.sql
@@ -0,0 +1,30 @@
+USE [VipunenTK]
+GO
+
+/****** Object:  Table [dbo].[d_oppimaara_vst]    Script Date: 29.4.2026 10.26.28 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[d_oppimaara_vst]') AND type in (N'U'))
+
+CREATE TABLE [dbo].[d_oppimaara_vst](
+	[id] [int] NULL,
+	[luotu] [date] NULL,
+	[alkaa] [date] NULL,
+	[paattyy] [date] NULL,
+	[oppimaara_koodi] [nvarchar](2) NULL,
+	[oppimaara] [nvarchar](250) NULL,
+	[oppimaara_SV] [nvarchar](250) NULL,
+	[oppimaara_EN] [nvarchar](250) NULL,
+	[jarjestys] [nvarchar](50) NULL,
+	[virhetilanne] [nchar](1) NULL,
+	[poistettu] [nchar](1) NULL,
+	[tietolahde] [nvarchar](50) NULL,
+	[kommentti] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
