43173c3ce7426810cb0675aa05fdadcd53d9a87d
kohojoha <johanna.koho@csc.fi>
2026-06-05 18:00:30 +0300

    CSCVIPUNEN-5041:SQL muutokset tuen tiedot

---
+++ db/sql/12111__alter_proc_p_lataa_f_varda_tuentiedot.sql
@@ -0,0 +1,93 @@
+USE [ANTERO]
+GO
+
+/****** Object:  StoredProcedure [dw].[p_lataa_f_varda_tuentiedot]    Script Date: 5.6.2026 17.20.06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ALTER   PROCEDURE [dw].[p_lataa_f_varda_tuentiedot]
+AS
+
+
+
+TRUNCATE TABLE dw.f_varda_tuentiedot
+
+
+
+-- Faktataulun lataus
+
+INSERT INTO dw.f_varda_tuentiedot (
+	tilastovuosi
+	,[tukipaatosmaara_id]
+	,[tukipaatosmaara]
+	,[vakajarjestaja_id]
+	,d_varda_yritysmuoto_id
+	,d_alueluokitus_id
+	,d_organisaatioluokitus_id
+	,d_kytkin_yksityinen_jarjestaja
+	,d_tuentaso_id
+	,d_ikaryhma_id
+	,source
+)
+
+SELECT
+	YEAR(s.tilastointi_pvm) as tilastovuosi,
+	s.id as tukipaatosmaara_id,
+	s.tukipaatosmaara as tukipaatosmaara,
+	s2.id as vakajarjestaja_id,
+	coalesce(d1.id,'-1') as d_varda_yritysmuoto_id,
+	coalesce(d2.id,'-1') as d_alueluokitus_id,
+	coalesce(d3.id,'-1') as d_organisaatioluokitus_id,
+	coalesce(d4.id,'-1') as d_kytkin_yksityinen_jarjestaja,
+	coalesce(d5.id,'-1') as d_tuentaso_id,
+	coalesce(d6.id,'-1') as d_ikaryhma_id,
+	'p_lataa_f_varda_vuokrattu_henkilosto' as source
+
+FROM varda_sa.sa.sa_varda_tuen_tiedot s
+LEFT JOIN [Varda_SA].[sa].[sa_varda_organisaatio] s2 on s.organisaatio_oid = s2.organisaatio_oid
+LEFT JOIN dw.d_varda_yritysluokitus d1 on d1.koodi = s2.yritysmuoto
+LEFT JOIN dw.d_alueluokitus d2 on d2.kunta_koodi = s2.kunta_koodi
+LEFT JOIN dw.d_organisaatioluokitus d3 on d3.organisaatio_oid = s2.organisaatio_oid
+LEFT JOIN dw.d_kytkin d4 on d4.kytkin_koodi = replace(replace(s.yksityinen_jarjestaja,'FALSE',0),'TRUE',1)
+LEFT JOIN sa.sa_varda_koodistot d5 on d5.koodi = upper(s.tuentaso_koodi)
+LEFT JOIN sa.sa_varda_koodistot d6 on d6.koodi = upper(s.ikaryhma_koodi)
+WHERE 
+-- Organisaation eli vaka-järjestäjän pitää olla voimassa vuoden viimeisenä päivänä. 
+	s2.alkamis_pvm <= cast(concat(YEAR(s.tilastointi_pvm),'-12-31') as date) and (coalesce(s2.paattymis_pvm, '9999-12-31') >= cast(concat(YEAR(s.tilastointi_pvm),'-12-31') as date))
---
+++ db/sql/12112__drop_create_proc_p_lataa_sa_varda_tuen_tiedot.sql
@@ -0,0 +1,63 @@
+USE [Varda_SA]
+GO
+
+/****** Object:  StoredProcedure [sa].[p_lataa_sa_varda_tuen_tiedot]    Script Date: 5.6.2026 17.45.11 ******/
+DROP PROCEDURE [sa].[p_lataa_sa_varda_tuen_tiedot]
+GO
+
+/****** Object:  StoredProcedure [sa].[p_lataa_sa_varda_tuen_tiedot]    Script Date: 5.6.2026 17.45.11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+CREATE   PROCEDURE  [sa].[p_lataa_sa_varda_tuen_tiedot] AS
+
+TRUNCATE TABLE [sa].[sa_varda_tuen_tiedot]
+
+DECLARE @table AS NVARCHAR(max), @query AS nvarchar(max)
+
+
+DECLARE table_cursor CURSOR FOR
+SELECT t.TABLE_NAME 
+FROM [INFORMATION_SCHEMA].[TABLES] t
+WHERE TABLE_NAME like 'sa_varda_tuen_tiedot_20__'
+
+OPEN table_cursor  
+FETCH NEXT FROM table_cursor INTO @table  
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN
+
+SET @query='INSERT INTO [sa].[sa_varda_tuen_tiedot]
+           ([organisaatio_oid]
+           ,[tilastointi_pvm]
+           ,[yksityinen_jarjestaja]
+           ,[tuentaso_koodi]
+           ,[ikaryhma_koodi]
+           ,[tukipaatosmaara]
+           ,[source]
+           ,[loadtime]
+           ,[username]) 
+		   SELECT [organisaatio_oid]
+			,[tilastointi_pvm]
+			,[yksityinen_jarjestaja]
+			,[tuentaso_koodi]
+			,[ikaryhma_koodi]
+			,[tukipaatosmaara]
+			,[source]
+			,[loadtime]
+			,[username]
+			FROM [sa].' +QUOTENAME(@table)  
+
+EXEC sp_executesql @query
+
+	FETCH NEXT FROM table_cursor INTO @table
+END
+CLOSE table_cursor
+DEALLOCATE table_cursor
