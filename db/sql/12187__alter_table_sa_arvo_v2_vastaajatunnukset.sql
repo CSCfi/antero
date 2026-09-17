62d8a6f00ff428aec713b231f472e45bc1c7a853
villehoo <ville.hamalainen@csc.fi>
2026-09-17 10:31:23 +0300

    CSCVIPUNEN-5151 metatietojen lisäys tauluun - virhe korjattu

--- db/sql/12187__alter_table_sa_arvo_v2_vastaajatunnukset.sql
+++ db/sql/12187__alter_table_sa_arvo_v2_vastaajatunnukset.sql
@@ -7,7 +7,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-USE 
 
 ALTER TABLE sa.sa_arvo_v2_vastaajatunnukset ADD metatiedot VARCHAR(2000);
 ALTER TABLE sa.sa_arvo_v2_vastaajatunnukset ADD ei_kuulu_lahetettavien_perusjoukkoon INT;
